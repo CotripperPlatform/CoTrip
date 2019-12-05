0a7041dc072ae2c334941178cc87fca6b9258174
Ren Lucas <llucas314@gmail.com>
2019-12-04 23:25:54 -0500

    updated config

--- browser/.storybook/config.js
+++ browser/.storybook/config.js
@@ -1,4 +1,12 @@
-import { configure } from '@storybook/react';
+import { configure } from "@storybook/react";
+import requireContext from "require-context.macro";
 
-// automatically import all files ending in *.stories.js
-configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+import "../src/index.css";
+
+const req = requireContext("../src/components", true, /\.stories\.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
