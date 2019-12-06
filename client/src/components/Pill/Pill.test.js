9fcb5b3369de0e1f0def889425b71a09788a401e
NickMckenney <mckenney2001@gmail.com>
2019-12-06 16:47:08 -0500

    fixed responsive on pill container and text.  Used view ports to make text responsive

--- client/src/components/Pill/Pill.test.js
+++ client/src/components/Pill/Pill.test.js
@@ -4,11 +4,8 @@ import Pill from "./Pill.js.js";
 import Adapter from "enzyme-adapter-react-16";
 configure({ adapter: new Adapter() });
 
-// We will describe a block of tests
 describe("Pill component", () => {
-  // we will write one individual test
   it("should render as expected", () => {
-    // Shallow rendering renders a component without rendering any of its children
     const component = shallow(<Pill />);
   });
 });
