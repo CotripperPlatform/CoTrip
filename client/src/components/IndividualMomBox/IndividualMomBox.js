0776cb340864a1caaf0dce39a1c1d1f99d507eb9
DallasLind <mcgraw.dallas@gmail.com>
2019-12-06 16:52:36 -0500

    Started the storybook process and began making the css. this is essentially the same as person card and will be regarded as such

---
+++ client/src/components/IndividualMomBox/IndividualMomBox.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import "./IndividualMomBox.css";
+
+// Function based React Component
+const IndividualMomBox = (props) => {
+
+  // Default Class to apply to Component
+  let classList = `IndividualMomBox`;
+
+
+  return(
+    <div className={classList}>
+      IndividualMomBox
+    </div>
+  );
+}
+
+export default IndividualMomBox;
