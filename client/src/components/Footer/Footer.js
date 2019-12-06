55c5a4862e686f521e63582ff6b935991d79db58
William Chrapcynski <wchrapcynski@gmail.com>
2019-12-06 16:49:12 -0500

    Set up footer component

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import "./Footer.css";
+
+// Function based React Component
+const Footer = (props) => {
+
+  // Default Class to apply to Component
+  let classList = `Footer`;
+
+
+  return(
+    <div className={classList}>
+      Footer
+    </div>
+  );
+}
+
+export default Footer;
