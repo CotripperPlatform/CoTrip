af2e840ec04ba70afbc9c53d8baf1ae524beabdd
Ren Lucas <llucas314@gmail.com>
2019-12-06 00:08:00 -0500

    added docs addon

--- browser/.storybook/config.js
+++ browser/.storybook/config.js
@@ -2,7 +2,15 @@ import { configure, addDecorator } from "@storybook/react";
 import StoryRouter from "storybook-react-router";
 import requireContext from "require-context.macro";
 import "../src/index.css";
+import { addParameters } from "@storybook/react";
+import { DocsPage, DocsContainer } from "@storybook/addon-docs/blocks";
 
+addParameters({
+  docs: {
+    container: DocsContainer,
+    page: DocsPage
+  }
+});
 addDecorator(StoryRouter());
 
 const req = requireContext("../src/components", true, /\.stories\.js$/);
