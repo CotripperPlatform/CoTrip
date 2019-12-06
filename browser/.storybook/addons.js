20a48460c7c659f26302b95f22e2dc6ad31d6f9b
Ren Lucas <llucas314@gmail.com>
2019-12-06 08:41:04 -0500

    added links addon

--- browser/.storybook/addons.js
+++ browser/.storybook/addons.js
@@ -4,3 +4,4 @@ import "@storybook/addon-links/register";
 import "@storybook/addon-storysource/register";
 import "@storybook/addon-backgrounds/register";
 import "@storybook/addon-viewport/register";
+import "@storybook/addon-links/register";
