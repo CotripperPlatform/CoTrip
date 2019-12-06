e4b53d41dd247562d83797338d2a9ce21be00e41
blake <blakeeh723@gmail.com>
2019-12-05 20:12:51 -0500

    first jest test working

--- browser/src/App.test.js
+++ browser/src/App.test.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import { shallow, render, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({adapter: new Adapter()});
+
+describe('App component', () => {
+  test('is an Object', () => {
+    const component = shallow(<App />);
+    expect(component).toEqual(Object.prototype);
+  });
+  test('snapshot renders', () => {
+    const component = renderer.create(<App />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  test('contains a header element', () => {
+    const component = render(<App />);
+    expect(component['0'].children[0].name).toBe('header');
+  });
 });
+
+// sources:
+//
+// https://medium.com/@rossbulat/testing-in-react-with-jest-and-enzyme-an-introduction-99ce047dfcf8
+//
+// https://www.robinwieruch.de/react-testing-jest
+//
+// https://gist.github.com/fokusferit/e4558d384e4e9cab95d04e5f35d4f913
