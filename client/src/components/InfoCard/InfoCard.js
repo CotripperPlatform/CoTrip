6bf4c78928d463f3ddfc84a3e66ef93948a1a0eb
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-12-06 16:40:11 -0500

    started markdown and styling

---
+++ client/src/components/InfoCard/InfoCard.js
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import "./InfoCard.css";
+
+// Function based React Component
+const InfoCard = (props) => {
+
+  // Default Class to apply to Component
+  let classList = `InfoCard`;
+  //if Group Card, classNames are groupName, members, and location
+  //if Location card, classNames are locationName and date
+
+  return(
+    <div className={classList}>
+      <p className='groupName locationName'>Group Name</p>
+      <div className='infoCardInfo'>
+        <p className='members'># of Members</p>
+        <p className='location'>Location, State</p>
+      </div>
+      {/* <p className='date'></p> */}
+    </div>
+  );
+}
+
+export default InfoCard;
