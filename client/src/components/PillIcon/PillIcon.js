e6abe48fbe3b5cea8ab6ccb980d3f66edecdaa34
NickMckenney <mckenney2001@gmail.com>
2019-12-07 15:02:53 -0500

    Made a new branch pill-icon-component and merging to dev branch

---
+++ client/src/components/PillIcon/PillIcon.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./PillIcon.css";
+
+// Function based React Component
+const PillIcon = props => {
+  // Default Class to apply to Component
+  let classList = `PillIcon`;
+
+  return <div className={classList}>PillIcon</div>;
+};
+
+export default PillIcon;
---
+++ client/src/components/PillIcon/PillIcon.stories.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import PillIcon from "./PillIcon";
+
+storiesOf("PillIcon", module).add("Default", () => <PillIcon />);
