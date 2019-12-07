5b2b12a5d0ffb58fe09fb397a17ecb67d65041dd
William Chrapcynski <wchrapcynski@gmail.com>
2019-12-07 11:28:03 -0500

    Footer looks good. May need CSS adjustments later

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import "./Footer.css";
+import "../../App.css";
 
 // Function based React Component
 const Footer = (props) => {
@@ -10,12 +11,13 @@ const Footer = (props) => {
 
 
   return (
+    // These are hard links in the HTLM files and will need to be udpated once we have the info
     <div className={classList}>
       <div className="footer-left">
         <a href="">Sitemap</a>
       </div>
       <div className="footer-center">
-        <a href="mailto:info@cotripper.co?">info@cotripper.co</a>
+        <a href="mailto:info@cotripper.co?" target="_blank">info@cotripper.co</a>
       </div>
       <div className="footer-right">
         <a href="">Instagram</a>
