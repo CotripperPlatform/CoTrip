4f78667de5d7dc69d2cbd83a2113e2909dffbfcf
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-07 12:47:35 -0500

    add each type of input field, different styling for small versions

--- client/src/components/InputTextField/InputTextField.js
+++ client/src/components/InputTextField/InputTextField.js
@@ -4,7 +4,7 @@ import "./InputTextField.css";
  * Created by Tyler
  *
  * Props will define the type of input field
- * Input fields include email, password, name, password, confirm password, city *  of residence, what is your age?, what is your dream destination, First name,*   last name, search my directory w/ magnifying glass,
+ * Input fields include email, password, name, confirm password, city of residence, what is your age?, what is your dream destination, First name,   last name, search my directory w/ magnifying glass,
  */
 
 // Class Based React Component
@@ -16,6 +16,7 @@ class InputTextField extends Component {
     // Default CSS class to apply to the Component
     this.state = {
       classList: "input-text-field-container",
+      classListSmall: "input-text-field-container-small",
       email: "",
       name: ""
     };
@@ -47,6 +48,76 @@ class InputTextField extends Component {
       return (
         <div className={this.state.classList}>
           <input type="text" name="name" placeholder="Name" />
+        </div>
+      );
+    }
+    if (this.props.type === "City") {
+      return (
+        <div className={this.state.classList}>
+          <input type="text" name="name" placeholder="City of Residence" />
+        </div>
+      );
+    }
+    if (this.props.type === "Age") {
+      return (
+        <div className={this.state.classList}>
+          <input type="text" name="name" placeholder="What is your age?" />
+        </div>
+      );
+    }
+    if (this.props.type === "Dream") {
+      return (
+        <div className={this.state.classList}>
+          <input type="text" name="name" placeholder="What is your dream destination(s)?" />
+        </div>
+      );
+    }
+    if (this.props.type === "First Name") {
+      return (
+        <div className={this.state.classListSmall}>
+          <input type="text" name="name" placeholder="First Name" />
+        </div>
+      );
+    }
+    if (this.props.type === "Last Name") {
+      return (
+        <div className={this.state.classListSmall}>
+          <input type="text" name="name" placeholder="Last Name" />
+        </div>
+      );
+    }
+    if (this.props.type === "Email Small") {
+      return (
+        <div className={this.state.classListSmall}>
+          <input type="text" name="email" placeholder="Email" onChange={this.changeHandler} />
+        </div>
+      );
+    }
+    if (this.props.type === "Password") {
+      return (
+        <div className={this.state.classList}>
+          <input type="password" name="name" placeholder="Password" />
+        </div>
+      );
+    }
+    if (this.props.type === "Confirm Password") {
+      return (
+        <div className={this.state.classList}>
+          <input type="password" name="name" placeholder="Confirm Password" />
+        </div>
+      );
+    }
+    if (this.props.type === "Search Directory") {
+      return (
+        <div className={this.state.classList}>
+          <input type="text" name="name" placeholder="Search My Directory" />
+        </div>
+      );
+    }
+    if (this.props.type === "Search Trips") {
+      return (
+        <div className={this.state.classList}>
+          <input type="text" name="name" placeholder="Search Trips" />
         </div>
       );
     }
--- client/src/components/InputTextField/InputTextField.stories.js
+++ client/src/components/InputTextField/InputTextField.stories.js
@@ -10,7 +10,8 @@ storiesOf("InputTextField", module)
   .add("Dream", () => <InputTextField type="Dream" />)
   .add("First Name", () => <InputTextField type="First Name" />)
   .add("Last Name", () => <InputTextField type="Last Name" />)
+  .add("Email Small", () => <InputTextField type="Email Small" />)
   .add("Password", () => <InputTextField type="Password" />)
   .add("Confirm Password", () => <InputTextField type="Confirm Password" />)
-  .add("Search Magnify", () => <InputTextField type="Search Magnify" />)
-  .add("Last Name", () => <InputTextField type="Last Name" />);
+  .add("Search Directory", () => <InputTextField type="Search Directory" />)
+  .add("Search Trips", () => <InputTextField type="Search Trips" />);
