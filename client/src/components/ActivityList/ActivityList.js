7a5b11ea485fefe331709555903b5c4ef868561b
Steven Kim <sskim14@gmail.com>
2019-12-06 23:08:43 -0500

    Added method to render Activity Items based on prop type. The prop type will determine which array to use to display on each item.

--- client/src/components/ActivityList/ActivityList.js
+++ client/src/components/ActivityList/ActivityList.js
@@ -1,7 +1,5 @@
-
 import React, {Component} from 'react';
 import "./ActivityList.css";
-
 
 // Class Based React Component
 class ActivityList extends Component{
@@ -13,11 +11,33 @@ class ActivityList extends Component{
     this.state = {
       classList: "ActivityList"
     };
+    this.momActivityList = ['Spa', 'Wine Nights', 'Brunch with Other Moms'];
+    this.kidsActivityList = ['Snorkeling', 'Hiking', 'Island Hopping', 'Hawaiian Luaus', 'Food Markets'];
   }
 
+  renderActivities = () => {
+    const momActivities = this.momActivityList
+    const kidsActivities = this.kidsActivityList
+    let activities = [];
+    if (this.props.type === 'mom') {
+      momActivities.forEach(activity => {
+        activities.push(
+          <div className='activity mom'>{activity}</div>
+        )
+      })
+    } else if (this.props.type === 'kids') {
+      kidsActivities.forEach(activity => {
+        activities.push(
+          <div className='activity kids'>{activity}</div>
+        )
+      })
+    }
+    return <div className='activityContainer'>{activities}</div>
+  }
 
   // Runs after Component is loaded in the broswer
-  componentDidMount(){}
+  componentDidMount(){
+  }
 
 
   // Runs after a component has been updated
@@ -26,11 +46,13 @@ class ActivityList extends Component{
 
   // Runs right before a component is removed from the DOM
   componentWillUnmount(){}
-
+  
   render(){
+    const renderActivities = this.renderActivities()
     return(
       <div className={this.state.classList}>
-        ActivityList
+        <span className='activityHeader'>Activities for {this.props.label}</span>
+        {renderActivities}
       </div>
     );
   }
--- client/src/components/ActivityList/ActivityList.stories.js
+++ client/src/components/ActivityList/ActivityList.stories.js
@@ -5,5 +5,16 @@ import ActivityList from './ActivityList';
 storiesOf('ActivityList', module)
   .add("Default", () => (
     <ActivityList />
-  )
-)
+  ))
+  .add("Mom", () => (
+    <ActivityList
+      type='mom'
+      label='Mom'
+    />
+  ))
+  .add("Kids", () => (
+    <ActivityList
+      type='kids'
+      label='Mom & Kids'
+    />
+  ))
