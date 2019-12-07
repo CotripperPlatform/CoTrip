74989dfc772aa92aeea62d56cdcb47eedb5409ad
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2019-12-06 19:24:39 -0500

    created heart icons in black white and pink. simple border placed around icons to seperate icons from background so you can see, simple comment out or remove border in css file

--- client/src/components/HeartIcon/HeartIcon.stories.js
+++ client/src/components/HeartIcon/HeartIcon.stories.js
@@ -4,8 +4,9 @@ import HeartIcon from './HeartIcon';
 import Hearty from './HeartIcon.png';
 import Heartyy from './HeartIconBlack.png';
 import Heartyyy from './HeartIconPink.png';
+import "./HeartIcon.css";
 
 storiesOf('HeartIcon', module)
-.add('Heart Icon', () => <HeartIcon type="hearty"icon={Hearty}/>)
+.add('Heart Icon White', () => <HeartIcon className="whitecon" type="hearty"icon={Hearty}/>)
 .add('Heart Icon Black', () => <HeartIcon type="hearty"icon={Heartyy}/>)
 .add('Heart Icon Pink', () => <HeartIcon type="hearty"icon={Heartyyy}/>)
