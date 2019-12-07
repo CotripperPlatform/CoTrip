8f5785d4b60db5b451f4a4c2b19de29e7ad09662
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-07 13:55:38 -0500

    added all versions of input, different styles, and sizes

--- client/src/components/InputTextField/InputTextField.js
+++ client/src/components/InputTextField/InputTextField.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./InputTextField.css";
+import searchIcon from "../../../../assets/images/global-nav_search_icon.png";
 /**
  * Created by Tyler
  *
@@ -17,6 +18,7 @@ class InputTextField extends Component {
     this.state = {
       classList: "input-text-field-container",
       classListSmall: "input-text-field-container-small",
+      classListSearch: "input-text-field-container search",
       email: "",
       name: ""
     };
@@ -109,14 +111,26 @@ class InputTextField extends Component {
     }
     if (this.props.type === "Search Directory") {
       return (
-        <div className={this.state.classList}>
+        <div className={this.state.classListSearch}>
+          <svg viewBox="-4 -2 24 24">
+            <path
+              fill="#bababa"
+              d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"
+            />
+          </svg>
           <input type="text" name="name" placeholder="Search My Directory" />
         </div>
       );
     }
     if (this.props.type === "Search Trips") {
       return (
-        <div className={this.state.classList}>
+        <div className={this.state.classListSearch}>
+          <svg viewBox="-4 -2 24 24">
+            <path
+              fill="#bababa"
+              d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"
+            />
+          </svg>
           <input type="text" name="name" placeholder="Search Trips" />
         </div>
       );
