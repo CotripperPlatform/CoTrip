edc638aee9f5d6db9891b5dca0f2d994ce71bc68
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-12-08 13:03:09 -0500

    used hygen to generate function component for issue #93

---
+++ client/src/components/UpcomingTripsCard/UpcomingTripsCard.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import "./UpcomingTripsCard.css";
+
+// Function based React Component
+const UpcomingTripsCard = (props) => {
+
+  // Default Class to apply to Component
+  let classList = `UpcomingTripsCard`;
+
+
+  return(
+    <div className={classList}>
+      UpcomingTripsCard
+    </div>
+  );
+}
+
+export default UpcomingTripsCard;
---
+++ client/src/components/UpcomingTripsCard/UpcomingTripsCard.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import UpcomingTripsCard from './UpcomingTripsCard';
+
+storiesOf('UpcomingTripsCard', module)
+  .add("Default", () => (
+    <UpcomingTripsCard />
+  )
+)
