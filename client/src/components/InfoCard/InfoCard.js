3c1ab2bb9d1f2363fa76ce854a9bcaba116bddb1
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-12-08 12:45:06 -0500

    added conditional rendering for group card and location card

--- client/src/components/InfoCard/InfoCard.js
+++ client/src/components/InfoCard/InfoCard.js
@@ -7,20 +7,34 @@ const InfoCard = (props) => {
 
   // Default Class to apply to Component
   let classList = `InfoCard`;
-  let name = ['Group Name', 'Location']
-  //if Group Card, classNames are groupName, members, and location
-  //if Location card, classNames are locationName and date
+  let types = ['group', 'location'];
 
-  return(
-    <div className={classList}>
-      <p className='groupName locationName'>Group Name</p>
-      <div className='infoCardInfo'>
-        <p className='members'># of Members</p>
-        <p className='location'>Location, State</p>
+  if (types.includes(props.type)) {
+    classList += ` ${props.type}`
+  }
+
+  if (props.type === "group"){
+    return(
+      <div className={classList}>
+        <p className='group-name'>{props.name}</p>
+        <div className='info-card-info'>
+          <p className='members'>{props.members}</p>
+          <p className='location'>{props.location}</p>
+        </div>
       </div>
-      {/* <p className='date'></p> */}
-    </div>
-  );
+    )
+  }
+
+  if (props.type === "location"){
+    return (
+      <div className={classList}>
+        <p className='location-name'>{props.name}</p>
+        <div className='date'>
+          {props.date}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default InfoCard;
--- client/src/components/InfoCard/InfoCard.stories.js
+++ client/src/components/InfoCard/InfoCard.stories.js
@@ -4,11 +4,13 @@ import InfoCard from './InfoCard';
 
 storiesOf('InfoCard', module)
   .add("Group Info Card", () => <InfoCard 
+    type="group"
     name="Group Name"
     members="# of Members"
     location="Location, State"
   />)
   .add("Location Info Card", () => <InfoCard 
+    type="location"
     name="Location"
     date="Month Year"
   />)
