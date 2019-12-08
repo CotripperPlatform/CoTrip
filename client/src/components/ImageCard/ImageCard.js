e1c407591bbd59f34855af618b1da8bc654680cc
Ben Miller <benbmiller123@gmail.com>
2019-12-08 16:38:37 -0500

    added styling for img and styling to allow words on top of it

--- client/src/components/ImageCard/ImageCard.js
+++ client/src/components/ImageCard/ImageCard.js
@@ -5,11 +5,16 @@ import "./ImageCard.css";
 const ImageCard = props => {
   // Default Class to apply to Component
   let classList = `ImageCard`;
-
-  textEls = props.text.map(text => {
-    return <p>{text}</p>;
+  let image = <img className="backgroundImage" src={props.url} alt="trip background" />;
+  let textEls = props.text.map(text => {
+    return <h3 className="textEl">{text}</h3>;
   });
-  return <div className={classList}>{textEls}</div>;
+  return (
+    <div className={classList}>
+      {image}
+      <div className="textHolder">{textEls}</div>
+    </div>
+  );
 };
 
 export default ImageCard;
