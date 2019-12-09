c41f4a94086ea3686ec07bfb366cc73ff6e12bef
NickMckenney <mckenney2001@gmail.com>
2019-12-09 10:24:21 -0500

    Adding color as a prop so the user can pass it in inside of stories. Previous color was asigned to a variable

--- client/src/components/PillIcon/PillIcon.stories.js
+++ client/src/components/PillIcon/PillIcon.stories.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import { storiesOf } from "@storybook/react";
 import PillIcon from "./PillIcon";
 const text = "Los Angeles";
-const backgroundColor = "orange";
+// const backgroundColor = "#FB836F";
+
 storiesOf("PillIcon", module).add("Default", () => (
-  <PillIcon text={text} backgroundColor={backgroundColor} />
+  <PillIcon text={text} backgroundColor={"yellow"} />
 ));
