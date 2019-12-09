827d8a41327059d0f4d396da83cff195d92a1c0f
karenheyn <karenheyn@gmail.com>
2019-12-09 15:50:04 -0500

    m

--- client/src/components/BookTripCard/BookTripCard.js
+++ client/src/components/BookTripCard/BookTripCard.js
@@ -1,39 +1,13 @@
-
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import "./BookTripCard.css";
 
+const BookTripCard = props => {
+  // Default Class to apply to Component
+  let classList = `InteractionCard`;
 
-// Class Based React Component
-class BookTripCard extends Component{
-  constructor(props){
-    super(props);
-    console.log(props);
-
-    // Default CSS class to apply to the Component
-    this.state = {
-      classList: "BookTripCard"
-    };
-  }
-
-
-  // Runs after Component is loaded in the broswer
-  componentDidMount(){}
-
-
-  // Runs after a component has been updated
-  componentDidUpdate(){}
-
-
-  // Runs right before a component is removed from the DOM
-  componentWillUnmount(){}
-
-  render(){
-    return(
-      <div className={this.state.classList}>
-        BookTripCard
-      </div>
-    );
-  }
-}
+  return <div className={classList}
+   <LocationInfoCard />
+   <LocationInfoCard /></div>;
+};
 
 export default BookTripCard;
