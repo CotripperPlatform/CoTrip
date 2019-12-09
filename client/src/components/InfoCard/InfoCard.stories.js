262f64cabf71fb4e2e38697e55d5ba3f7c0ba509
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-09 15:50:17 -0500

    added many examples for use

--- client/src/components/InfoCard/InfoCard.stories.js
+++ client/src/components/InfoCard/InfoCard.stories.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import InfoCard from "./InfoCard";
-import image from "../../../../assets/images/card_medium1.png";
+import image from "../../../../assets/images/card_medium1.png"; //here for example
 
 storiesOf("InfoCard", module)
   .add("Info Card, Purple", () => <InfoCard variant="purple" size="large" />)
   .add("Info Card, Purple, Small", () => <InfoCard variant="purple" size="small" />)
   .add("Info Card, Pink", () => <InfoCard variant="pink" size="large" />)
   .add("Info Card, Pink, Small", () => <InfoCard variant="pink" size="small" />)
-  .add("Info Card, Picture BG", () => <InfoCard variant="picture" size="medium" picture={image} />);
+  .add("Info Card, Blank", () => <InfoCard size="large" />)
+  .add("Info Card, Blank, Small", () => <InfoCard size="small" />);
