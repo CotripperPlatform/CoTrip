1c3492273167ed53c1f2de04eded78708d26b1a5
DallasLind <mcgraw.dallas@gmail.com>
2019-12-09 15:59:27 -0500

    Most of the way done

---
+++ client/src/components/ForumComment/ForumComment.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import "./ForumComment.css";
+import  '../../App.css';
+import img from "../../Assets/mom.png";
+
+//Function based React Component
+const ForumComment = (props) => {
+
+    let classList = `ForumComment`;
+
+    return(
+        <div className={classList}>
+        <img className="profile" src={props.img}></img>
+        <p className="momName">{props.momName}</p>
+          <p className="comment">
+          {props.comment} 
+          </p>
+          <p className="date">
+          {props.date} 
+          </p>
+          <p className="likes">
+              Likes
+          </p>
+          <p className="replies">
+              Replies
+          </p>
+        </div>
+      );
+    }
+
+
+
+
+
+
+
+
+
+export default ForumComment;
