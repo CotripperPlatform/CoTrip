a36125e9278389a9f7bc654d974bdc4cf2363339
Tissa Khosla <tissa.music@gmail.com>
2019-12-09 16:29:55 -0500

    updated CSS class to use BEM and changed conditional if statements to function with the css classnames as opposed to being if statements

--- client/src/components/MediaCard/MediaCard.js
+++ client/src/components/MediaCard/MediaCard.js
@@ -13,26 +13,11 @@ const MediaCard = (props) => {
     backgroundSize: 'cover',
   }
 
-  if (props.small) {
-    styles.height = '220px'
-    styles.width = '220px'
-  } else if (props.medium) {
-    styles.height = '248px'
-    styles.width = '248px'
-  } else if (props.large) {
-    styles.height = '300px'
-    styles.width = '300px'
-  } else {
-    styles.height = '275px'
-    styles.width = '275px'
-  }
-  
-
   return (
   
-    <Link className='media-card' to={props.to} style={styles}>
-      <footer className='media-footer'>
-        <p className='footer-text'>{props.footerText}</p>
+    <Link className={`MediaCard MediaCard--${props.size}`} to={props.to} style={styles}>
+      <footer className='MediaCard__footer'>
+        <p className='MediaCard__footer-text'>{props.footerText}</p>
       </footer>
     </Link>
   );
