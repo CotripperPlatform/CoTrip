cab743fe57a85e3cc329b35398543affebaabdea
William Chrapcynski <wchrapcynski@gmail.com>
2019-12-09 14:01:36 -0500

    Setup of Navbar molecular component

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import "./Navbar.css";
+
+// Function based React Component
+const Navbar = (props) => {
+
+  // Default Class to apply to Component
+  let classList = `Navbar`;
+
+
+  return(
+    <div className={classList}>
+      Navbar
+    </div>
+  );
+}
+
+export default Navbar;
