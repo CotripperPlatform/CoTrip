bda82aaf82a0fef01e0871853e87f6148552df27
Tissa Khosla <tissa.music@gmail.com>
2019-12-09 13:06:19 -0500

    added diefferent size possibilities

--- client/src/components/MediaCard/MediaCard.js
+++ client/src/components/MediaCard/MediaCard.js
@@ -3,25 +3,37 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import "./MediaCard.css";
 
-
 // Function based React Component
 const MediaCard = (props) => {
 
   // Default Class to apply to Component
+
   let styles = {
-    height: '220px',
-    width: '220px',
     backgroundImage: `url(${props.imageSrc})`,
     backgroundSize: 'cover',
     display: 'flex',
     flexDirection: 'column-reverse'
   }
+
+  if (props.small) {
+    styles.height = '220px'
+    styles.width = '220px'
+  } else if (props.medium) {
+    styles.height = '248px'
+    styles.width = '248px'
+  } else if (props.large) {
+    styles.height = '300px'
+    styles.width = '300px'
+  } else {
+    styles.height = '275px'
+    styles.width = '275px'
+  }
+  
   
   let userPath = `/User/--usernameid--`
-
-  console.log(props.imageSrc)
   return (
-    <Link className='media-card'to={userPath} style={styles}>
+  
+    <Link className='media-card' to={userPath} style={styles}>
       <footer className='media-footer'>
         <p className='footer-text'>Posted By: {props.userFirstName}</p>
       </footer>
--- client/src/components/MediaCard/MediaCard.stories.js
+++ client/src/components/MediaCard/MediaCard.stories.js
@@ -8,10 +8,26 @@ import nightSky from '../../../../assets/images/media-card-4.png';
 import waterfall from '../../../../assets/images/media-card-5.png';
 import flight from '../../../../assets/images/media-card-6.png';
 
-storiesOf('Media Cards', module)
-  .add("Books", () => <MediaCard imageSrc={books} userFirstName='User' />)
-  .add("Happiness", () => <MediaCard imageSrc={happiness} userFirstName='User' />)
-  .add("Van", () => <MediaCard imageSrc={van} userFirstName='User' />)
-  .add("Night Sky", () => <MediaCard imageSrc={nightSky} userFirstName='User' />)
-  .add("Waterfall", () => <MediaCard imageSrc={waterfall} userFirstName='User' />)
-  .add("Flight", () => <MediaCard imageSrc={flight} userFirstName='User' />)
+storiesOf('Small Media Cards', module)
+  .add("Books", () => <MediaCard small imageSrc={books} userFirstName='User' />)
+  .add("Happiness", () => <MediaCard small imageSrc={happiness} userFirstName='User' />)
+  .add("Van", () => <MediaCard small imageSrc={van} userFirstName='User' />)
+  .add("Night Sky", () => <MediaCard small imageSrc={nightSky} userFirstName='User' />)
+  .add("Waterfall", () => <MediaCard small imageSrc={waterfall} userFirstName='User' />)
+  .add("Flight", () => <MediaCard small imageSrc={flight} userFirstName='User' />)
+
+storiesOf('Medium Media Cards', module)
+  .add("Books", () => <MediaCard medium imageSrc={books} userFirstName='User' />)
+  .add("Happiness", () => <MediaCard medium imageSrc={happiness} userFirstName='User' />)
+  .add("Van", () => <MediaCard medium imageSrc={van} userFirstName='User' />)
+  .add("Night Sky", () => <MediaCard medium imageSrc={nightSky} userFirstName='User' />)
+  .add("Waterfall", () => <MediaCard medium imageSrc={waterfall} userFirstName='User' />)
+  .add("Flight", () => <MediaCard medium imageSrc={flight} userFirstName='User' />)
+  
+storiesOf('Large Media Cards', module)
+  .add("Books", () => <MediaCard large imageSrc={books} userFirstName='User' />)
+  .add("Happiness", () => <MediaCard large imageSrc={happiness} userFirstName='User' />)
+  .add("Van", () => <MediaCard large imageSrc={van} userFirstName='User' />)
+  .add("Night Sky", () => <MediaCard large imageSrc={nightSky} userFirstName='User' />)
+  .add("Waterfall", () => <MediaCard large imageSrc={waterfall} userFirstName='User' />)
+  .add("Flight", () => <MediaCard large imageSrc={flight} userFirstName='User' />)
