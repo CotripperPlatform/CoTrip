10c0f8392eae08910d87eb7f147a3c3fcabbe476
Tissa Khosla <tissa.music@gmail.com>
2019-12-08 19:11:48 -0500

    pre-pull request commit  - styled component  - adjusted image exports from figma to not be styled  - added box-shadow

--- client/src/components/MediaCard/MediaCard.stories.js
+++ client/src/components/MediaCard/MediaCard.stories.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import MediaCard from './MediaCard';
+import books from '../../../../assets/images/media-card-1.png'
 import happiness from '../../../../assets/images/media-card-2.png';
-import van from '../../../../assets/images/media-card-1.png'
-// import space from '../../../../assets/images/card_medium3.png'
+import van from '../../../../assets/images/media-card-3.png';
+import nightSky from '../../../../assets/images/media-card-4.png';
+import waterfall from '../../../../assets/images/media-card-5.png';
+import flight from '../../../../assets/images/media-card-6.png';
 
 storiesOf('MediaCard', module)
-  .add("Fun and Happiness", () => <MediaCard imageSrc={happiness} userFirstName='User'/>)
-  .add("Van", () => <MediaCard imageSrc={van} userFirstName='User'/>)
-  // .add("Space", () => <MediaCard imageSrc={space} userFirstName='User'/>)
+  .add("Books", () => <MediaCard imageSrc={books} userFirstName='User' />)
+  .add("Happiness", () => <MediaCard imageSrc={happiness} userFirstName='User' />)
+  .add("Van", () => <MediaCard imageSrc={van} userFirstName='User' />)
+  .add("Night Sky", () => <MediaCard imageSrc={nightSky} userFirstName='User' />)
+  .add("Waterfall", () => <MediaCard imageSrc={waterfall} userFirstName='User' />)
+  .add("Flight", () => <MediaCard imageSrc={flight} userFirstName='User' />)
