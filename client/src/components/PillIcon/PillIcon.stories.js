0d45d4c5db8499e02ac0dce423ed71999f9df573
NickMckenney <mckenney2001@gmail.com>
2019-12-09 10:43:25 -0500

    changed the background color from yellow to orange

--- client/src/components/PillIcon/PillIcon.stories.js
+++ client/src/components/PillIcon/PillIcon.stories.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import { storiesOf } from "@storybook/react";
 import PillIcon from "./PillIcon";
 const text = "Los Angeles";
-// const backgroundColor = "#FB836F";
-
 storiesOf("PillIcon", module).add("Default", () => (
-  <PillIcon text={text} backgroundColor={"yellow"} />
+  <PillIcon text={text} backgroundColor={"#fb836f"} />
 ));
