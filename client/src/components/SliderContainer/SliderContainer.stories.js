7f7db1136e430e3825920bb0b1717c6936fcb2b8
Tory Klingenstein <toryklingenstein@gmail.com>
2019-12-09 13:53:12 -0500

    remove slider files

--- client/src/components/SliderContainer/SliderContainer.stories.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import SliderContainer from "./SliderContainer";
-
-storiesOf("SliderContainer", module)
-  .add("Default", () => <SliderContainer />)
-  .add("Slider Container with Props", () => <SliderContainer />);
