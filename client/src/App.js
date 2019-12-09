f4658dbf04830639bd72c122330e0e0d4c9add61
William Chrapcynski <wchrapcynski@gmail.com>
2019-12-09 10:18:04 -0500

    Revert "Revert "Updated css variables with another font that was needed. Also cleaned up App and removed default React setup code.""
    This reverts commit 08b33aa966d72102ab854362490c55d2b264fcf0.

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
+=======
+  
+>>>>>>> parent of 08b33aa... Revert "Updated css variables with another font that was needed. Also cleaned up App and removed default React setup code."
     </div>
   );
 }
