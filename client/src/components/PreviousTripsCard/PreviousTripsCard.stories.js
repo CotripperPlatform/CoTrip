36ade64bfa33e04759bff4b067154514975c87d5
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-10 12:33:02 -0500

    removed arrow link from no data version

--- client/src/components/PreviousTripsCard/PreviousTripsCard.stories.js
+++ client/src/components/PreviousTripsCard/PreviousTripsCard.stories.js
@@ -14,10 +14,7 @@ storiesOf("PreviousTripsCard", module)
     </PreviousTripsCard>
   ))
   .add("Default, No Trips", () => (
-    <PreviousTripsCard
-      trips={false}
-      link="https://www.figma.com/file/ggst6OoJWkwaV2DGq4nBzA/CoTripper?node-id=0%3A1"
-    >
+    <PreviousTripsCard trips={false}>
       <h1>Previous Trips</h1>
       <h2>None with CoTripper yet!</h2>
       <a href="/book">Book a Trip Now!</a>
