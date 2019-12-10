0b8b6881c2a615861d084229d9a8fb60410d2e2c
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-10 11:05:06 -0500

    added explicit class names

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -4,7 +4,7 @@ import "./Card.css";
 const Card = props => {
   let { size = "large", color, picture, alt } = props;
   return (
-    <div className={`info-card-container info-card-${color} info-card-${size}`}>
+    <div className={`Card Card_${color} Card_${size}`}>
       {picture ? <img src={picture} alt={alt}></img> : null}
       {props.children}
     </div>
