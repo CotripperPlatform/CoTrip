c4df13cf29d277c190fda23aaaf73e92e472e614
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-10 11:02:29 -0500

    changed file names to card, added alt text as prop

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,11 +1,11 @@
 import React from "react";
-import "./InfoCard.css";
+import "./Card.css";
 
 const Card = props => {
-  let { size = "large", color, picture } = props;
+  let { size = "large", color, picture, alt } = props;
   return (
     <div className={`info-card-container info-card-${color} info-card-${size}`}>
-      {picture ? <img src={picture} alt="whoops"></img> : null}
+      {picture ? <img src={picture} alt={alt}></img> : null}
       {props.children}
     </div>
   );
