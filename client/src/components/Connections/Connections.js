52cba698895055d4ac00efb76c7aec366544f6a3
Tissa Khosla <tissa.music@gmail.com>
2019-12-10 11:29:54 -0500

    init commit on component connections NEXT STEPS:  - finish jennas user pic component  - implement it into Connections

---
+++ client/src/components/Connections/Connections.js
@@ -0,0 +1,39 @@
+
+import React, {Component} from 'react';
+import "./Connections.css";
+
+
+// Class Based React Component
+class Connections extends Component{
+  constructor(props) {
+    super(props);
+    console.log(props);
+
+    // Default CSS class to apply to the Component
+    this.state = {
+      classList: "Connections"
+    };
+  }
+
+
+  // Runs after Component is loaded in the broswer
+  componentDidMount(){}
+
+
+  // Runs after a component has been updated
+  componentDidUpdate(){}
+
+
+  // Runs right before a component is removed from the DOM
+  componentWillUnmount(){}
+
+  render(){
+    return(
+      <div className={this.state.classList}>
+        Connections
+      </div>
+    );
+  }
+}
+
+export default Connections;
---
+++ client/src/components/Connections/Connections.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Connections from './Connections';
+
+storiesOf('Connections', module)
+.add("User Connections", () => <Connections userViewing={true} />)
+.add("Her Connections", () => <Connections userViewing={false} />)
