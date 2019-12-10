818a3545ea24417eb7dc3f56126421bb8ee623d1
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-12-10 12:08:45 -0500

    added stories for default and example, built component, and styled

--- client/src/components/UpcomingEventsCard/UpcomingEventsCard.js
+++ client/src/components/UpcomingEventsCard/UpcomingEventsCard.js
@@ -1,17 +1,15 @@
-
 import React from 'react';
 import "./UpcomingEventsCard.css";
+// import { prependOnceListener } from 'cluster';
 
-// Function based React Component
 const UpcomingEventsCard = (props) => {
 
-  // Default Class to apply to Component
-  let classList = `UpcomingEventsCard`;
-
-
   return(
-    <div className={classList}>
-      UpcomingEventsCard
+    <div className="UpcomingEventsCard">
+      <span className="UpcomingEventsCard__name">{props.name}</span>
+      <span className="UpcomingEventsCard__date">Date: {props.date}</span>
+      <span className="UpcomingEventsCard__time">Time: {props.time}</span>
+      <span className="UpcomingEventsCard__location">Location: {props.location}</span>
     </div>
   );
 }
--- client/src/components/UpcomingEventsCard/UpcomingEventsCard.stories.js
+++ client/src/components/UpcomingEventsCard/UpcomingEventsCard.stories.js
@@ -3,7 +3,15 @@ import { storiesOf } from '@storybook/react';
 import UpcomingEventsCard from './UpcomingEventsCard';
 
 storiesOf('UpcomingEventsCard', module)
-  .add("Default", () => (
-    <UpcomingEventsCard />
-  )
-)
+  .add("Default", () => <UpcomingEventsCard 
+    name="Upcoming Event"
+    date="Month Day"
+    time="Time"
+    location="City, State"
+  />)
+  .add("Example", () => <UpcomingEventsCard 
+    name="DC Mom Playdates"
+    date="August 24th"
+    time="4:30pm"
+    location="Washington, DC"
+  />)
