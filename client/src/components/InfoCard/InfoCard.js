6d3385d3cb6b3d5f8462ff449d720992ceb2f675
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-09 19:26:44 -0500

    removed comments

--- client/src/components/InfoCard/InfoCard.js
+++ client/src/components/InfoCard/InfoCard.js
@@ -1,10 +1,8 @@
 import React from "react";
 import "./InfoCard.css";
 
-// Function based React Component
 const Card = props => {
   let { size = "large", color, picture } = props;
-  // Default Class to apply to Component
   return (
     <div className={`info-card-container info-card-${color} info-card-${size}`}>
       {picture ? <img src={picture} alt="whoops"></img> : null}
