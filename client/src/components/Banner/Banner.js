c6f86e2a7b25dbd039353bce50eaaf66c65314da
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-12-10 12:19:11 -0500

    understood and figured out how to render props.children and change the background using props

--- client/src/components/Banner/Banner.js
+++ client/src/components/Banner/Banner.js
@@ -2,8 +2,14 @@ import React from "react";
 import "./Banner.css";
 
 const Banner = props => {
-  let classlist = `banner banner-${props.bannerType}`;
-  return <div className={classlist}>{props.children}</div>;
+  let classlist = "banner";
+  return <div className={classlist} style={{backgroundImage: `${props.bannerType}`}}>{props.children}</div>;
 };
 
 export default Banner;
+
+
+// const Banner = props => {
+//   let classlist = `banner banner-${props.bannerType}`;
+//   return <div className={classlist}>{props.children}</div>;
+// };
--- client/src/components/Banner/Banner.stories.js
+++ client/src/components/Banner/Banner.stories.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import Banner from "./Banner";
+import { checkPropTypes } from "prop-types";
 
 storiesOf("Banner", module)
-  .add("Home Banner", () => <Banner bannerType="home" />)
-  .add("Community Banner", () => <Banner bannerType="community" />)
-  .add("Profile Banner", () => <Banner bannerType="profile" />);
+  .add("Home Banner", () => <Banner 
+        bannerType='var(--home-banner)' 
+        />)
+
+  .add("Home Banner with text", () => 
+  <Banner bannerType='var(--home-banner)'>
+    <h1>xyx</h1>
+  </Banner>)
+
+  .add("Community Banner", () => <Banner bannerType='var(--community-banner)' />)
+  
+  .add("Profile Banner", () => <Banner bannerType='var(--profile-banner)' />);
