44fd3b57997a6d49f68f74cb90d181556366cb9a
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-12-10 12:13:17 -0500

    added react router link so that the card can link to related page

--- client/src/components/UpcomingEventsCard/UpcomingEventsCard.js
+++ client/src/components/UpcomingEventsCard/UpcomingEventsCard.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import "./UpcomingEventsCard.css";
 // import { prependOnceListener } from 'cluster';
+import { Link } from "react-router-dom";
 
 const UpcomingEventsCard = (props) => {
 
   return(
-    <div className="UpcomingEventsCard">
+    <Link className="UpcomingEventsCard" to="#">
       <span className="UpcomingEventsCard__name">{props.name}</span>
       <span className="UpcomingEventsCard__date">Date: {props.date}</span>
       <span className="UpcomingEventsCard__time">Time: {props.time}</span>
       <span className="UpcomingEventsCard__location">Location: {props.location}</span>
-    </div>
+    </Link>
   );
 }
 
