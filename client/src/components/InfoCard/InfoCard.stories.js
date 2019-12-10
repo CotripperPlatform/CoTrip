ec2b4789cf7342dff15c29e52284b063c52cb644
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-09 19:25:35 -0500

    added photo size medium, thats the only card in the mockup that uses this sizing though.

--- client/src/components/InfoCard/InfoCard.stories.js
+++ client/src/components/InfoCard/InfoCard.stories.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import InfoCard from "./InfoCard";
-import image from "../../../../assets/images/card_medium1.png"; //here for example
+import image from "../../../../assets/images/card_small3.png"; //here for example
 
 storiesOf("InfoCard", module)
   .add("Info Card, Purple", () => <InfoCard color="purple" size="large" />)
@@ -10,4 +10,4 @@ storiesOf("InfoCard", module)
   .add("Info Card, Pink, Small", () => <InfoCard color="pink" size="small" />)
   .add("Info Card, Blank", () => <InfoCard size="large" />)
   .add("Info Card, Blank, Small", () => <InfoCard size="small" />)
-  .add("Info Card, Picture BG", () => <InfoCard picture={image} size="small" />);
+  .add("Info Card, Picture BG, Large", () => <InfoCard picture={image} size="medium" />);
