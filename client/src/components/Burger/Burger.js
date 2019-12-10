406913e642a71682fd04673e0575e998910f27eb
William Chrapcynski <wchrapcynski@gmail.com>
2019-12-10 15:00:50 -0500

    Burger working as two different stories on SB. It will be activated with a passed prop

--- client/src/components/Burger/Burger.js
+++ client/src/components/Burger/Burger.js
@@ -1,55 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Burger.css";
 
-// Class Based React Component
-class Burger extends Component {
-  constructor(props) {
-    super(props);
-    console.log(props);
+const Burger = (props) => {
+  return (
+    <div className={`Burger ${props.active && "Burger--active"}`} onClick={props.onClick}>
+      <div className={"Burger__line"}></div>
+      <div className={"Burger__line"}></div>
+      <div className={"Burger__line"}></div>
+    </div>
+  );
+};
 
-    // Default CSS class to apply to the Component
-    this.state = {
-      classList: "Burger",
-      active: false
-    };
-  }
-
-  clicked = () => {
-    this.setState({ active: !this.state.active })
-    // Utilizes a callback in the Navbar to display the menu.
-    // this.props.DisplayMenu()
-  };
-
-  // Runs after Component is loaded in the broswer
-  componentDidMount() {}
-
-  // Runs after a component has been updated
-  componentDidUpdate() {}
-
-  // Runs right before a component is removed from the DOM
-  componentWillUnmount() {}
-
-  render() {
-    console.log(this.state.active);
-    return (
-      <div>
-        <div className={this.state.classList} onClick={this.clicked}>
-          <div className={
-            this.state.active ? "burger-line-one burger-line-one-x" : "burger-line-one"
-          }></div>
-          <div className={
-            this.state.active ? "burger-line-two fade" : "burger-line-two"
-          }></div>
-          <div className={
-            this.state.active ? "burger-line-three burger-line-three-x" : "burger-line-three"
-          }></div>
-        </div>
-        <div>
-          
-        </div>
-      </div>
-    );
-  }
-}
+Burger.defaultProps = {
+  active: false
+};
 
 export default Burger;
