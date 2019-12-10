78ca4065ef9cf53b9c84647f5c292c79b4e66443
Noah Clark <noahbc08@gmail.com>
2019-12-10 10:36:09 -0500

    prettier'd code

--- client/src/components/Pill/Pill.stories.js
+++ client/src/components/Pill/Pill.stories.js
@@ -1,85 +1,77 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import Pill from './Pill';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Pill from "./Pill";
 
-
-function pillClick(val){
-  console.log(val)
+function pillClick(val) {
+  console.log(val);
 }
 
-
-storiesOf('Pill', module)
+storiesOf("Pill", module)
   .add("Onboarding Pill", () => (
     <Pill
-      text={'Traveling'}
+      text={"Traveling"}
       active={false}
-      size={'Large'}
-      color={'Pink'}
+      size={"Large"}
+      color={"Pink"}
       clickCallback={pillClick}
       selectId={0}
-     />
-  )
-)
+    />
+  ))
   .add("Pill with White Heart", () => (
     <Pill
-      text={'Traveling'}
-      size={'Medium'}
-      color={'Pink'}
-      icon={'white'}
+      text={"Traveling"}
+      size={"Medium"}
+      color={"Pink"}
+      icon={"white"}
       clickCallback={pillClick}
       selectId={0}
-     />
-  )
-)
+    />
+  ))
   .add("Pill with Pink Heart Offset", () => (
     <Pill
-      text={'Traveling'}
-      size={'Large'}
-      icon={'pink'}
+      text={"Traveling"}
+      size={"Large"}
+      icon={"pink"}
       iconOffset
       clickCallback={pillClick}
       selectId={0}
-     />
-  )
-)
+    />
+  ))
   .add("Community People Pill (Toggle)", () => (
     <Pill
-      text={'Traveling'}
-      size={'Large'}
+      text={"Traveling"}
+      size={"Large"}
       active={true}
-      color={'Purple'}
-      icon={'white'}
+      color={"Purple"}
+      icon={"white"}
       iconOffset
       shadow
       clickCallback={pillClick}
       selectId={0}
-     />
-  )
-)
+    />
+  ))
   .add("Hashtag Border Pill Purple", () => (
     <Pill
-      text={'#hashtags'}
-      size={'Large'}
-      color={'Purple'}
-      icon={'white'}
+      text={"#hashtags"}
+      size={"Large"}
+      color={"Purple"}
+      icon={"white"}
       iconOffset
       shadow
       border
       clickCallback={pillClick}
       selectId={0}
-     />
-  )
-)
+    />
+  ))
   .add("Hashtag Border Pill Pink with Heart", () => (
     <Pill
-      text={'#hashtags'}
-      size={'Large'}
-      color={'Pink'}
-      icon={'pink'}
+      text={"#hashtags"}
+      size={"Large"}
+      color={"Pink"}
+      icon={"pink"}
       shadow
       border
       clickCallback={pillClick}
       selectId={0}
-     />
-  )
-)
+    />
+  ));
