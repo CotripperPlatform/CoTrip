cadc61050b25e974f9fb6ea7bc4581ae55fe9aa6
Ren Lucas <llucas314@gmail.com>
2019-12-09 19:29:46 -0500

    added styles to storybook

--- client/src/components/BannerParent/BannerParent.stories.js
+++ client/src/components/BannerParent/BannerParent.stories.js
@@ -2,4 +2,7 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import BannerParent from "./BannerParent";
 
-storiesOf("BannerParent", module).add("Default", () => <BannerParent type={"home"} />);
+storiesOf("BannerParent", module)
+  .add("Home Banner", () => <BannerParent bannerType="home" />)
+  .add("Community Banner", () => <BannerParent bannerType="community" />)
+  .add("Profile Banner", () => <BannerParent bannerType="profile" />);
