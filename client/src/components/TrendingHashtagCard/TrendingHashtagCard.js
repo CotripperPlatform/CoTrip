71e91debc943a13ec9822a87b5460c0e626a29ee
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-09 21:29:49 -0500

    card renders hashtags passed in

--- client/src/components/TrendingHashtagCard/TrendingHashtagCard.js
+++ client/src/components/TrendingHashtagCard/TrendingHashtagCard.js
@@ -10,7 +10,7 @@ const TrendingHashtagCard = props => {
       <div className="hashtag-card-container">
         {props.children}
         <div className="hashtags-container">
-          {data.map(item => {
+          {data.map((item, key) => {
             return <div className="single-hashtag-container">{item}</div>;
           })}
         </div>
