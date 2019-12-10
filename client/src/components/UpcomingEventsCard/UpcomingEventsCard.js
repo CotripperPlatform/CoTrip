98edc7f36a72a45874af0d7ce3639590c5ce4a2f
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-12-10 15:02:12 -0500

    imported Card parent component and passed UpcomingEventsCard as its child

--- client/src/components/UpcomingEventsCard/UpcomingEventsCard.js
+++ client/src/components/UpcomingEventsCard/UpcomingEventsCard.js
@@ -2,16 +2,18 @@ import React from 'react';
 import "./UpcomingEventsCard.css";
 // import { prependOnceListener } from 'cluster';
 import { Link } from "react-router-dom";
+import Card from "../Card/Card";
 
 const UpcomingEventsCard = (props) => {
-
   return(
-    <Link className="UpcomingEventsCard" to="#">
-      <span className="UpcomingEventsCard__name">{props.name}</span>
-      <span className="UpcomingEventsCard__date">Date: {props.date}</span>
-      <span className="UpcomingEventsCard__time">Time: {props.time}</span>
-      <span className="UpcomingEventsCard__location">Location: {props.location}</span>
-    </Link>
+      <Card color="pink" size="small">
+        <Link className="UpcomingEventsCard" to="#">
+          <span className="UpcomingEventsCard__name">{props.name}</span>
+          <span className="UpcomingEventsCard__date">Date: {props.date}</span>
+          <span className="UpcomingEventsCard__time">Time: {props.time}</span>
+          <span className="UpcomingEventsCard__location">Location: {props.location}</span>
+        </Link>
+      </Card>
   );
 }
 
