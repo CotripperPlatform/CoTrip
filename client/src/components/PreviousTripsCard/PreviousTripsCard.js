5dcea3284d19797a7179b3a6e31ca2e567a559f8
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-10 14:56:29 -0500

    destructored props.children

--- client/src/components/PreviousTripsCard/PreviousTripsCard.js
+++ client/src/components/PreviousTripsCard/PreviousTripsCard.js
@@ -5,20 +5,25 @@ import Card from "../Card/Card";
 // Function based React Component
 const PreviousTripsCard = props => {
   let { link, trips } = props;
+  let [title, tripsItemOne, tripsItemTwo] = [
+    props.children[0],
+    props.children[1],
+    props.children[2]
+  ];
 
   return (
     <Card color="purple">
-      <div className="PreviousTripCard__container">{props.children[0]}</div>
+      <div className="PreviousTripCard__container">{title}</div>
       {trips ? (
         <div className="PreviousTripCard__trips">
-          {props.children[1]}
-          {props.children[2]}
+          {tripsItemOne}
+          {tripsItemTwo}
           <a href={link} className="PreviousTripCard__arrow" />
         </div>
       ) : (
         <div className="PreviousTripCard__no-trips">
-          {props.children[1]}
-          {props.children[2]}
+          {tripsItemOne}
+          {tripsItemTwo}
         </div>
       )}
     </Card>
