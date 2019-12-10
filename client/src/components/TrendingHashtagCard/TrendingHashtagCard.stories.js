668b5c9930d337eedf2dfb37d36c34635c4e0186
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-09 21:35:29 -0500

    added an example with no data

--- client/src/components/TrendingHashtagCard/TrendingHashtagCard.stories.js
+++ client/src/components/TrendingHashtagCard/TrendingHashtagCard.stories.js
@@ -2,20 +2,27 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import TrendingHashtagCard from "./TrendingHashtagCard";
 
-storiesOf("TrendingHashtagCard", module).add("Default", () => (
-  <TrendingHashtagCard
-    data={[
-      "#dope",
-      "#torch",
-      "#hawaii",
-      "#kids",
-      "#cats",
-      "#dogs",
-      "#italy",
-      "#starwars",
-      "#babyyoda"
-    ]}
-  >
-    <h1>Trending Hashtags</h1>
-  </TrendingHashtagCard>
-));
+storiesOf("TrendingHashtagCard", module)
+  .add("Default", () => (
+    <TrendingHashtagCard
+      data={[
+        "#dope",
+        "#torch",
+        "#hawaii",
+        "#kids",
+        "#cats",
+        "#dogs",
+        "#italy",
+        "#starwars",
+        "#babyyoda"
+      ]}
+    >
+      <h1>Trending Hashtags</h1>
+    </TrendingHashtagCard>
+  ))
+  .add("Default, No Data", () => (
+    <TrendingHashtagCard data={[]}>
+      <h1>Trending Hashtags</h1>
+      <h2>No Hashtags :(</h2>
+    </TrendingHashtagCard>
+  ));
