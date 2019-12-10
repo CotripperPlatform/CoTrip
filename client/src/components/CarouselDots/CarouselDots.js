d70b33530ddda96ad40a126f5a1b2bde92b9a47e
Noah Clark <noahbc08@gmail.com>
2019-12-10 11:28:20 -0500

    Removed SVG and used basic CSS to create dots

--- client/src/components/CarouselDots/CarouselDots.js
+++ client/src/components/CarouselDots/CarouselDots.js
@@ -5,81 +5,40 @@ import ReactDOM from "react-dom";
 class CarouselDots extends Component {
   constructor(props) {
     super(props);
-    console.log(props);
-
     this.state = {
       classList: "CarouselDots",
-      activeLocation: props.activeLocation,
-      activeFill: "#6A4C93",
-      inactiveFill: "#FAFAFA"
+      activeLocation: props.activeLocation
     };
   }
-  componentDidUpdate(props) {
-    console.log("Carousel Updated");
-  }
 
-  dotClicked = index => {
+  dotClicked = evt => {
+    let index = evt.target.id;
     this.setState({ activeLocation: index }, () => {
-      this.props.onStatusChange("Updated");
+      this.props.onStatusChange(index);
     });
   };
 
   componentWillUnmount() {}
 
-  renderBorderedSvg = (i, active) => {
-    let proportions = 50;
-    let fill = active ? this.state.activeFill : this.state.inactiveFill;
-    return (
-      <svg key={i} height={proportions} width={proportions}>
-        <circle
-          onClick={() => {
-            this.dotClicked(i);
-          }}
-          cx={proportions / 2}
-          cy={proportions / 2}
-          r={proportions / 5}
-          stroke={this.state.activeFill}
-          strokeWidth={2}
-          fill={fill}
-        />
-      </svg>
-    );
-  };
+  generateClassList(active) {
+    let classList = "CarouselDot ";
+    classList = this.props.hasOwnProperty("border")
+      ? (classList += " CarouselDot__Border ")
+      : classList;
+    classList = active ? (classList += "CarouselDot__Active ") : classList;
+    return classList;
+  }
 
-  renderBorderlessSvg = (i, active) => {
-    let proportions = 50;
-    let fill = active ? this.state.activeFill : this.state.inactiveFill;
-    return (
-      <svg key={i} eight={proportions} width={proportions}>
-        <circle
-          onClick={() => {
-            this.dotClicked(i);
-          }}
-          cx={proportions / 2}
-          cy={proportions / 2}
-          r={proportions / 5}
-          fill={fill}
-        />
-      </svg>
-    );
-  };
-
-  generateDots = () => {
-    let dots = [];
-    let { numberOfDots, border } = this.props;
-    let { activeLocation } = this.state;
-    let borderWeight = this.state.borderWeight;
-    for (let i = 1; i <= numberOfDots; i++) {
-      let dot = border
-        ? this.renderBorderedSvg(i, i == activeLocation)
-        : this.renderBorderlessSvg(i, i == activeLocation);
-      dots.push(dot);
-    }
-    return dots.map(val => val);
-  };
 
   render() {
-    let dots = this.generateDots();
+    let numberOfDots = this.props.numberOfDots;
+    let activeLocation = this.state.activeLocation;
+        let dots = [];
+    for (let i = 0; i <= numberOfDots-1; i++) {
+      let dot = <div key={i} id={i} onClick={this.dotClicked} className={this.generateClassList(i==activeLocation)} />
+      dots.push(dot);
+    }
+
     return (
       <div className={this.state.classList} style={this.state.style}>
         {dots}
