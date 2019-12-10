910971876f411635bfcbe5b2170b341782517ead
karenheyn <karenheyn@gmail.com>
2019-12-10 15:50:23 -0500

    added card component"

--- client/src/components/BookTripCard/BookTripCard.js
+++ client/src/components/BookTripCard/BookTripCard.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import "./BookTripCard.css";
+import Card from "../Card/Card";
 
 const BookTripCard = props => {
   // Default Class to apply to Component
   let classList = `InteractionCard`;
 
-  return <div className={classList}
-   <LocationInfoCard />
-   <LocationInfoCard /></div>;
+  return <Card color="pink" size="large" />;
 };
 
 export default BookTripCard;
