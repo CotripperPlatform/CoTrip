e70a3e3d78d33735672fcc545b9c2830c08bb1e3
karenheyn <karenheyn@gmail.com>
2019-12-10 15:21:46 -0500

    finished component

--- client/src/components/InteractionCard/InteractionCard.js
+++ client/src/components/InteractionCard/InteractionCard.js
@@ -1,18 +1,20 @@
 import React from "react";
 import "./InteractionCard.css";
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import Button from "../Button/Button";
 // Function based React Component
+
 const InteractionCard = props => {
   // Default Class to apply to Component
   let classList = `InteractionCard`;
-
+  const handleClick = e => console.log("clicked");
   return (
     <div className={classList}>
       <Link to={props.to}>
         <img className={"image-interaction"} src={props.img}></img>
       </Link>
-      <button className="connect-small" onclick={props.connect}></button>
-      <button className="message-small" onclick={props.message}></button>
+      <Button text="Submit" color="pink" size="small" handleClick={handleClick} />
+      <Button text="Submit" color="outline" size="small" handleClick={handleClick} />
     </div>
   );
 };
--- client/src/components/InteractionCard/InteractionCard.stories.js
+++ client/src/components/InteractionCard/InteractionCard.stories.js
@@ -2,7 +2,7 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import InteractionCard from "./InteractionCard";
 import { checkPropTypes } from "prop-types";
-
+import Image from "../../../../assets/images/profile_large.png";
 storiesOf("InteractionCard", module).add("Default", () => (
-  <InteractionCard to="/route" img="https://www.placecage.com/c/300/300" />
+  <InteractionCard to="/route" img={Image} />
 ));
