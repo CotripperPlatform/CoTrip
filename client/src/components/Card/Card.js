1dca777046172274f3e565103f0dcd858aa940b7
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-10 11:11:08 -0500

    fixed classnames

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -4,7 +4,7 @@ import "./Card.css";
 const Card = props => {
   let { size = "large", color, picture, alt } = props;
   return (
-    <div className={`Card Card_${color} Card_${size}`}>
+    <div className={`Card Card--${color} Card--${size}`}>
       {picture ? <img src={picture} alt={alt}></img> : null}
       {props.children}
     </div>
