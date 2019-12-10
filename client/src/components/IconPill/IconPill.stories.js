029fee1fb7931d29329b07e02151a5b2a04df468
Noah Clark <noahbc08@gmail.com>
2019-12-10 10:38:46 -0500

    removed PillIcon and ButtonSelect

--- client/src/components/IconPill/IconPill.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import IconPill from './IconPill';
-
-storiesOf('IconPill', module)
-  .add("Default", () => (
-    <IconPill />
-  )
-)
