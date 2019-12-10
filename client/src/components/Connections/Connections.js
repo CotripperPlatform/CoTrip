81fc3bf56ca723cdda3c0ad2385addceaa996763
Tissa Khosla <tissa.music@gmail.com>
2019-12-10 12:04:22 -0500

    commit before dev merge into here

--- client/src/components/Connections/Connections.js
+++ client/src/components/Connections/Connections.js
@@ -1,36 +1,46 @@
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import "./Connections.css";
 
 
 // Class Based React Component
-class Connections extends Component{
+class Connections extends Component {
   constructor(props) {
     super(props);
     console.log(props);
 
     // Default CSS class to apply to the Component
     this.state = {
-      classList: "Connections"
+      classList: "Connections",
+      Heading: "Connections"
     };
   }
 
 
   // Runs after Component is loaded in the broswer
-  componentDidMount(){}
+  componentDidMount(){
+    if (this.props.userViewing === true) {
+      this.setState({ Heading: "My Connections"})
+    } else if (this.props.userViewing === false) {
+      this.setState({ Heading: "Her Connections" })
+    } else if (!this.props.userViewing) {
+      this.setState({ Heading: "Members" })
+    }
+  }
+  //Why isn't this working?
 
 
   // Runs after a component has been updated
-  componentDidUpdate(){}
+  componentDidUpdate() { }
 
 
   // Runs right before a component is removed from the DOM
-  componentWillUnmount(){}
+  componentWillUnmount() { }
 
-  render(){
-    return(
+  render() {
+    return (
       <div className={this.state.classList}>
-        Connections
+        <h1 className={`${this.state.classList}__heading`}>{this.state.Heading}</h1>
       </div>
     );
   }
--- client/src/components/Connections/Connections.stories.js
+++ client/src/components/Connections/Connections.stories.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Connections from './Connections';
 
+
+
 storiesOf('Connections', module)
 .add("User Connections", () => <Connections userViewing={true} />)
 .add("Her Connections", () => <Connections userViewing={false} />)
+.add("Members", () => <Connections />)
