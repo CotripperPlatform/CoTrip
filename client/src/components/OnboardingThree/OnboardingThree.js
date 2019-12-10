b6fa1b70bf323bf5ec9846dcc392c8d2ff9f36ff
Ren Lucas <llucas314@gmail.com>
2019-12-09 19:50:45 -0500

    added onboarding component

---
+++ client/src/components/OnboardingThree/OnboardingThree.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import "./OnboardingThree.css";
+
+// Function based React Component
+const OnboardingThree = (props) => {
+
+  // Default Class to apply to Component
+  let classList = `OnboardingThree`;
+
+
+  return(
+    <div className={classList}>
+      OnboardingThree
+    </div>
+  );
+}
+
+export default OnboardingThree;
---
+++ client/src/components/OnboardingThree/OnboardingThree.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import OnboardingThree from './OnboardingThree';
+
+storiesOf('OnboardingThree', module)
+  .add("Default", () => (
+    <OnboardingThree />
+  )
+)
