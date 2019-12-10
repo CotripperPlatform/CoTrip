c3cdf5318d50b2ab0f34028f83fc8f2810e29b4c
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-10 12:03:38 -0500

    added arrow that, can pass in link as prop

--- client/src/components/PreviousTripsCard/PreviousTripsCard.js
+++ client/src/components/PreviousTripsCard/PreviousTripsCard.js
@@ -4,11 +4,18 @@ import Card from "../Card/Card";
 
 // Function based React Component
 const PreviousTripsCard = props => {
-  // Default Class to apply to Component
+  let { link } = props;
+  console.log(link);
 
   return (
     <Card color="purple">
-      <div className="PreviousTripCard_container">{props.children}</div>
+      <div className="PreviousTripCard_container">{props.children[0]}</div>
+      <div className="PreviousTripCard_trips">
+        {props.children[1]}
+        {props.children[2]}
+      </div>
+
+      <a href={link} className="PreviousTripCard_arrow" />
     </Card>
   );
 };
--- client/src/components/PreviousTripsCard/PreviousTripsCard.stories.js
+++ client/src/components/PreviousTripsCard/PreviousTripsCard.stories.js
@@ -3,7 +3,9 @@ import { storiesOf } from "@storybook/react";
 import PreviousTripsCard from "./PreviousTripsCard";
 
 storiesOf("PreviousTripsCard", module).add("Default", () => (
-  <PreviousTripsCard>
+  <PreviousTripsCard link="https://www.figma.com/file/ggst6OoJWkwaV2DGq4nBzA/CoTripper?node-id=0%3A1">
     <h1>Trending Hashtags</h1>
+    <div>1</div>
+    <div>2</div>
   </PreviousTripsCard>
 ));
