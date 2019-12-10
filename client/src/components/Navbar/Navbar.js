4077f7f8a9bbdb7296552d6ebdc5dcf11c5a20b7
William Chrapcynski <wchrapcynski@gmail.com>
2019-12-10 10:02:23 -0500

    Getting code ready for burger

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -7,12 +7,19 @@ const Navbar = (props) => {
 
   // Default Class to apply to Component
   let classList = `Navbar`;
+  let menuShow = false;
 
+  const DisplayMenu = () => {
+    menuShow = !menuShow;
+  }
 
   return (
     <div className={classList}>
       <div className="navbar-logo">
         <Logo clickable to="/route" />
+      </div>
+      <div> 
+        {/* <Burger menuShow="DisplayMenu" /> */}
       </div>
     </div>
   );
