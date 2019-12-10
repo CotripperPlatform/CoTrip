27df770a2d5c204f954c20ab9a23ab2311f69e70
Tory Klingenstein <toryklingenstein@gmail.com>
2019-12-10 15:56:10 -0500

    add profile picture component

--- client/src/components/ForumPost/ForumPost.js
+++ client/src/components/ForumPost/ForumPost.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./ForumPost.css";
 import PostBody from "../PostBody/PostBody.js";
 import Button from "../Button/Button.js";
+import ProfilePicture from "../ProfilePicture/ProfilePicture.js";
+import Image from "../../../../assets/images/profile_large.png";
+import heart from "../../../../assets/images/heart_black.png";
+import comment from "../../../../assets/images/comment_black.png";
 
 // Function based React Component
 const ForumPost = props => {
@@ -11,7 +15,10 @@ const ForumPost = props => {
   return (
     <div className={classList}>
       <div className="ForumPost__top">
-        <div className="ForumPost__face-card-placeholder"></div>
+        <div className="ForumPost__face-card">
+          <ProfilePicture type="extra-small" link="#" image={props.image} />
+          <p className="ForumPost__name">{props.name}</p>
+        </div>
         <PostBody />
       </div>
       <div className="ForumPost__middle">
@@ -32,11 +39,15 @@ const ForumPost = props => {
         </div>
       </div>
       <div className="ForumPost__bottom">
-        <Button />
-        <Button />
+        <Button icon={heart} text="Like" type="like" onClick={props.onClick} />
+        <Button icon={comment} text="Comment" type="comment" onClick={props.onClick} />
       </div>
     </div>
   );
 };
 
+ForumPost.defaultProps = {
+  image: Image,
+  name: "Lexi R."
+};
 export default ForumPost;
