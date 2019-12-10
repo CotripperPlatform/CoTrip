2904e109410f64e4b2c903b5ca17f85eb04d7cdb
Noah Clark <noahbc08@gmail.com>
2019-12-10 12:39:27 -0500

    moved class assignment to render

--- client/src/components/CarouselDots/CarouselDots.js
+++ client/src/components/CarouselDots/CarouselDots.js
@@ -11,42 +11,44 @@ class CarouselDots extends Component {
     };
   }
 
-  dotClicked = evt => {
+  handleClick = evt => {
     let index = evt.target.id;
     this.setState({ activeLocation: index }, () => {
+      console.log("Index is :", index)
       this.props.onStatusChange(index);
     });
   };
 
-  componentWillUnmount() {}
-
   generateClassList(active) {
     let classList = "CarouselDot ";
     classList = this.props.hasOwnProperty("border")
-      ? (classList += " CarouselDot__Border ")
+      ? (classList += " CarouselDot__border ")
       : classList;
-    classList = active ? (classList += "CarouselDot__Active ") : classList;
+    classList = active ? (classList += "CarouselDot--active ") : classList;
     return classList;
   }
 
   render() {
     let numberOfDots = this.props.numberOfDots;
     let activeLocation = this.state.activeLocation;
+    console.log(activeLocation);
     let dots = [];
-    for (let i = 0; i <= numberOfDots - 1; i++) {
+    for (let i = 1; i <= numberOfDots; i++) {
+      let classList = 'CarouselDot ';
+      console.log('Location :', activeLocation, i===activeLocation)
       let dot = (
         <div
           key={i}
           id={i}
-          onClick={this.dotClicked}
-          className={this.generateClassList(i == activeLocation)}
+          onClick={this.handleClick}
+          className={`CarouselDot ${this.props.hasOwnProperty('border') ? ' CarouselDot__border ' : ''} ${i==activeLocation ? ' CarouselDot--active ' : ''} `}
         />
       );
       dots.push(dot);
     }
 
     return (
-      <div className={this.state.classList} style={this.state.style}>
+      <div className={'CarouselDots'} style={this.state.style}>
         {dots}
       </div>
     );
