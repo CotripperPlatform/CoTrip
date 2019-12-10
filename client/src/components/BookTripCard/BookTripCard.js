5279de8aaea998704ba162f217f6654166bbbc85
karenheyn <karenheyn@gmail.com>
2019-12-10 16:10:31 -0500

    working on route

--- client/src/components/BookTripCard/BookTripCard.js
+++ client/src/components/BookTripCard/BookTripCard.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 import "./BookTripCard.css";
 import Card from "../Card/Card";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
 const BookTripCard = props => {
   // Default Class to apply to Component
   let classList = `InteractionCard`;
+  let [title, image1, image2, link] = [
+    props.children[0],
+    props.children[1],
+    props.children[2],
+    props.children[3]
+  ];
 
-  return <Card color="pink" size="large" />;
+  return (
+    <Card color="pink" size="large">
+      {props.children[0]}
+      {props.children[1]}
+      {props.children[2]}
+      <Link to={props.to}>{props.children[3]}</Link>
+    </Card>
+  );
 };
 
 export default BookTripCard;
--- client/src/components/BookTripCard/BookTripCard.stories.js
+++ client/src/components/BookTripCard/BookTripCard.stories.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import BookTripCard from './BookTripCard';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import BookTripCard from "./BookTripCard";
+import { Route } from "@storybook/router";
+import { checkPropTypes } from "prop-types";
 
-storiesOf('BookTripCard', module)
-  .add("Default", () => (
-    <BookTripCard />
-  )
-)
+storiesOf("BookTripCard", module).add("Default", () => (
+  <BookTripCard>
+    <h1></h1>
+    <img src="https://www.placecage.com/200/300"></img>
+    <img src="https://www.placecage.com/200/300"></img>
+    <h3 to="/route">Book a Trip Now!</h3>
+  </BookTripCard>
+));
