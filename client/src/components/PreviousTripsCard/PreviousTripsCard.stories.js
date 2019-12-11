405c74f5c277f8b340ecc5e1978b3d17737b419a
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-11 10:17:08 -0500

    changed story example order

--- client/src/components/PreviousTripsCard/PreviousTripsCard.stories.js
+++ client/src/components/PreviousTripsCard/PreviousTripsCard.stories.js
@@ -3,10 +3,16 @@ import { storiesOf } from "@storybook/react";
 import PreviousTripsCard from "./PreviousTripsCard";
 
 storiesOf("PreviousTripsCard", module)
-  .add("Default", () => (
+  .add("Default, Two Trips", () => (
     <PreviousTripsCard trips={true} link="/">
       <h1>Previous Trips</h1>
       <div>placeholder</div>
+      <div>placeholder</div>
+    </PreviousTripsCard>
+  ))
+  .add("Default, One Trip", () => (
+    <PreviousTripsCard trips={true} link="/">
+      <h1>Previous Trips</h1>
       <div>placeholder</div>
     </PreviousTripsCard>
   ))
@@ -15,11 +21,5 @@ storiesOf("PreviousTripsCard", module)
       <h1>Previous Trips</h1>
       <h2>None with CoTripper yet!</h2>
       <a href="/">Book a Trip Now!</a>
-    </PreviousTripsCard>
-  ))
-  .add("Default, One Trip", () => (
-    <PreviousTripsCard trips={true} link="/">
-      <h1>Previous Trips</h1>
-      <div>placeholder</div>
     </PreviousTripsCard>
   ));
