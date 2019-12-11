dbc347873ebf367f4d7ba4396c1b891cfd9225dc
blake <blakeeh723@gmail.com>
2019-12-11 14:06:12 -0500

    added dummy handleClick function

--- client/src/components/SplashPage/SplashPage.js
+++ client/src/components/SplashPage/SplashPage.js
@@ -3,16 +3,19 @@ import { Link } from 'react-router-dom';
 import Button from '../Button/Button';
 import "./SplashPage.css";
 
+function handleClick() {
+  console.log('login or register button clicked at splash page');
+}
 const SplashPage = () => {
   return (
     <div className='SplashPage'>
       <h1>CoTripper</h1>
       <p>Book trips, meet moms, and experience family travel together!</p>
       <div className='SplashPage__button-margin--small'>
-        <Link to='/login'><Button text="Login" /></Link>
+        <Link to='/login'><Button text="Login" handleClick={handleClick}/></Link>
       </div>
       <div className='SplashPage__button-margin'>
-        <Link to='/register'><Button text="Register" color="purple" /></Link>
+        <Link to='/register'><Button text="Register" color="purple" handleClick={handleClick}/></Link>
       </div>
     </div>
   );
