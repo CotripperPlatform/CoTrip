b79baeb890bec5305fc90b06abf22541f486f0f8
Steven Kim <sskim14@gmail.com>
2019-12-11 00:21:35 -0500

    Combined the two render methods into one conditional rendering method. (To change based on props)

--- client/src/components/ActivityList/ActivityList.js
+++ client/src/components/ActivityList/ActivityList.js
@@ -10,36 +10,33 @@ class ActivityList extends Component {
   }
 
   render() {
-    const renderMomActivities = () => {
-      const momActivities = this.props.momActivityList.map(activity => {
-        return (
-          <div className="ActivityList__activity ActivityList__mom">
-            <span className="ActivityList__activities">{activity}</span>
-          </div>
-        );
-      });
-      return <div className="ActivityList__activity-container">{momActivities}</div>;
+    const renderActivities = () => {
+      if (this.props.type === "mom") {
+        const momActivities = this.props.momActivityList.map((activity, index) => {
+          return (
+            <div key={index} className="ActivityList__activity ActivityList__mom">
+              <span className="ActivityList__activities">{activity}</span>
+            </div>
+          );
+        });
+        return <div className="ActivityList__activity-container">{momActivities}</div>;
+      } else if (this.props.type === "kids") {
+        const kidsActivities = this.props.kidsActivityList.map((activity, index) => {
+          return (
+            <div key={index} className="ActivityList__activity ActivityList__kids">
+              <span className="ActivityList__activities">{activity}</span>
+            </div>
+          );
+        });
+        return <div className="ActivityList__activity-container">{kidsActivities}</div>;
+      }
     };
 
-    const renderKidsActivities = () => {
-      const kidsActivities = this.props.kidsActivityList.map(activity => {
-        return (
-          <div className="ActivityList__activity ActivityList__kids">
-            <span className="ActivityList__activities">{activity}</span>
-          </div>
-        );
-      });
-      return <div className="ActivityList__activity-container">{kidsActivities}</div>;
-    };
     return (
       <div className={this.state.classList}>
         <div className="ActivityList__activities-container">
-          <h1 className="ActivityList__activity-header">Activities for Mom</h1>
-          {renderMomActivities()}
-        </div>
-        <div className="ActivityList__activities-container">
-          <h1 className="ActivityList__activity-header">Activities for Mom & Kids</h1>
-          {renderKidsActivities()}
+          <h1 className="ActivityList__activity-header">{this.props.label}</h1>
+          {renderActivities()}
         </div>
       </div>
     );
