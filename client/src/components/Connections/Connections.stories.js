45ae34c723c832f8c97ca3e73750043508174b44
Tissa Khosla <tissa.music@gmail.com>
2019-12-11 11:43:26 -0500

    entered users into an array and passed them as props via slice  - Next steps: change from class component to function component

--- client/src/components/Connections/Connections.stories.js
+++ client/src/components/Connections/Connections.stories.js
@@ -5,66 +5,67 @@ import pic1 from '../../../../assets/images/card_profile1.png'
 import pic2 from '../../../../assets/images/card_profile2.png'
 import pic3 from '../../../../assets/images/card_profile3.png'
 
-let user1 = {
-  userId: 1,
-  userFirstName: 'Paula',
-  userSurname: 'Bannerman',
-  userPic: pic1
-}
-let user2 = {
-  userId: 2,
-  userFirstName: 'Jack',
-  userSurname: 'Johnson',
-  userPic: pic2
-}
-let user3 = {
-  userId: 3,
-  userFirstName: 'Jenny',
-  userSurname: 'Jones',
-  userPic: pic3
-}
-let user4 = {
-  userId: 4,
-  userFirstName: 'Joan',
-  userSurname: 'Rivers',
-  userPic: pic1
-}
-let user5 = {
-  userId: 5,
-  userFirstName: 'Freddy',
-  userSurname: 'Mercury',
-  userPic: pic2
-}
-let user6 = {
-  userId: 5,
-  userFirstName: 'Leslie',
-  userSurname: 'Knope',
-  userPic: pic3
-}
-let user7 = {
-  userId: 7,
-  userFirstName: 'Frank',
-  userSurname: 'Ocean',
-  userPic: pic1
-}
+let testUsers =
+  [{
+    userId: 1,
+    userFirstName: 'Paula',
+    userSurname: 'Bannerman',
+    userPic: pic1
+  },
+  {
+    userId: 2,
+    userFirstName: 'Jack',
+    userSurname: 'Johnson',
+    userPic: pic2
+  },
+  {
+    userId: 3,
+    userFirstName: 'Jenny',
+    userSurname: 'Jones',
+    userPic: pic3
+  },
+  {
+    userId: 4,
+    userFirstName: 'Joan',
+    userSurname: 'Rivers',
+    userPic: pic1
+  },
+  {
+    userId: 5,
+    userFirstName: 'Freddy',
+    userSurname: 'Mercury',
+    userPic: pic2
+  },
+  {
+    userId: 5,
+    userFirstName: 'Leslie',
+    userSurname: 'Knope',
+    userPic: pic3
+  },
+  {
+    userId: 7,
+    userFirstName: 'Frank',
+    userSurname: 'Ocean',
+    userPic: pic1
+  }]
 
 storiesOf('Connections', module)
   .add("User Connections", () =>
     <Connections
       to='User/userid/connections'
       userViewing={true}
-      users={[user1, user2, user3, user4, user5, user6, user7]}
+      users={testUsers}
       extraUsers="See More">
     </Connections>)
   .add("Her Connections", () =>
     <Connections
       to='User/userid/connections'
       userViewing={false}
-      users={[user5, user1, user3]}
+      users={testUsers.slice(0, 3)}
       extraUsers='See More'>
     </Connections>)
   .add("Members", () =>
     <Connections to='User/userid/connections'
-      users={[user7, user4, user2, user1, user5]}
+      users={testUsers.slice(0, 6)}
       extraUsers="View All"
     />)
