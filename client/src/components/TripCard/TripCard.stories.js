49d3053d25cbe7953cb51faf9271d7b4c046b5b0
Ben Miller <benbmiller123@gmail.com>
2019-12-11 09:47:27 -0500

    got background image to work

--- client/src/components/TripCard/TripCard.stories.js
+++ client/src/components/TripCard/TripCard.stories.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import TripCard from "./TripCard";
+import example1 from "../../../../assets/images/media-card-1.png";
 
-storiesOf("TripCard", module).add("Default", () => (
-  <TripCard path="https://wallpaperaccess.com/full/144067.jpg" location="Hawaii" date="May 2020" />
-));
+storiesOf("TripCard", module)
+  .add("Default", () => (
+    <TripCard
+      path="https://wallpaperaccess.com/full/144067.jpg"
+      location="Hawaii"
+      date="May 2020"
+    />
+  ))
+  .add("Puerto Rico", () => <TripCard path={example1} location="Puerto Rico" date="April 2019" />);
