553d4638fc9788114319cf49a6ff267e91b621ad
Tory Klingenstein <toryklingenstein@gmail.com>
2019-12-11 12:18:49 -0500

    make component functional and update asset paths

--- client/src/components/ForumPost/ForumPost.js
+++ client/src/components/ForumPost/ForumPost.js
@@ -1,77 +1,70 @@
-import React, { Component } from "react";
+import React from "react";
 import "./ForumPost.css";
 import PostBody from "../PostBody/PostBody.js";
 import Button from "../Button/Button.js";
 import ProfilePicture from "../ProfilePicture/ProfilePicture.js";
-import Image from "../../../../assets/images/profile_large.png";
-import heart from "../../../../assets/images/heart_black.png";
-import comment from "../../../../assets/images/comment_black.png";
+import Image from "../../assets/images/profile_large.png";
+import heart from "../../assets/images/heart_black.png";
+import comment from "../../assets/images/comment_black.png";
 import Pill from "../Pill/Pill.js";
 import PropTypes from "prop-types";
 
-class ForumPost extends Component {
-  render() {
-    return (
-      <div className="ForumPost">
-        <div className="ForumPost__top-middle">
-          <div className="ForumPost__top">
-            <div className="ForumPost__face-card">
-              <ProfilePicture type="small" link={this.props.to} image={this.props.image} />
-              <p className="ForumPost__name">{this.props.name}</p>
-            </div>
-            {this.props.post ? (
-              <PostBody
-                title={this.props.post.title}
-                date={this.props.post.date}
-                time={this.props.post.time}
-                body={this.props.post.body}
-                hashtags={this.props.post.hashtags}
-              />
-            ) : (
-              <PostBody />
-            )}
+export default function ForumPost(props) {
+  return (
+    <div className="ForumPost">
+      <div className="ForumPost__top-middle">
+        <div className="ForumPost__top">
+          <div className="ForumPost__face-card">
+            <ProfilePicture type="small" link={props.to} image={props.image} />
+            <p className="ForumPost__name">{props.name}</p>
           </div>
-          <div className="ForumPost__middle">
-            <div className="ForumPost__topic-pills">
-              {this.props.topics
-                ? this.props.topics.map(topic => (
-                    <Pill
-                      key={topic}
-                      text={topic}
-                      active={false}
-                      size={"small"}
-                      color={"red"}
-                      inactiveColor={"pink"}
-                      onClick={this.props.pillClick}
-                      selectId={0}
-                    />
-                  ))
-                : null}
-            </div>
-            <div className="ForumPost__likes-comments">
-              <p>
-                {this.props.likes}
-                {" Likes"}
-              </p>
-              <p>
-                {this.props.comments}
-                {" Comments"}
-              </p>
-            </div>
-          </div>
+          {props.post ? (
+            <PostBody
+              title={props.post.title}
+              date={props.post.date}
+              time={props.post.time}
+              body={props.post.body}
+              hashtags={props.post.hashtags}
+            />
+          ) : (
+            <PostBody />
+          )}
         </div>
-        <div className="ForumPost__bottom">
-          <Button icon={heart} text="Like" type="like" handleClick={this.props.likeClick} />
-          <Button
-            icon={comment}
-            text="Comment"
-            type="comment"
-            handleClick={this.props.commentClick}
-          />
+        <div className="ForumPost__middle">
+          <div className="ForumPost__topic-pills">
+            {props.topics
+              ? props.topics.map(topic => (
+                  <Pill
+                    key={topic}
+                    text={topic}
+                    active={false}
+                    size={"small"}
+                    color={"red"}
+                    inactiveColor={"pink"}
+                    onClick={props.pillClick}
+                    selectId={0}
+                  />
+                ))
+              : null}
+          </div>
+          <div className="ForumPost__likes-comments">
+            <p>
+              {props.likes}
+              {" Likes"}
+            </p>
+            <p>
+              {props.comments}
+              {" Comments"}
+            </p>
+          </div>
         </div>
       </div>
-    );
-  }
+      <div className="ForumPost__bottom">
+        <Button icon={heart} text="Like" type="like" handleClick={props.likeClick} />
+        <Button icon={comment} text="Comment" type="comment" handleClick={props.commentClick} />
+      </div>
+    </div>
+  );
 }
 
 ForumPost.defaultProps = {
@@ -92,4 +85,3 @@ ForumPost.propTypes = {
   topics: PropTypes.array,
   post: PropTypes.object
 };
-export default ForumPost;
