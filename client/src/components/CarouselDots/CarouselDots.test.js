e156f824fd5d5f51b4a513cecee0fc37997abb9e
Ren Lucas <llucas314@gmail.com>
2019-12-10 21:34:11 -0500

    added tests for carouseldots

--- client/src/components/CarouselDots/CarouselDots.test.js
+++ client/src/components/CarouselDots/CarouselDots.test.js
@@ -1,16 +1,42 @@
 import React from "react";
-import { shallow, configure } from "enzyme";
+import { shallow } from "enzyme";
 import CarouselDots from "./CarouselDots.js";
-import Adapter from "enzyme-adapter-react-16";
-configure({ adapter: new Adapter() });
+import { findByTestAttribute } from "../../../../utils/utlis";
 
-// We will describe a block of tests
+const setUp = (props = {}) => {
+  const component = shallow(<CarouselDots {...props} />);
+  return component;
+};
 describe("CarouselDots component", () => {
-  // we will write one individual test
-  it("should render as expected", () => {
-    // Shallow rendering renders a component without rendering any of its children
-    const component = shallow(<CarouselDots />);
-    // We create an assertion within the test that checks if our component renders our name prop
-    // expect(component.contains('Your name')).toBe(true)
+  describe("Have props", () => {
+    let wrapper;
+    beforeEach(() => {
+      const props = {
+        numberOfDots: 3,
+        activeLocation: "Test Active",
+        border: true,
+        onStatusChange: "Test Change Function"
+      };
+      wrapper = setUp(props);
+    });
+    it("Should render without errors", () => {
+      const component = findByTestAttribute(wrapper, "CarouselDots");
+      expect(component.length).toBe(1);
+    });
+    it("Should render dots", () => {
+      const component = findByTestAttribute(wrapper, "dot");
+      expect(component.length).toBe(3);
+    });
+  });
+
+  describe("Have NO props", () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = setUp();
+    });
+    it("Should not render", () => {
+      const component = findByTestAttribute(wrapper, "dot");
+      expect(component.length).toBe(0);
+    });
   });
 });
