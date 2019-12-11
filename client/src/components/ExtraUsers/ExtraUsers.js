ca3f67828a8876b8b5a9cd55d8c4e1c0404f9771
Tissa Khosla <tissa.music@gmail.com>
2019-12-11 11:08:32 -0500

    updated from class component to function component  - deleted redundant styling in the Link tag (it was already in the CSS)  - removed console.logs

--- client/src/components/ExtraUsers/ExtraUsers.js
+++ client/src/components/ExtraUsers/ExtraUsers.js
@@ -1,57 +1,33 @@
-
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
 import "./ExtraUsers.css";
 
 
 // Class Based React Component
-class ExtraUsers extends Component {
-  constructor(props) {
-    super(props);
-    console.log(props);
+function ExtraUsers(props) {
+  let extraUsersValue = null
 
-    // Default CSS class to apply to the Component
-    this.state = {
-      classList: "ExtraUsers",
-      extraUsersValue: null
-    };
-
-    this.howManyMore = this.howManyMore.bind(this)
-  }
-
-  howManyMore(theArray) {
+  function howManyMore(theArray) {
     if (theArray.length > 4) {
       let extraValue = theArray.length - 4
       return extraValue
     }
   }
-  // Runs after Component is loaded in the broswer
-  componentDidMount() {
-    this.setState({
-      extraUsersValue: this.howManyMore(this.props.users)
-    })
-  }
 
-  // Runs after a component has been updated
-  componentDidUpdate() { }
+  extraUsersValue = howManyMore(props.users)
 
-
-  // Runs right before a component is removed from the DOM
-  componentWillUnmount() { }
-
-  render() {
-    return (
-      <div className='ExtraUsers'>
-        <Link style={{textDecoration: 'none'}} to={this.props.to} className='ExtraUsers__text'>
-          +{this.state.extraUsersValue}
-        </Link>
-      </div>
-    );
-  }
+  return (
+    <div className='ExtraUsers'>
+      <Link to={props.to} className='ExtraUsers__text'>
+        +{extraUsersValue}
+      </Link>
+    </div>
+  );
 }
 
-ExtraUsers.defaultPropsm = {
+ExtraUsers.defaultProps = {
   to: 'PATH',
-  users: [1,2,3,4,5,6,7,8,9,0]
+  users: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 }
+
 export default ExtraUsers;
