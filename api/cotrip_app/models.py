16e8e5c33c6acfc2fda82c410c5fccc7b1924145
Joseph_Tully <tully4school@gmail.com>
2019-12-11 16:05:23 -0500

    created the user model in cotrip_app folder. added email, password, and profile

--- api/cotrip_app/models.py
+++ api/cotrip_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=100)
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name='user')
+
+    def __str__(self):
+        # return super().__str__()
+        return self.email
