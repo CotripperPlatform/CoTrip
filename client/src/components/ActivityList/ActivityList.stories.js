fb40ba202c84c079e5c062ebd314dd12a9e7c562
Steven Kim <sskim14@gmail.com>
2019-12-10 23:55:59 -0500

    Added sample arrays to be passed as props to the  component.

--- client/src/components/ActivityList/ActivityList.stories.js
+++ client/src/components/ActivityList/ActivityList.stories.js
@@ -2,4 +2,16 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import ActivityList from "./ActivityList";
 
-storiesOf("ActivityList", module).add("Default", () => <ActivityList />);
+let momActivityList = ["Spa", "Wine Nights", "Brunch with Other Moms"];
+let kidsActivityList = [
+  "Snorkeling",
+  "Hiking",
+  "Island Hopping",
+  "Hawaiian Luaus",
+  "Food Markets",
+  "Beaches"
+];
+
+storiesOf("ActivityList", module).add("Default", () => (
+  <ActivityList momActivityList={momActivityList} kidsActivityList={kidsActivityList} />
+));
