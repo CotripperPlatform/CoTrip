17946e728bd7ba83ad676d32d1d4a90a28d44637
karenheyn <karenheyn@gmail.com>
2019-12-11 12:33:32 -0500

    fiinished

--- client/src/components/SignUpAd/SignUpAd.js
+++ client/src/components/SignUpAd/SignUpAd.js
@@ -1,20 +1,22 @@
 import React from "react";
 import "./SignUpAd.css";
 import InputTextField from "../InputTextField/InputTextField";
+import Button from "../Button/Button";
 // Function based React Component
 const SignUpAd = props => {
   // Default Class to apply to Component
-  let classList = `SignUpAd`;
+  let [heading, subheading] = [props.children[0], props.children[1]];
 
   return (
-    <div className={classList}>
-      <h3>{props.head}</h3>
-      <h5>{props.subhead}</h5>
+    <div className="SignUpAd">
+      <h3>{heading}</h3>
+      <h5>{subheading}</h5>
       <div className="inputs">
         <InputTextField type="text" variation="small" name="first name" placeholder="First Name" />
         <InputTextField type="text" variation="small" name="last name" placeholder="Last Name" />
         <InputTextField type="text" variation="small" name="email" placeholder="Email" />
       </div>
+      <Button text="Join the Tribe" color="pink" onClick={props.handleClick} />
     </div>
   );
 };
--- client/src/components/SignUpAd/SignUpAd.stories.js
+++ client/src/components/SignUpAd/SignUpAd.stories.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import SignUpAd from './SignUpAd';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import SignUpAd from "./SignUpAd";
+import { tsPropertySignature } from "@babel/types";
 
-storiesOf('SignUpAd', module)
-  .add("Default", () => (
-    <SignUpAd />
-  )
-)
+const handleClick = e => console.log("clicked");
+
+storiesOf("SignUpAd", module).add("Default", () => (
+  <SignUpAd onClick={handleClick}>
+    <h2>Never Miss a Trip</h2>
+    <h3>Sign up with your email address to recieve news and updates</h3>
+  </SignUpAd>
+));
