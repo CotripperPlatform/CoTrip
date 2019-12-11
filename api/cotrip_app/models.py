63f9951bfcd978b5686fe0644a5326b6ea25363b
Ben Miller <benbmiller123@gmail.com>
2019-12-11 16:35:59 -0500

    added city model

--- api/cotrip_app/models.py
+++ api/cotrip_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class City(models.Model):
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.city + ", " + self.country 
