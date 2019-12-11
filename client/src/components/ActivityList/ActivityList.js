df559c2c1bb3746e33bfd30a59e8273160afc9b7
Steven Kim <sskim14@gmail.com>
2019-12-10 19:59:03 -0500

    Moved function declrations from outside to inside render().

--- client/src/components/ActivityList/ActivityList.js
+++ client/src/components/ActivityList/ActivityList.js
@@ -4,8 +4,14 @@ import "./ActivityList.css";
 class ActivityList extends Component {
   constructor(props) {
     super(props);
-    this.momActivityList = ["Spa", "Wine Nights", "Brunch with Other Moms"];
-    this.kidsActivityList = [
+    this.state = {
+      classList: "ActivityList"
+    };
+  }
+
+  render() {
+    let momActivityList = ["Spa", "Wine Nights", "Brunch with Other Moms"];
+    let kidsActivityList = [
       "Snorkeling",
       "Hiking",
       "Island Hopping",
@@ -13,40 +19,36 @@ class ActivityList extends Component {
       "Food Markets",
       "Beaches"
     ];
-  }
+    function renderMomActivities() {
+      const momActivities = momActivityList.map(activity => {
+        return (
+          <div className="ActivityList__activity ActivityList__mom">
+            <span className="ActivityList__activities">{activity}</span>
+          </div>
+        );
+      });
+      return <div className="ActivityList__activity-container">{momActivities}</div>;
+    }
 
-  renderMomActivities = () => {
-    const momActivities = this.momActivityList.map(activity => {
-      return (
-        <div className="ActivityList__activity ActivityList__mom">
-          <span className="ActivityList__activities">{activity}</span>
-        </div>
-      );
-    });
-    return <div className="ActivityList__activity-container">{momActivities}</div>;
-  };
-
-  renderKidsActivities = () => {
-    const kidsActivities = this.kidsActivityList.map(activity => {
-      return (
-        <div className="ActivityList__activity ActivityList__kids">
-          <span className="ActivityList__activities">{activity}</span>
-        </div>
-      );
-    });
-    return <div className="ActivityList__activity-container">{kidsActivities}</div>;
-  };
-
-  render() {
+    function renderKidsActivities() {
+      const kidsActivities = kidsActivityList.map(activity => {
+        return (
+          <div className="ActivityList__activity ActivityList__kids">
+            <span className="ActivityList__activities">{activity}</span>
+          </div>
+        );
+      });
+      return <div className="ActivityList__activity-container">{kidsActivities}</div>;
+    }
     return (
       <div className={this.state.classList}>
         <div className="ActivityList__activities-container">
           <h1 className="ActivityList__activity-header">Activities for Mom</h1>
-          {this.renderMomActivities()}
+          {renderMomActivities()}
         </div>
         <div className="ActivityList__activities-container">
           <h1 className="ActivityList__activity-header">Activities for Mom & Kids</h1>
-          {this.renderKidsActivities()}
+          {renderKidsActivities()}
         </div>
       </div>
     );
