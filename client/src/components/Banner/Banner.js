650d5660accb49baa138d1d2e624f550fd70e7d1
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-12-11 10:17:38 -0500

    made the text banners

--- client/src/components/Banner/Banner.js
+++ client/src/components/Banner/Banner.js
@@ -3,7 +3,12 @@ import "./Banner.css";
 
 const Banner = props => {
   let classlist = "banner";
-  return <div className={classlist} style={{backgroundImage: `${props.bannerType}`}}>{props.children}</div>;
+  return <div className={classlist} style={{
+    backgroundImage: `${props.bannerType}`,
+    height: `${props.bannerHeight}`, fontSize: `${props.bannerFontSize}`, lineHeight: `${props.bannerLineHeight}`
+  }}>
+    {props.children}
+  </div>;
 };
 
 export default Banner;
--- client/src/components/Banner/Banner.stories.js
+++ client/src/components/Banner/Banner.stories.js
@@ -1,18 +1,34 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import Banner from "./Banner";
+
 import { checkPropTypes } from "prop-types";
 
 storiesOf("Banner", module)
-  .add("Home Banner", () => <Banner 
-        bannerType='var(--home-banner)' 
-        />)
+  .add("Home Banner", () => <Banner
+    bannerType='var(--home-banner)'
+    bannerHeight='380px'
+  />)
+  .add("Home Banner with Welcome text", () =>
+    <Banner bannerType='var(--home-banner)' bannerHeight='380px' bannerText='Welcome,' bannerFontSize='80px' bannerLineHeight='120px'>
+      <h2>Welcome,</h2>
+    </Banner>
+  )
+  .add("Home Banner with Welcome Back text", () =>
+    <Banner bannerType='var(--home-banner)' bannerHeight='380px' bannerText='Welcome,' bannerFontSize='80px' bannerLineHeight='120px'>
+      <h2>Welcome Back,</h2>
+    </Banner>
+  )
 
-  .add("Home Banner with text", () => 
-  <Banner bannerType='var(--home-banner)'>
-    <h1>xyx</h1>
-  </Banner>)
+  .add("Community Banner", () => <Banner
+    bannerType='var(--community-banner)'
+  />)
+  .add("Community Banner with input", () =>
+    <Banner bannerType='var(--community-banner)'>
+      <h3>Community: People</h3>
+      <input/>
+    </Banner>
+  )
 
-  .add("Community Banner", () => <Banner bannerType='var(--community-banner)' />)
-  
+
   .add("Profile Banner", () => <Banner bannerType='var(--profile-banner)' />);
