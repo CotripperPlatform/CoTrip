388e61b2fffeda37f7b01b3f5f3e922ad086d85a
Ren Lucas <llucas314@gmail.com>
2019-12-10 21:04:31 -0500

    added data-test atrribute

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -5,7 +5,7 @@ const Button = props => {
   let classList = `Button Button--${props.color} Button--${props.size} Button--${props.textColor}-text Button--${props.type}`;
 
   return (
-    <button className={classList} onClick={() => props.handleClick()}>
+    <button className={classList} data-test="Button" onClick={() => props.handleClick()}>
       {props.icon ? <img src={props.icon} alt={props.text} /> : null}
       {props.text}
     </button>
