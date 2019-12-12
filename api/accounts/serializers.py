29cd83a47cc5ed81f01f224fe23db5b07269b6e1
Ben Miller <benbmiller123@gmail.com>
2019-12-12 16:04:09 -0500

    created Profile serializer

--- api/accounts/serializers.py
+++ api/accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -11,3 +12,9 @@ class UserSerializer(serializers.ModelSerializer):
 
 class TokenSerializer(serializers.Serializer):
     token = serializers.CharField(max_length=255)
+
+class  ProfileSerializer(serializers.Serializer):
+    
+    class Meta:
+        model = Profile
+        fields = ("first_name", "last_name", "city_of_residence", "topics")
