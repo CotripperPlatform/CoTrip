6093872c25dbf29c0dfbf5ace8ba181df1a78215
Ren Lucas <llucas314@gmail.com>
2019-12-12 16:47:17 -0500

    updated nav link dropdown  test

--- client/src/components/NavLinkDropdownMenu/NavLinkDropdownMenu.test.js
+++ client/src/components/NavLinkDropdownMenu/NavLinkDropdownMenu.test.js
@@ -8,24 +8,52 @@ const setUp = (props = {}) => {
   return component;
 };
 describe("NavLinkDropdownMenu component", () => {
-  let wrapper;
-  beforeEach(() => {
-    const props = {
-      menuList: [
-        {
-          text: "About",
-          to: "/"
-        },
-        {
-          text: "Home",
-          to: "/"
-        }
-      ]
-    };
-    wrapper = setUp(props);
+  describe("Has Props", () => {
+    describe("Has Links", () => {
+      const props = {
+        menuList: [
+          {
+            text: "About",
+            to: "/"
+          },
+          {
+            text: "Home",
+            to: "/"
+          }
+        ]
+      };
+      const wrapper = setUp(props);
+
+      it("Should render as expected", () => {
+        const component = findByTestAttribute(wrapper, ".NavLinkDropdownMenu");
+        expect(component.length).toBe(1);
+      });
+      it("Should render links", () => {
+        const component = findByTestAttribute(wrapper, "Link");
+        expect(component.length).toBe(2);
+      });
+    });
+    describe("Does Not Have Links", () => {
+      const props = { menuList: [] };
+      const wrapper = setUp(props);
+      it("Should render as expected", () => {
+        const component = findByTestAttribute(wrapper, ".NavLinkDropdownMenu");
+        expect(component.length).toBe(1);
+      });
+      it("Should render NO links", () => {
+        const component = findByTestAttribute(wrapper, "Link");
+        expect(component.length).toBe(0);
+      });
+    });
   });
-  it("Should render as expected", () => {
-    const component = findByTestAttribute(wrapper, ".NavLinkDropdownMenu");
-    expect(component.length).toBe(1);
+  describe("Have NO props", () => {
+    const props = {
+      data: null
+    };
+    const wrapper = setUp(props);
+
+    it("Should render without errors", () => {
+      expect(wrapper.length).toBe(1);
+    });
   });
 });
