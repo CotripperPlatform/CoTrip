b1b880076e085837111468e959151289034161ef
NickMckenney <mckenney2001@gmail.com>
2019-12-12 15:19:46 -0500

    working on matching the page of Onboarding three to the storybook.

--- client/src/components/OnboardingThree/OnboardingThree.js
+++ client/src/components/OnboardingThree/OnboardingThree.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./OnboardingThree.css";
-import Logo from "../Logo/Logo";
+import ProfilePicture from "../ProfilePicture/ProfilePicture";
 import InputTextField from "../InputTextField/InputTextField";
 import { Link } from "@storybook/router";
-
+import OnboardingParent from "../OnboardingParent/OnboardingParent";
+import Image from "../../assets/images/profile_large.png";
+import Button from "../Button/Button";
 // Function based React Component
 const OnboardingThree = props => {
   // Default Class to apply to Component
@@ -11,21 +13,17 @@ const OnboardingThree = props => {
 
   return (
     <div className={classList}>
-      <Logo />
-      <h1 className="onboarding-three-text">Step Three: Personalize Your Profile (optional)</h1>
+      <OnboardingParent>
+        <h1 className="onboarding-three-text">Step Three: Personalize Your Profile (optional)</h1>
 
-      <div className="filler-div"></div>
-      <InputTextField type="text" name="age" placeholder="What is your age?" />
-      <InputTextField type="destination" placeholder="What is your dream destination(s)?" />
-      <button className="filler-btn">Finish</button>
-      <div className="filler-circles">
-        <div className="filler-circle"></div>
-        <div className="filler-circle"></div>
-        <div className="filler-circle filler-purple"></div>
-      </div>
-      <Link to={props.link} className="onboarding-three-link">
-        Skip
-      </Link>
+        <ProfilePicture className="profile-image" type="medium" to="#" image={Image} />
+        <InputTextField className="input" type="text" name="age" placeholder="What is your age?" />
+        <InputTextField type="destination" placeholder="What is your dream destination(s)?" />
+        <Button text="Finish"></Button>
+        <Link to={props.link} className="onboarding-three-link">
+          Skip
+        </Link>
+      </OnboardingParent>
     </div>
   );
 };
