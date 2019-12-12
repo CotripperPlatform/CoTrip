da59a930275c3125fca8398a8101370c32644951
blake <blakeeh723@gmail.com>
2019-12-12 16:27:05 -0500

    added pages folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Route, Link } from "react-router-dom";
-import SplashPage from "./components/SplashPage/SplashPage";
+import SplashPage from "./pages/SplashPage/SplashPage";
 import "./App.css";
 
 class App extends Component {
