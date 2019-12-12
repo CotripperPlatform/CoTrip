802949cf4c7a02853b66bc849dfea6d0227ff6d7
blake <blakeeh723@gmail.com>
2019-12-12 15:39:17 -0500

    used Button component instead of Link

--- client/src/components/SplashPage/SplashPage.js
+++ client/src/components/SplashPage/SplashPage.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import Button from '../Button/Button';
 import "./SplashPage.css";
 
-function handleClick() {
-  console.log('login or register button clicked at splash page');
+function toLogin() {
+  return <Redirect to='/login' />;
 }
+function toRegister() {
+  return <Redirect to='/register' />;
+}
+
 const SplashPage = () => {
   return (
     <div className='SplashPage'>
       <h1>CoTripper</h1>
       <p>Book trips, meet moms, and experience family travel together!</p>
-      <div className='SplashPage__button-margin--small'>
-        <Link to='/login'><Button text="Login" handleClick={handleClick}/></Link>
-      </div>
-      <div className='SplashPage__button-margin'>
-        <Link to='/register'><Button text="Register" color="purple" handleClick={handleClick}/></Link>
-      </div>
+      <Button text="Login" handleClick={toLogin} />
+      <Button text="Register" color="purple" handleClick={toRegister} />
     </div>
   );
 }
