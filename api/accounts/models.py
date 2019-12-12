3c56e68169781c6ee20755cfac3cf0e493821622
Ben Miller <benbmiller123@gmail.com>
2019-12-12 16:26:42 -0500

    added link between user create and update methods and profile

--- api/accounts/models.py
+++ api/accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.apps import apps
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 Topic = apps.get_model('cotrip_app', 'Topic')
 
 
@@ -10,6 +12,8 @@ class Profile(models.Model):
         on_delete=models.CASCADE,
         primary_key=True,
     )
+    # add profile pic, its a must
+    # add connections, list of users?
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     city_of_residence = models.CharField(max_length=200)
@@ -19,3 +23,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.first_name + " " + self.last_name
+
+@reciever(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+        
+@reciever(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
