9f3c34b29da9f81cd547c061596deead4d49f29b
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-12-12 16:38:47 -0500

    changed header class. Trying to pass in props for TripCard

--- client/src/components/UpcomingTripsCard/UpcomingTripsCard.js
+++ client/src/components/UpcomingTripsCard/UpcomingTripsCard.js
@@ -4,7 +4,7 @@ import "./UpcomingTripsCard.css";
 import { Link } from "react-router-dom";
 import Card from "../Card/Card";
 import TripCard from "../TripCard/TripCard";
-import img from "../../assets/images/card_small2.png";
+// import img from "../../assets/images/card_small2.png";
 
 const UpcomingTripsCard = (props) => {
 
@@ -12,9 +12,10 @@ const UpcomingTripsCard = (props) => {
 
   return(
     <Card color="pink" className="UpcomingTripsCard">
-      <header>{props.header}</header>
+      <header className="UpcomingTripsCard__header">{props.header}</header>
       <div className="UpcomingTripsCard__info">
-        <TripCard src={img} location="Puerto Rico" date="April 2019"/>
+        {/* <TripCard src={img} location="Puerto Rico" date="April 2019"/> */}
+        <TripCard src={props.src} location={props.location} date={props.date}/>
         <div className="UpcomingTripsCard__text-info">
           <p className="UpcomingTripsCard__general-information">General Information:</p>
           <ul>
