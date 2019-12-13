f3caaab4dede5a3acd414681ff1f1cee936d7017
karenheyn <karenheyn@gmail.com>
2019-12-13 11:41:31 -0500

    added responsiveness, made changes

--- client/src/components/BookTripCard/BookTripCard.js
+++ client/src/components/BookTripCard/BookTripCard.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import "./BookTripCard.css";
 import Card from "../Card/Card";
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const BookTripCard = props => {
   // Default Class to apply to Component
-  let classList = `InteractionCard`;
+
   let [title, image1, image2, link] = [
     props.children[0],
     props.children[1],
@@ -18,7 +18,7 @@ const BookTripCard = props => {
       {title}
       <div className="container">
         {image1}
-        {image2}
+        <span>{image2}</span>
       </div>
       <Link to={props.to}>{link}</Link>
     </Card>
--- client/src/components/BookTripCard/BookTripCard.stories.js
+++ client/src/components/BookTripCard/BookTripCard.stories.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import BookTripCard from "./BookTripCard";
-import { Route } from "@storybook/router";
-import { checkPropTypes } from "prop-types";
 import TripCard from "../TripCard/TripCard";
 import example1 from "../../assets/images/card_small2.png";
+import example2 from "../../assets/images/Hawaii-resort.jpg";
 
 storiesOf("BookTripCard", module).add("Default", () => (
   <BookTripCard>
     <h1>Book a Trip</h1>
-    <TripCard src="https://wallpaperaccess.com/full/144067.jpg" location="Hawaii" date="May 2020" />
+    <TripCard src={example2} location="Hawaii" date="May 2020" />
     <TripCard src={example1} location="Puerto Rico" date="April 2019" />
     <h3 to="/route">Book a Trip Now!</h3>
   </BookTripCard>
