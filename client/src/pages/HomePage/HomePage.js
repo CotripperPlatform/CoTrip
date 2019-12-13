54523afa6517ccd6a3a5985c746198e7b3f628f6
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-13 09:31:10 -0500

    initialized homepage component, moved to pages dir

---
+++ client/src/pages/HomePage/HomePage.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import "./HomePage.css";
+
+// Function based React Component
+const HomePage = (props) => {
+
+  // Default Class to apply to Component
+  let classList = `HomePage`;
+
+
+  return(
+    <div className={classList}>
+      HomePage
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ client/src/pages/HomePage/HomePage.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import HomePage from './HomePage';
+
+storiesOf('HomePage', module)
+  .add("Default", () => (
+    <HomePage />
+  )
+)
