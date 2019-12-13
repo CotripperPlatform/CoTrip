f384631ae685d87b3cd8f732032c8254e9de817d
Ren Lucas <llucas314@gmail.com>
2019-12-13 09:57:48 -0500

    fixed syntax error

--- client/src/components/NavLinkDropdownMenu/NavLinkDropdownMenu.js
+++ client/src/components/NavLinkDropdownMenu/NavLinkDropdownMenu.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./NavLinkDropdownMenu.css";
 
-const NavLinkDropdownMenu = (props) => (
-  <ul className='NavLinkDropdownMenu'>
-    {props.menuList.map(li => 
+const NavLinkDropdownMenu = props => (
+  <ul className="NavLinkDropdownMenu">
+    {props.menuList.map(li => (
       <Link to={li.to} key={li.text}>
         <li>{li.text}</li>
       </Link>
