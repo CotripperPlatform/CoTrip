4c4c62ee318515c6a041c2b9a1fe72263d83d45d
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-13 10:37:32 -0500

    added groups cards and header

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import NavBar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
+import Card from "../../components/Card/Card";
+import GroupCard from "../../components/GroupCard/GroupCard";
 
 const HomePage = props => {
   return (
@@ -16,8 +18,30 @@ const HomePage = props => {
           <h1>WELCOME USER</h1>
         </div>
         <div className="HomePage_top-items-wrapper">
-          <div className="HomePage__suggestions-container"></div>
-          <div className="HomePage__top-cards-container"></div>
+          <span className="HomePage__suggestions-container">
+            <h1 className="HomePage__suggested-header">Suggested Groups</h1>
+            <div className="HomePage__group-cards-container">
+              <GroupCard
+                name="DIY with your kids"
+                members="98"
+                location="Boston, MA"
+                picture="https://images.unsplash.com/photo-1453749024858-4bca89bd9edc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1007&q=80"
+              />
+              <GroupCard
+                name="DIY with your kids"
+                members="98"
+                location="Boston, MA"
+                picture="https://images.unsplash.com/photo-1453749024858-4bca89bd9edc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1007&q=80"
+              />
+              <GroupCard
+                name="DIY with your kids"
+                members="98"
+                location="Boston, MA"
+                picture="https://images.unsplash.com/photo-1453749024858-4bca89bd9edc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1007&q=80"
+              />
+            </div>
+          </span>
+          <span className="HomePage__top-cards-container"></span>
         </div>
       </div>
       <footer className="HomePage__footer-wrapper">
