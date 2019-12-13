50f9c3795081fc920b07a76c7abfc7415bcfac46
Steven Kim <sskim14@gmail.com>
2019-12-12 22:48:58 -0500

    Added responsiveness to component with standard media query breakpoints.

--- client/src/components/OnboardingOne/OnboardingOne.js
+++ client/src/components/OnboardingOne/OnboardingOne.js
@@ -18,36 +18,34 @@ class OnboardingOne extends Component {
     return (
       <OnboardingParent>
         <div className={this.state.classList}>
-          <div className="OnboardingOne__wrapper">
-            <form className="OnboardingOne__form" action="">
-              <h1 className="OnboardingOne__header">Step One</h1>
-              <div className="OnboardingOne__container">
-                <InputTextField type="text" name="name" placeholder="Name"></InputTextField>
-              </div>
-              <div className="OnboardingOne__container">
-                <InputTextField type="text" name="email" placeholder="Email"></InputTextField>
-              </div>
-              <div className="OnboardingOne__container">
-                <InputTextField type="text" name="password" placeholder="Password"></InputTextField>
-              </div>
-              <div className="OnboardingOne__container">
-                <InputTextField
-                  className="OnboardingOne__container"
-                  type="text"
-                  name="confirm password"
-                  placeholder="Confirm Password"
-                ></InputTextField>
-              </div>
-              <div className="OnboardingOne__button-container">
-                <Button
-                  className="OnboardingOne__button"
-                  text="Next"
-                  size="small"
-                  handleClick={() => this.props.handleClick}
-                ></Button>
-              </div>
-            </form>
-          </div>
+          <form className="OnboardingOne__form" action="">
+            <h1 className="OnboardingOne__header">Step One</h1>
+            <div className="OnboardingOne__container">
+              <InputTextField type="text" name="name" placeholder="Name"></InputTextField>
+            </div>
+            <div className="OnboardingOne__container">
+              <InputTextField type="text" name="email" placeholder="Email"></InputTextField>
+            </div>
+            <div className="OnboardingOne__container">
+              <InputTextField type="text" name="password" placeholder="Password"></InputTextField>
+            </div>
+            <div className="OnboardingOne__container">
+              <InputTextField
+                className="OnboardingOne__container"
+                type="text"
+                name="confirm password"
+                placeholder="Confirm Password"
+              ></InputTextField>
+            </div>
+            <div className="OnboardingOne__button-container">
+              <Button
+                className="OnboardingOne__button"
+                text="Next"
+                size="large"
+                handleClick={() => this.props.handleClick}
+              ></Button>
+            </div>
+          </form>
         </div>
       </OnboardingParent>
     );
