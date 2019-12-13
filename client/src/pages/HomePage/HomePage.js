f1b41263035dbb80120924ef3fb395182f5d8e74
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-13 10:08:53 -0500

    positioning footer and header

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -3,13 +3,18 @@ import "./HomePage.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import NavBar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
 
 const HomePage = props => {
   return (
-    <div className="home-page-wrapper">
+    <div className="HomePage__wrapper">
       <nav>
-        <NavBar />
+        <NavBar></NavBar>
       </nav>
+      <div className="content"></div>
+      <footer className="HomePage__footer-wrapper">
+        <Footer />
+      </footer>
     </div>
   );
 };
