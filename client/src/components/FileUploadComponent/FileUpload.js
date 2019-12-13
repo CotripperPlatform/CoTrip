a6ebc052feb0b261a3b6a788aacb94bd5eb49a71
Tissa Khosla <tissa.music@gmail.com>
2019-12-13 15:06:26 -0500

    cautionary commit

--- client/src/components/FileUploadComponent/FileUpload.js
+++ client/src/components/FileUploadComponent/FileUpload.js
@@ -1,34 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
+import ProfilePicture from '../ProfilePicture/ProfilePicture'
 import './FileUpload.css'
 
-const FileUpload = (props) => {
+class FileUpload extends Component {
+  constructor(props) {
+    super(props)
 
-  function handleChange(eo) {
-    eo.preventDefault()
-    eo.persist()
-    console.log(eo)
+    this.state = {
+      file: null,
+      renderThis: null
+    }
+
+    this.handleChange = this.handleChange.bind(this)
   }
 
-  if (props.context === "userPhoto") {
-    return (
-      <div onChange={handleChange} className='NewUserUpload'>
-        <p>Upload a profile photo</p>
-        <label for="inputTypeFile" className="FileUpload__label" >{props.buttonLabel}</label>
-        <input type="file" id="inputTypeFile" className='FileUpload' ></input>
-      </div>
-    );
-  } else {
-    return (
-      <div onChange={handleChange}>
-        <label for="inputTypeFile" className="FileUpload__label" >{props.buttonLabel}</label>
-        <input type="file" id="inputTypeFile" className='FileUpload' ></input>
-      </div>
-    )
+  handleChange(eo) {
+    eo.preventDefault()
+    eo.persist()
+    this.setState({ 
+      file: eo.target.files,
+      renderThis: <ProfilePicture to="#" image={this.state.file} />
+    })
+  }
+
+  componentDidMount() {
+    if (this.props.context === "userPhoto") {
+      this.setState({
+        renderThis:
+          <div onChange={this.handleChange} className='NewUserUpload'>
+            <p>Upload a profile photo</p>
+            <label for="inputTypeFile" className="FileUpload__label" >{this.props.buttonLabel}</label>
+            <input type="file" id="inputTypeFile" className='FileUpload' ></input>
+          </div>
+      })
+
+    } else {
+      this.setState({
+        renderThis:
+          <div onChange={this.handleChange}>
+            <label for="inputTypeFile" className="FileUpload__label" >{this.props.buttonLabel}</label>
+            <input type="file" id="inputTypeFile" className='FileUpload' ></input>
+          </div>
+      })
+    }
+  }
+
+  render() {
+    return this.state.renderThis
   }
 }
 
 FileUpload.defaultProps = {
-  buttonLabel: "Browse Files",
+  buttonLabel: 'Browse Files'
 }
 
 export default FileUpload;
