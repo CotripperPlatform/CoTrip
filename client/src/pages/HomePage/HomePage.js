a98769d3fbce64a92994ecfb23d272b944c12669
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-13 10:48:02 -0500

    Added yellow arrow

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -39,6 +39,7 @@ const HomePage = props => {
                 location="Boston, MA"
                 picture="https://images.unsplash.com/photo-1453749024858-4bca89bd9edc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1007&q=80"
               />
+              <a href="/" className="HomePage__arrow" />
             </div>
           </span>
           <span className="HomePage__top-cards-container"></span>
