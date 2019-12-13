33c52dfb6ee8cf246761cff543e80ede3784caef
DallasLind <mcgraw.dallas@gmail.com>
2019-12-13 09:58:08 -0500

    Working on formatting

---
+++ client/src/components/ForumComment/ForumComment.stories.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import ForumComment from "./ForumComment";
+import Profile from "../../assets/images/card_profile3.png";
+
+storiesOf("ForumComment", module)
+    .add("ForumComment", () => (
+    <ForumComment
+      name="Lexi R."
+      likes={2}
+      comments={5}
+      image={Profile}
+      reply={{
+        date: "May 5",
+        time: "4:45pm",
+        body:
+          "That sounds like a great time! I hope my kids and I can do something like that this summer!"
+      }}
+    />
+  ));
