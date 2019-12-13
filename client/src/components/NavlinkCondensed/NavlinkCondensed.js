795693a5a4e160c755b67b57cab2feac1076bb04
William Chrapcynski <wchrapcynski@gmail.com>
2019-12-13 11:00:02 -0500

    Setup of NavlinkCondensed

---
+++ client/src/components/NavlinkCondensed/NavlinkCondensed.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import "./NavlinkCondensed.css";
+
+// Function based React Component
+const NavlinkCondensed = (props) => {
+
+  // Default Class to apply to Component
+  let classList = `NavlinkCondensed`;
+
+
+  return(
+    <div className={classList}>
+      NavlinkCondensed
+    </div>
+  );
+}
+
+export default NavlinkCondensed;
---
+++ client/src/components/NavlinkCondensed/NavlinkCondensed.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import NavlinkCondensed from './NavlinkCondensed';
+
+storiesOf('NavlinkCondensed', module)
+  .add("Default", () => (
+    <NavlinkCondensed />
+  )
+)
