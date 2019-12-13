e4a086c87c700e24b2c1224602eb50f6d660c675
Steven Kim <sskim14@gmail.com>
2019-12-13 14:48:46 -0500

    Set default prop for array of pills

--- client/src/components/OnboardingPills/OnboardingPills.js
+++ client/src/components/OnboardingPills/OnboardingPills.js
@@ -3,7 +3,6 @@ import "./OnboardingPills.css";
 import Pill from "../Pill/Pill";
 
 const OnboardingPills = props => {
-  let classList = `OnboardingPills`;
   const renderPills = props.pills.map((pill, index) => {
     return (
       <Pill
@@ -18,10 +17,25 @@ const OnboardingPills = props => {
     );
   });
   return (
-    <div className={classList}>
+    <div className="OnboardingPills">
       <div className="OnboardingPills__inner-container">{renderPills}</div>
     </div>
   );
 };
-
+OnboardingPills.defaultProps = {
+  pills: [
+    "Parenting",
+    "Teens",
+    "Arts",
+    "Traveling",
+    "Primary School",
+    "Sports",
+    "Meetups",
+    "Secondary School",
+    "Flights",
+    "Infants",
+    "Budgeting",
+    "Health & Wellness"
+  ]
+};
 export default OnboardingPills;
--- client/src/components/OnboardingPills/OnboardingPills.stories.js
+++ client/src/components/OnboardingPills/OnboardingPills.stories.js
@@ -2,19 +2,4 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import OnboardingPills from "./OnboardingPills";
 
-let pillList = [
-  "Parenting",
-  "Teens",
-  "Arts",
-  "Traveling",
-  "Primary School",
-  "Sports",
-  "Meetups",
-  "Secondary School",
-  "Flights",
-  "Infants",
-  "Budgeting",
-  "Health & Wellness"
-];
-
-storiesOf("OnboardingPills", module).add("Default", () => <OnboardingPills pills={pillList} />);
+storiesOf("OnboardingPills", module).add("Default", () => <OnboardingPills />);
