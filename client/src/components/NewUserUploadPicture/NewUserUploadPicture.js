44713583625d6bf6e4ed7b09995efdecaa34d67f
Tissa Khosla <tissa.music@gmail.com>
2019-12-13 10:16:09 -0500

    passed handleclick as props

--- client/src/components/NewUserUploadPicture/NewUserUploadPicture.js
+++ client/src/components/NewUserUploadPicture/NewUserUploadPicture.js
@@ -2,16 +2,12 @@ import React from "react";
 import './NewUserUploadPicture.css'
 import Button from '../Button/Button'
 
-const handleClick = e => {
-  console.log("Clicked");
-}; //from ../Button/Button.stories.js
-
-const NewUserUpload = () => {
+const NewUserUpload = (props) => {
 
   return (
     <div className='NewUserUpload'>
-      <p className='NewUserUpload__p'>Upload a profile photo</p>
-      <Button text="Browse Files" color="pink" size="extra-small" handleClick={handleClick} />
+      <p>Upload a profile photo</p>
+      <Button text="Browse Files" color="pink" size="extra-small" handleClick={props.handleClick} />
     </div>
   );
 };
--- client/src/components/NewUserUploadPicture/NewUserUploadPicture.stories.js
+++ client/src/components/NewUserUploadPicture/NewUserUploadPicture.stories.js
@@ -2,6 +2,11 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import NewUserUpload from "./NewUserUploadPicture"
 
+const handleClick = e => {
+  console.log("Clicked");
+}; //from ../Button/Button.stories.js
+
+
 storiesOf("New User Upload", module)
-  .add("New User Upload", () => <NewUserUpload></NewUserUpload>)
+  .add("New User Upload", () => <NewUserUpload handleClick={handleClick}></NewUserUpload>)
 
