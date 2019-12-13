c556bfe428f2b12d9fe474558a51a8884e785ed4
Steven Kim <sskim14@gmail.com>
2019-12-13 11:34:36 -0500

    Added pill onClick function to each pill.

--- client/src/components/OnboardingPills/OnboardingPills.js
+++ client/src/components/OnboardingPills/OnboardingPills.js
@@ -2,27 +2,21 @@ import React from "react";
 import "./OnboardingPills.css";
 import Pill from "../Pill/Pill";
 
-const pillList = [
-  "Parenting",
-  "Teens",
-  "Arts",
-  "Traveling",
-  "Primary School",
-  "Sports",
-  "Meetups",
-  "Secondary School",
-  "Flights",
-  "Infants",
-  "Budgeting",
-  "Health & Wellness"
-];
-
 const OnboardingPills = props => {
   let classList = `OnboardingPills`;
-  const renderPills = pillList.map((pill, index) => {
-    return <Pill className="OnboardingPills__pill-container" key={index} text={pill} />;
+  const renderPills = props.pills.map((pill, index) => {
+    return (
+      <Pill
+        className="OnboardingPills__pill-container"
+        key={index}
+        text={pill}
+        color={"pink"}
+        active={false}
+        inactiveColor={"white"}
+        onClick={() => props.pillClick}
+      />
+    );
   });
-
   return (
     <div className={classList}>
       <div className="OnboardingPills__inner-container">{renderPills}</div>
--- client/src/components/OnboardingPills/OnboardingPills.stories.js
+++ client/src/components/OnboardingPills/OnboardingPills.stories.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import OnboardingPills from './OnboardingPills';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import OnboardingPills from "./OnboardingPills";
 
-storiesOf('OnboardingPills', module)
-  .add("Default", () => (
-    <OnboardingPills />
-  )
-)
+let pillList = [
+  "Parenting",
+  "Teens",
+  "Arts",
+  "Traveling",
+  "Primary School",
+  "Sports",
+  "Meetups",
+  "Secondary School",
+  "Flights",
+  "Infants",
+  "Budgeting",
+  "Health & Wellness"
+];
+
+storiesOf("OnboardingPills", module).add("Default", () => <OnboardingPills pills={pillList} />);
