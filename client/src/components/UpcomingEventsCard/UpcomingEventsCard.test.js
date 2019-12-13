a562b23021fa842a0c125f75640d389ee2ade595
Noah Clark <noahbc08@gmail.com>
2019-12-13 12:12:28 -0500

    completed UpcomingEventsCard testing

--- client/src/components/UpcomingEventsCard/UpcomingEventsCard.test.js
+++ client/src/components/UpcomingEventsCard/UpcomingEventsCard.test.js
@@ -7,11 +7,8 @@ configure({adapter: new Adapter()});
 
 // We will describe a block of tests
 describe('UpcomingEventsCard component', () => {
-	// we will write one individual test
   it('should render as expected', () => {
-    // Shallow rendering renders a component without rendering any of its children
     const component = shallow(<UpcomingEventsCard />)
-    // We create an assertion within the test that checks if our component renders our name prop
    expect(component.exists()).toBe(true)
   })
   it('Component should render defaults', () => {
