1a972dd22cd2a52cda78f52bc5bbdb695df57f43
Ren Lucas <llucas314@gmail.com>
2019-12-14 14:23:46 -0500

    added test to navlink

--- client/src/components/NavLink/NavLink.test.js
+++ client/src/components/NavLink/NavLink.test.js
@@ -1,16 +1,67 @@
-import React from 'react'
-import { shallow, configure } from 'enzyme'
-import NavLink from './NavLink.js'
-import Adapter from 'enzyme-adapter-react-16';
-configure({adapter: new Adapter()});
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { mount } from "enzyme";
+import NavLink from "./NavLink.js";
+const setUp = props => {
+  const component = mount(
+    <BrowserRouter>
+      <NavLink {...props} />
+    </BrowserRouter>
+  );
+  return component;
+};
+describe("NavLink component", () => {
+  describe("Has Props", () => {
+    let wrapper;
+    beforeEach(() => {
+      const props = {
+        text: "community",
+        to: "/",
+        menuList: [
+          {
+            text: "Explore People",
+            to: "/"
+          },
+          {
+            text: "Join Groups",
+            to: "/"
+          }
+        ]
+      };
+      wrapper = setUp(props);
+    });
+    it("Should render as expected", () => {
+      console.log(wrapper.debug());
+      expect(wrapper.find(".NavLink").length).toBe(1);
+    });
+  });
+});
 
-// We will describe a block of tests
-describe('NavLink component', () => {
-	// we will write one individual test
-  it('should render as expected', () => {
-    // Shallow rendering renders a component without rendering any of its children
-    const component = shallow(<NavLink />)
-    // We create an assertion within the test that checks if our component renders our name prop
-  // expect(component.contains('Your name')).toBe(true)
-  })
-})
+// it("Component should render the number of dots supplied in the properties", () => {
+//   const component = mount(
+//     <NavLink numberOfDots={3} activeLocation={1} onStatusChange={jest.fn()} />
+//   );
+//   console.log(component.debug());
+//   expect(component.find(".CarouselDot").length).toBe(3);
+// });
+
+// it("Component should switch from dot 1 to dot 2 upon clicking 2nd dot", () => {
+//   function callback(val) {}
+//   const component = mount(
+//     <NavLink numberOfDots={3} activeLocation={1} onStatusChange={callback} />
+//   );
+//   component
+//     .find(".CarouselDot")
+//     .at(1)
+//     .simulate("click");
+
+//   expect(component.state().activeLocation).toBe(2);
+// });
+
+// it("Component should render border when prop `border` is set ", () => {
+//   const component = mount(
+//     <NavLink numberOfDots={3} activeLocation={0} border onStatusChange={jest.fn()} />
+//   );
+
+//   expect(component.find(".CarouselDot__border").exists()).toBe(true);
+// });
