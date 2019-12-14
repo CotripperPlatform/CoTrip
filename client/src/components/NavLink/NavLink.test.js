d9d8733246018c1700f12194b4e688c61417113f
Ren Lucas <llucas314@gmail.com>
2019-12-14 15:01:46 -0500

    updated navlink test

--- client/src/components/NavLink/NavLink.test.js
+++ client/src/components/NavLink/NavLink.test.js
@@ -1,13 +1,8 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import { mount } from "enzyme";
+import { shallow } from "enzyme";
 import NavLink from "./NavLink.js";
 const setUp = props => {
-  const component = mount(
-    <BrowserRouter>
-      <NavLink {...props} />
-    </BrowserRouter>
-  );
+  const component = shallow(<NavLink {...props} />);
   return component;
 };
 describe("NavLink component", () => {
@@ -31,37 +26,49 @@ describe("NavLink component", () => {
       wrapper = setUp(props);
     });
     it("Should render as expected", () => {
-      console.log(wrapper.debug());
       expect(wrapper.find(".NavLink").length).toBe(1);
+    });
+    it("Should render h1 text as expected", () => {
+      expect(wrapper.find("h1").text()).toBe("community");
+    });
+    it("Should render NavDropdown", () => {
+      expect(wrapper.find("NavLinkDropdownMenu").length).toBe(1);
+    });
+    it("Should have initial dropdownVisible state false", () => {
+      expect(wrapper.state("dropdownVisible")).toBe(false);
+    });
+    it("Should have change state on mouseover", () => {
+      const component = wrapper.find(".NavLink");
+      component.simulate("mouseenter");
+      expect(wrapper.state("dropdownVisible")).toBe(true);
+      component.simulate("mouseleave");
+      expect(wrapper.state("dropdownVisible")).toBe(false);
+    });
+  });
+  describe("Has NO Props", () => {
+    let wrapper;
+    beforeEach(() => {
+      const props = null;
+      wrapper = setUp(props);
+    });
+    it("Should render as expected", () => {
+      expect(wrapper.find(".NavLink").length).toBe(1);
+    });
+    it("Should render h1 text as expected", () => {
+      expect(wrapper.find("h1").text()).toBe("community");
+    });
+    it("Should render NavDropdown", () => {
+      expect(wrapper.find("NavLinkDropdownMenu").length).toBe(1);
+    });
+    it("Should have initial dropdownVisible state false", () => {
+      expect(wrapper.state("dropdownVisible")).toBe(false);
+    });
+    it("Should have change state on mouseover", () => {
+      const component = wrapper.find(".NavLink");
+      component.simulate("mouseenter");
+      expect(wrapper.state("dropdownVisible")).toBe(true);
+      component.simulate("mouseleave");
+      expect(wrapper.state("dropdownVisible")).toBe(false);
     });
   });
 });
-
-// it("Component should render the number of dots supplied in the properties", () => {
-//   const component = mount(
-//     <NavLink numberOfDots={3} activeLocation={1} onStatusChange={jest.fn()} />
-//   );
-//   console.log(component.debug());
-//   expect(component.find(".CarouselDot").length).toBe(3);
-// });
-
-// it("Component should switch from dot 1 to dot 2 upon clicking 2nd dot", () => {
-//   function callback(val) {}
-//   const component = mount(
-//     <NavLink numberOfDots={3} activeLocation={1} onStatusChange={callback} />
-//   );
-//   component
-//     .find(".CarouselDot")
-//     .at(1)
-//     .simulate("click");
-
-//   expect(component.state().activeLocation).toBe(2);
-// });
-
-// it("Component should render border when prop `border` is set ", () => {
-//   const component = mount(
-//     <NavLink numberOfDots={3} activeLocation={0} border onStatusChange={jest.fn()} />
-//   );
-
-//   expect(component.find(".CarouselDot__border").exists()).toBe(true);
-// });
