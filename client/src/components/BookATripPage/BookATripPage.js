e42cbe233d0a4560e8f986d31cb0befe14b77e1f
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-12-16 10:17:23 -0500

    generated component for BookATripPage with hygen

---
+++ client/src/components/BookATripPage/BookATripPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./BookATripPage.css";
+
+const BookATripPage = (props) => {
+
+  let classList = `BookATripPage`;
+
+
+  return(
+    <div className={classList}>
+      BookATripPage
+    </div>
+  );
+}
+
+export default BookATripPage;
