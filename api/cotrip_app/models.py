102cacdba13be910fdf67fd5b45ada03c0a88ffa
DallasLind <mcgraw.dallas@gmail.com>
2019-12-16 15:31:21 -0500

    Topics model works and 12 topics have been added to the server

--- api/cotrip_app/models.py
+++ api/cotrip_app/models.py
@@ -1,30 +1,5 @@
 from django.db import models
 
-
-class User(models.Model):
-    email = models.EmailField(max_length=200)
-    password = models.CharField(max_length=100)
-
-    def __str__(self):
-        # return super().__str__()
-        return self.email
-
-
-class Profile(models.Model):
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='user')
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    age = models.IntegerField()
-    city_of_residence = models.CharField(max_length=200)
-    dream_destination = models.CharField(max_length=100)
-    profile_photo = models.ImageField(max_length=200)
-    # topics = models.ForeignKey(Topic, on_delete=models.PROTECT)
-
-    def __str__(self):
-        return self.last_name
-
-
 # Create your models here.
 
 class City(models.Model):
@@ -34,3 +9,9 @@ class City(models.Model):
     
     def __str__(self):
         return self.city + ", " + self.country 
+
+class Topic(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
--- api/cotrip_app/views.py
+++ api/cotrip_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
+def topic_list(request):
+    topics = Topic.objects.all()
+    return render(request, 'cotrip/topics/html', {'topics': topics})
