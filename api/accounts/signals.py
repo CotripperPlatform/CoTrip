00a2534f41e61c4795eceb36ad5e1acca3399e8c
Tory Klingenstein <toryklingenstein@gmail.com>
2019-12-16 16:17:38 -0500

    update signals for custom user

--- api/accounts/signals.py
+++ api/accounts/signals.py
@@ -1,15 +1,14 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, CustomUser
 
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=CustomUser)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=CustomUser)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
