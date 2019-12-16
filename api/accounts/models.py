17bcd435a6af3ac762ae9ebad06456026ff76e6b
Ben Miller <benbmiller123@gmail.com>
2019-12-16 10:42:31 -0500

    made migrations

--- api/accounts/models.py
+++ api/accounts/models.py
@@ -12,16 +12,16 @@ class Profile(models.Model):
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     first_name = models.CharField(max_length=200, blank=True)
     last_name = models.CharField(max_length=200, blank=True)
-    city_of_residence = models.CharField(max_length=200, blank=True)
-    # topics = models.ManyToManyField(Topic)
-    # add connections, list of users?
-    # trips = models.ManyToManyField(Trip)
-    age = models.IntegerField(blank=True)
-    dream_destination = models.CharField(max_length=200, blank=True)
-    bio = models.TextField(blank=True)
-    instagram_url = models.URLField(blank=True)
-    pinterest_url = models.URLField(blank=True)
-    facebook_url = models.URLField(blank=True)
+    # city_of_residence = models.CharField(max_length=200, blank=True)
+    # # topics = models.ManyToManyField(Topic)
+    # # add connections, list of users?
+    # # trips = models.ManyToManyField(Trip)
+    # age = models.IntegerField(blank=True)
+    # dream_destination = models.CharField(max_length=200, blank=True)
+    # bio = models.TextField(blank=True)
+    # instagram_url = models.URLField(blank=True)
+    # pinterest_url = models.URLField(blank=True)
+    # facebook_url = models.URLField(blank=True)
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name} Profile'
+        return f' Profile'
--- api/accounts/signals.py
+++ api/accounts/signals.py
@@ -5,11 +5,11 @@ from .models import Profile
 
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
+def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
 
 @receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
+def save_profile(sender, instance, **kwargs):
     instance.profile.save()
