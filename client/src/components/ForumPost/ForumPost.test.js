1bb8261bdfabd95579c5c2da2126a45b8ab6adea
Ren Lucas <llucas314@gmail.com>
2019-12-15 19:15:42 -0500

    added tests for ForumPost

--- client/src/components/ForumPost/ForumPost.test.js
+++ client/src/components/ForumPost/ForumPost.test.js
@@ -1,16 +1,43 @@
 import React from 'react'
-import { shallow, configure } from 'enzyme'
+import { shallow } from 'enzyme'
 import ForumPost from './ForumPost.js'
-import Adapter from 'enzyme-adapter-react-16';
-configure({adapter: new Adapter()});
 
-// We will describe a block of tests
+const setUp = props =>{
+  const component = shallow(<ForumPost {...props}/>);
+  return component;
+}
 describe('ForumPost component', () => {
-	// we will write one individual test
-  it('should render as expected', () => {
-    // Shallow rendering renders a component without rendering any of its children
-    const component = shallow(<ForumPost />)
-    // We create an assertion within the test that checks if our component renders our name prop
-  // expect(component.contains('Your name')).toBe(true)
+
+  let wrapper;
+  let onClick = jest.fn()
+  beforeEach(()=>{
+    const props = {
+      image: "../../assets/images/card_profile3.png",
+      name: "Lexi R.",
+      likes: 0,
+      comments: 0,
+      pillClick: onClick,
+      commentClick: onClick,
+      likeClick: onClick,
+      to:"/"
+    }
+    wrapper = setUp(props);
+    
+  })
+  it('Should render without errors', ()=>{
+    let component = wrapper.find('.ForumPost')
+    expect(component.length).toBe(1)
+  })
+  it('Should render two buttons', ()=>{
+    let component = wrapper.find('Button');
+    expect(component.length).toBe(2)
+  })
+  it('Should render post body', ()=>{
+    let component = wrapper.find('PostBody');
+    expect(component.length).toBe(1)
+  })
+  it('Should render profile picture', ()=>{
+    let component = wrapper.find('ProfilePicture');
+    expect(component.length).toBe(1)
   })
 })
