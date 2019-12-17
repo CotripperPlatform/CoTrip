89f1c7db0db6917a53fb65772e114c333824163d
Tissa Khosla <tissa.music@gmail.com>
2019-12-17 13:48:57 -0500

    built component for group card list

---
+++ client/src/components/GroupsList/GroupsList.js
@@ -0,0 +1,37 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import GroupCard from '../GroupCard/GroupCard';
+import "./GroupsList.css";
+
+// Function based React Component
+const GroupsList = (props) => {
+  return (
+    <div className="Groups">
+      <h1>{props.heading}</h1>
+      <div className="Groups__list">
+        <GroupCard
+          name="DIY with your kids"
+          members="98"
+          location="Boston, MA"
+          picture="https://images.unsplash.com/photo-1453749024858-4bca89bd9edc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1007&q=80"
+        />
+        <GroupCard
+          name="Raising Teenagers"
+          members="29"
+          location="Mixed Location"
+          picture="https://images.unsplash.com/photo-1453749024858-4bca89bd9edc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1007&q=80"
+        />
+        <GroupCard
+          name="Virtual Bookclub"
+          members="106"
+          location="Online"
+          picture="https://images.unsplash.com/photo-1453749024858-4bca89bd9edc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1007&q=80"
+        />
+        <Link className="Groups__list__link"to={props.to}>{props.moreGroups}</Link>
+      </div>
+    </div>
+  )
+}
+
+export default GroupsList;
