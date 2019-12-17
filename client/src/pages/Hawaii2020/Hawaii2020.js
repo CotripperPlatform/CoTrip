176500471b6c3b8ca4939f30238ca3cf4bf212ab
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-12-17 16:44:03 -0500

    imported navbar, banner, and footer for the page

--- client/src/pages/Hawaii2020/Hawaii2020.js
+++ client/src/pages/Hawaii2020/Hawaii2020.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import "./Hawaii2020.css";
+import NavBar from "../../components/Navbar/Navbar";
+import Banner from "../../components/Banner/Banner";
+import Banner__pink from "../../components/Banner/Banner__pink.png"
+import Footer from "../../components/Footer/Footer";
 
 const Hawaii2020 = (props) => {
   let classList = `Hawaii2020`;
 
   return(
     <div className={classList}>
-      Hawaii2020
+      <NavBar/>
+      <Banner background={Banner__pink}>
+        <h3>Trip: Hawaii 2020</h3>
+      </Banner>
+      <Footer />
     </div>
   );
 }
