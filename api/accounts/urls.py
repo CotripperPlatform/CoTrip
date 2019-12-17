7c3cf573f06368d3c4a049794799407aad29f423
Tory Klingenstein <toryklingenstein@gmail.com>
2019-12-17 09:24:07 -0500

    remove unused files, urls, and dependencies

--- api/accounts/urls.py
+++ api/accounts/urls.py
@@ -5,11 +5,11 @@ from knox import views as knox_views
 from .views import ProfileList, ProfileDetail
 
 urlpatterns = [
-    path('api/auth', include('knox.urls')),
-    path('api/auth/register', RegisterAPI.as_view()),
-    path('api/auth/login', LoginAPI.as_view()),
-    path('api/auth/user', UserAPI.as_view()),
-    path('api/auth/logout', knox_views.LogoutView.as_view(), name='knox_logout'),
-    path('api/profile', ProfileList.as_view(), name='profile_list'),
-    path('api/profile/<int:pk>', ProfileDetail.as_view(), name='profile_detail')
+    path('auth', include('knox.urls')),
+    path('auth/register', RegisterAPI.as_view()),
+    path('auth/login', LoginAPI.as_view()),
+    path('auth/user', UserAPI.as_view()),
+    path('auth/logout', knox_views.LogoutView.as_view(), name='knox_logout'),
+    path('profile', ProfileList.as_view(), name='profile_list'),
+    path('profile/<int:pk>', ProfileDetail.as_view(), name='profile_detail')
 ]
