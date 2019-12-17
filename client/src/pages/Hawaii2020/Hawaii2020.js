cd6bf2f50c257bf251e474b534282db79b271d8f
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-12-17 16:56:50 -0500

    added button and a placeholder for the ActivityList component

--- client/src/pages/Hawaii2020/Hawaii2020.js
+++ client/src/pages/Hawaii2020/Hawaii2020.js
@@ -4,6 +4,7 @@ import NavBar from "../../components/Navbar/Navbar";
 import Banner from "../../components/Banner/Banner";
 import Banner__pink from "../../components/Banner/Banner__pink.png"
 import TripDetails from "../../components/TripDetails/TripDetails";
+import Button from "../../components/Button/Button";
 import Footer from "../../components/Footer/Footer";
 
 const Hawaii2020 = (props) => {
@@ -14,6 +15,10 @@ const Hawaii2020 = (props) => {
         <h3>Trip: Hawaii 2020</h3>
       </Banner>
       <TripDetails />
+      <div>ActivityList component here</div>
+      <div className="Hawaii2020__button-container">
+        <Button text="Book Now" color="pink" size="large" />
+      </div>
       <Footer />
     </div>
   );
