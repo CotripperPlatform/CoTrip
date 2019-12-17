3ff6d6f49dfed4ea65beb51ee30acaccfe15627a
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-12-17 10:11:14 -0500

    added styling directly to the tags

--- client/src/components/Banner/Banner.js
+++ client/src/components/Banner/Banner.js
@@ -2,12 +2,8 @@ import React from "react";
 import "./Banner.css";
 
 const Banner = props => {
-  let classlist = "Banner";
-  return <div className={classlist} style={{
-    backgroundImage: `url(${props.background})`,
-    height: `${props.Height}`, fontSize: `${props.FontSize}`, lineHeight: `${props.LineHeight}`,
-    textAlign: `${props.TextAlign}`, display: `${props.display}`, flexDirection: `${props.flexdirection}`,
-    justifyContent: `${props.justifycontent}`, alignItems: `${props.alignitems}`
+  return <div className="Banner" style={{
+    backgroundImage: `url(${props.background})`
   }}>
     {props.children}
   </div>;
--- client/src/components/Banner/Banner.stories.js
+++ client/src/components/Banner/Banner.stories.js
@@ -15,80 +15,78 @@ const handleClick = e => {
 storiesOf("Banner", module)
   .add("Home Banner", () => <Banner background={Banner__purpleBig}/>)
   .add("Home Banner with Welcome text", () =>
-    <Banner background={Banner__purpleBig} Height='380px' FontSize='80px' LineHeight='120px' display='flex' flexdirection='column'
-    justifycontent='center' alignitems='center'>
+    <Banner background={Banner__purpleBig}>
         <h2>Welcome, Justine</h2>
     </Banner>
   )
   .add("Home Banner with Welcome Back text", () =>
-    <Banner background={Banner__purpleBig} Height='380px' FontSize='80px' LineHeight='120px' TextAlign="left" display='flex' flexdirection='column'
-    justifycontent='center' alignitems='center'> 
-      <h2>Welcome Back,<br />Justine.</h2>
+    <Banner background={Banner__purpleBig}> 
+      <h2 style={{textAlign: 'left', display: 'inline-block'}}>Welcome Back,<br />Justine.</h2>
     </Banner>
   )
 
   .add("Community Banner", () => <Banner background={Banner__purpleBig}/>)
   .add("Community Banner with input People", () =>
     <Banner background={Banner__purpleBig}>
-        <h3 className="Banner__header3"> Community: People </h3>
+        <h3 style={{margin: 0}}> Community: People </h3>
         <InputTextField type="text" variation="search" name="search directory" placeholder="Search My Directory" />
     </Banner>
   )
   .add("Community Banner with input Groups", () =>
     <Banner background={Banner__purpleBig}>
-        <h3 className="Banner__header3">Community: Groups</h3>
+        <h3 style={{margin: 0}}>Community: Groups</h3>
         <InputTextField type="text" variation="search" name="search directory" placeholder="Search My Directory" />
     </Banner>
   )
   .add("Community Banner with input and yellow button", () =>
     <Banner background={Banner__purpleBig}>     
-        <h3 className="Banner__header3">Group: Moms in DC</h3>
+        <h3 style={{margin: 0}}>Group: Moms in DC</h3>
         <InputTextField type="text" variation="search" name="search directory" placeholder="Search My Directory" />
         <Button text="Join" color="yellow" size="small" handleClick={handleClick} />
     </Banner>
   )
   .add("Community Banner with input and pink button", () =>
     <Banner background={Banner__purpleBig}>
-        <h3 className="Banner__header3">Group: Moms in DC</h3>
+        <h3 style={{margin: 0}}>Group: Moms in DC</h3>
         <InputTextField type="text" variation="search" name="search directory" placeholder="Search My Directory" />
         <Button text="Leave Group" color="pink" size="small" handleClick={handleClick} />
     </Banner>
   )
   .add("Community Banner with Forum: My Favorites Input", () =>
     <Banner background={Banner__purpleBig}>
-        <h3 className="Banner__header3">Forum: My Favorites</h3>
+        <h3 style={{margin: 0}}>Forum: My Favorites</h3>
         <InputTextField type="text" variation="search" name="search directory" placeholder="Search Topics and #hashtags" />
     </Banner>
   )
   .add("Community Banner with Forum: Discover Topics Input", () =>
     <Banner background={Banner__purpleBig}>
-        <h3 className="Banner__header3">Forum: Discover Topics</h3>
+        <h3 style={{margin: 0}}>Forum: Discover Topics</h3>
         <InputTextField type="text" variation="search" name="search directory" placeholder="Search Topics and #hashtags" />
     </Banner>
   )
   .add("Community Banner with Topic Search Input", () =>
     <Banner background={Banner__purpleBig}>
-        <h3 className="Banner__header3">Search: Packing</h3>
+        <h3 style={{margin: 0}}>Search: Packing</h3>
         <InputTextField type="text" variation="search" name="search directory" placeholder="Packing" />
     </Banner>
   )
   .add("Community Banner with Search in Topic Input and pink Favorited button", () =>
     <Banner background={Banner__purpleBig}>
-        <h3 className="Banner__header3">Topic: Parenting</h3>
+        <h3 style={{margin: 0}}>Topic: Parenting</h3>
         <InputTextField type="text" variation="search" name="search directory" placeholder="Search in Topic: Parenting" />
         <Button text="Favorited" color="pink" size="small" handleClick={handleClick} />
     </Banner>
   )
   .add("Community Banner with #hashtag Search Input and yellow follow button", () =>
     <Banner background={Banner__purpleBig}>
-        <h3 className="Banner__header3">#packinglight</h3>
+        <h3 style={{margin: 0}}>#packinglight</h3>
         <InputTextField type="text" variation="search" name="search directory" placeholder="Search in #packinglight" />
         <Button text="Follow #hashtag" color="yellow" size="small" handleClick={handleClick} />
     </Banner>
   )
   .add("Community Banner with #hashtag Search Input and pink followed button", () =>
     <Banner background={Banner__purpleBig}>
-        <h3 className="Banner__header3">#packinglight</h3>
+        <h3 style={{margin: 0}}>#packinglight</h3>
         <InputTextField type="text" variation="search" name="search directory" placeholder="Search in #packinglight" />
         <Button text="Followed" color="pink" size="small" handleClick={handleClick} />
     </Banner>
@@ -97,32 +95,31 @@ storiesOf("Banner", module)
 
   .add("Profile Banner", () => <Banner background={Banner__pink}/>)
   .add("Profile Banner with Text", () =>
-    <Banner background={Banner__pink} display='flex' flexdirection='column'
-    justifycontent='center' alignitems='center'>
+    <Banner background={Banner__pink}>
         <h3>Trip: Hawaii 2020</h3>
     </Banner>
   )
   .add("Profile Banner with Search Directory Input", () =>
     <Banner background={Banner__pink}>
-        <h3 className="Banner__header3">User Profile</h3>
+        <h3 style={{margin: 0}}>User Profile</h3>
         <InputTextField type="text" variation="search" name="search directory" placeholder="Search My Directory" />
     </Banner>
   )
   .add("Profile Banner with Directory: My Friends Search Input", () =>
     <Banner background={Banner__pink}>
-        <h3 className="Banner__header3">Directory: My Friends</h3>
+        <h3 style={{margin: 0}}>Directory: My Friends</h3>
         <InputTextField type="text" variation="search" name="search directory" placeholder="Search My Directory" />
     </Banner>
   )
   .add("Profile Banner with Directory: My Groups Search Input", () =>
     <Banner background={Banner__pink}>
-        <h3 className="Banner__header3">Directory: My Groups</h3>
+        <h3 style={{margin: 0}}>Directory: My Groups</h3>
         <InputTextField type="text" variation="search" name="search directory" placeholder="Search My Directory" />
     </Banner>
   )
   .add("Profile Banner with Search Trip input", () =>
     <Banner background={Banner__pink}>
-        <h3 className="Banner__header3">Book a Trip</h3>
+        <h3 style={{margin: 0}}>Book a Trip</h3>
         <InputTextField type="text" variation="search" name="search directory" placeholder="Search Trip"/>
     </Banner>
   )
