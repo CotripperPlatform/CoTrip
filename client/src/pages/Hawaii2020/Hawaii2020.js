94fc66131194728f839c0c432a9ccda66440e78c
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-12-17 16:38:00 -0500

    generated Hawaii2020 page component using Hygen

---
+++ client/src/pages/Hawaii2020/Hawaii2020.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Hawaii2020.css";
+
+const Hawaii2020 = (props) => {
+  let classList = `Hawaii2020`;
+
+  return(
+    <div className={classList}>
+      Hawaii2020
+    </div>
+  );
+}
+
+export default Hawaii2020;
---
+++ client/src/pages/Hawaii2020/Hawaii2020.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Hawaii2020 from './Hawaii2020';
+
+storiesOf('Hawaii2020', module)
+  .add("Default", () => (
+    <Hawaii2020 />
+  )
+)
