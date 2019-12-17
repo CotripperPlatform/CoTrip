404e7a9955131aa97710fe4317e23d5e69b0bcb9
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-12-17 16:51:00 -0500

    added TripDetails component

--- client/src/pages/Hawaii2020/Hawaii2020.js
+++ client/src/pages/Hawaii2020/Hawaii2020.js
@@ -3,17 +3,17 @@ import "./Hawaii2020.css";
 import NavBar from "../../components/Navbar/Navbar";
 import Banner from "../../components/Banner/Banner";
 import Banner__pink from "../../components/Banner/Banner__pink.png"
+import TripDetails from "../../components/TripDetails/TripDetails";
 import Footer from "../../components/Footer/Footer";
 
 const Hawaii2020 = (props) => {
-  let classList = `Hawaii2020`;
-
   return(
-    <div className={classList}>
+    <div className="Hawaii2020">
       <NavBar/>
       <Banner background={Banner__pink}>
         <h3>Trip: Hawaii 2020</h3>
       </Banner>
+      <TripDetails />
       <Footer />
     </div>
   );
