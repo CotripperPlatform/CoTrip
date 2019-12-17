02e55eb7cd0b113b665e11da3b00a743357f6acd
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-12-17 16:07:59 -0500

    completed setup for the trip banner image component

--- client/src/components/BannerImageCard/BannerImageCard.stories.js
+++ client/src/components/BannerImageCard/BannerImageCard.stories.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import BannerImageCard from './BannerImageCard';
+import Banner__Image from "./TripBanner.png";
 
 storiesOf('BannerImageCard', module)
   .add("Default", () => (
-    <BannerImageCard/>
+    <BannerImageCard background={Banner__Image}/>
   )
 )
