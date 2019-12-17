da0958b5587b5f2185a2887c587c05074614d33f
Jenna Dean <jenna.dean9@gmail.com>
2019-12-17 15:56:08 -0500

    gitignored image dir

--- api/api/settings.py
+++ api/api/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'trip',
     'rest_framework'
 ]
