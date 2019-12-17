987df210709da38f858566608a22631fa5626e95
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-12-17 17:10:01 -0500

    made the component responsive

--- client/src/components/BannerImageCard/BannerImageCard.js
+++ client/src/components/BannerImageCard/BannerImageCard.js
@@ -6,7 +6,7 @@ const BannerImageCard = (props) => {
     <div className='BannerImageCard' style={{
       backgroundImage: `url(${props.background})`
     }}>
-      <h2 style={{ margin: 0, marginTop: "8rem", marginRight: "2rem" }}>{props.message}</h2>
+      <h2>{props.message}</h2>
     </div>
   );
 }
