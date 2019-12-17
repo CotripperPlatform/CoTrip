bc71a09af6c9c21bf1a6d3b06b930e86b02bffee
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-12-17 15:53:13 -0500

    updated media queries for more responsiveness.

--- client/src/components/BookATripImage/BookATripImage.js
+++ client/src/components/BookATripImage/BookATripImage.js
@@ -10,10 +10,10 @@ const BookATripImage = (props) => {
       <div className="BookATripImage__image-group">
         {cards.map(card => {
           return (
-            <div className="BookATripImage__image-container">
-              <img src={require(`../../assets/images/${card.image}.png`)} className="BookATripImage__image"></img>
-              <div><Link to={card.url} className="BookATripImage__link">{card.description}</Link></div>
-            </div>
+              <div className="BookATripImage__image-container">
+                <img src={require(`../../assets/images/${card.image}.png`)} className="BookATripImage__image"></img>
+                <div><Link to={card.url} className="BookATripImage__link">{card.description}</Link></div>
+              </div>
           )
         })}
       </div>
