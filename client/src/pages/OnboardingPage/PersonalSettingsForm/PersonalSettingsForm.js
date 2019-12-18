eef6a6c0b357f1a1a305f1359b6e57c78ce2ad33
Tissa Khosla <tissa.music@gmail.com>
2019-12-18 14:11:18 -0500

    added functionality for upliad image, display profilePicture  - next steps:  - style page

--- client/src/pages/OnboardingPage/PersonalSettingsForm/PersonalSettingsForm.js
+++ client/src/pages/OnboardingPage/PersonalSettingsForm/PersonalSettingsForm.js
@@ -42,7 +42,7 @@ class PersonalSettingsForm extends Component {
       <div className="OnboardingPage__wrapper">
         <h1 className="OnboardingPage__text">Step Three: Personalize Your Profile (optional)</h1>
         <div className="OnboardingPage__form-container">
-          {this.state.img === "" ? (
+          {this.state.profile.img === "" ? (
             <FileUpload
               header="Upload a profile photo"
               buttonLabel="Browse Files"
@@ -51,12 +51,12 @@ class PersonalSettingsForm extends Component {
                 evt.persist();
                 console.log(evt);
                 let imageUrl = URL.createObjectURL(evt.target.files[0]);
-                this.setState({ img: imageUrl });
+                this.setState({ profile: { ...this.state.profile, img: imageUrl }});
                 return imageUrl;
               }}
             ></FileUpload>
           ) : (
-            <ProfilePicture type="medium" image={this.state.img}></ProfilePicture>
+            <ProfilePicture type="medium" image={this.state.profile.img}></ProfilePicture>
           )}
 
           <InputTextField
