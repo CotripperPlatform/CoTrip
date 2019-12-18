0154144850e55b98cea12555aedea6db134b75fa
Ren Lucas <llucas314@gmail.com>
2019-12-18 11:33:25 -0500

    updated pics and names

--- client/src/components/InboxMessage/InboxMessage.stories.js
+++ client/src/components/InboxMessage/InboxMessage.stories.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import InboxMessage from "./InboxMessage";
-import Image from "assets/images/profile_large.png";
+import Image from "assets/images/profile-picture-1.png";
 
 storiesOf("InboxMessage", module).add("Default", () => (
-  <InboxMessage profilePic={Image} name={"Raymond"} message={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod..."} />
+  <InboxMessage
+  // profilePic={Image}
+  // name={"Raymond"}
+  // message={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod..."}
+  />
 ));
