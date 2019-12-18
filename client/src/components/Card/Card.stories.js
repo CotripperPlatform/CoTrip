ab8aba19dcd1d430d5184837ba2047b8e39630e2
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-17 22:38:26 -0500

    adding text to stories for card so they're visable

--- client/src/components/Card/Card.stories.js
+++ client/src/components/Card/Card.stories.js
@@ -8,8 +8,16 @@ import trends from "../../assets/images/trending-hashtags.png";
 import groups from "../../assets/images/protest.png";
 
 storiesOf("Card", module)
-  .add("Card, Purple", () => <Card color="purple" size="large" />)
-  .add("Card, Purple, Small", () => <Card color="purple" size="small" />)
+  .add("Card, Purple", () => (
+    <Card color="purple" size="large">
+      <span>hello</span>
+    </Card>
+  ))
+  .add("Card, Purple, Small", () => (
+    <Card color="purple" size="small">
+      <span>hello</span>
+    </Card>
+  ))
   .add("Card, Pink", () => <Card color="pink" size="large" />)
   .add("Card, Pink, Small", () => <Card color="pink" size="small" />)
   .add("Card, Blank", () => <Card size="large" />)
