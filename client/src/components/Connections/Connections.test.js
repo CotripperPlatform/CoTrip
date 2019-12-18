a3c65bbc423aa7a42f5ab63c70e21a9bdaf9ff63
Ren Lucas <llucas314@gmail.com>
2019-12-18 15:24:19 -0500

    made changes to have unique keys

--- client/src/components/Connections/Connections.test.js
+++ client/src/components/Connections/Connections.test.js
@@ -1,16 +1,79 @@
-import React from 'react'
-import { shallow, configure } from 'enzyme'
-import Connections from './Connections.js'
-import Adapter from 'enzyme-adapter-react-16';
-configure({adapter: new Adapter()});
+import React from "react";
+import { shallow } from "enzyme";
+import Connections from "./Connections.js";
+import pic1 from "../../assets/images/profile-picture-1.png";
+import pic2 from "../../assets/images/profile-picture-2.png";
+import pic3 from "../../assets/images/profile-picture-3.png";
+import pic4 from "../../assets/images/profile-picture-4.png";
+import pic5 from "../../assets/images/profile-picture-5.png";
 
-// We will describe a block of tests
-describe('Connections component', () => {
-	// we will write one individual test
-  it('should render as expected', () => {
-    // Shallow rendering renders a component without rendering any of its children
-    const component = shallow(<Connections />)
-    // We create an assertion within the test that checks if our component renders our name prop
-    // expect(component.contains('Your name')).toBe(true)
-  })
-})
+const setUp = props => {
+  const component = shallow(<Connections {...props} />);
+  return component;
+};
+describe("Connections component", () => {
+  describe("Have props", () => {
+    it("Should render without errors", () => {
+      const props = {
+        users: [
+          {
+            userId: 1,
+            userFirstName: "Paula",
+            userSurname: "Bannerman",
+            userPic: pic1
+          },
+          {
+            userId: 2,
+            userFirstName: "Jack",
+            userSurname: "Johnson",
+            userPic: pic2
+          },
+          {
+            userId: 3,
+            userFirstName: "Jenny",
+            userSurname: "Jones",
+            userPic: pic3
+          },
+          {
+            userId: 4,
+            userFirstName: "Joan",
+            userSurname: "Rivers",
+            userPic: pic4
+          },
+          {
+            userId: 5,
+            userFirstName: "Freida",
+            userSurname: "Mercury",
+            userPic: pic5
+          },
+          {
+            userId: 6,
+            userFirstName: "Leslie",
+            userSurname: "Knope",
+            userPic: pic5
+          },
+          {
+            userId: 7,
+            userFirstName: "Frankie",
+            userSurname: "Ocean",
+            userPic: pic4
+          }
+        ]
+      };
+      let wrapper = setUp(props);
+      const component = wrapper.find(".Connections");
+      expect(component.length).toBe(1);
+    });
+  });
+
+  describe("Have NO props", () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = setUp();
+    });
+    it("Should not render", () => {
+      const component = wrapper.find(".Connections");
+      expect(component.length).toBe(1);
+    });
+  });
+});
