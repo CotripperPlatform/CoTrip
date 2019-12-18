a6f7c58f165acc8161c08c160e9da7c75f639ae2
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-12-18 14:39:25 -0500

    added components

--- client/src/pages/Hawaii2020/Hawaii2020.js
+++ client/src/pages/Hawaii2020/Hawaii2020.js
@@ -4,10 +4,21 @@ import NavBar from "../../components/Navbar/Navbar";
 import Banner from "../../components/Banner/Banner";
 import Banner__pink from "../../components/Banner/Banner__pink.png"
 import TripDetails from "../../components/TripDetails/TripDetails";
+import ActivityList from "../../components/ActivityList/ActivityList";
 import Button from "../../components/Button/Button";
 import Footer from "../../components/Footer/Footer";
 
 const Hawaii2020 = (props) => {
+  let momActivities = ["Spa", "Wine Nights", "Brunch with Other Moms"];
+  let kidsActivities = [
+    "Snorkeling",
+    "Hiking",
+    "Island Hopping",
+    "Hawaiian Luaus",
+    "Food Markets",
+    "Beaches"
+  ];
+
   return(
     <div className="Hawaii2020">
       <NavBar/>
@@ -15,7 +26,8 @@ const Hawaii2020 = (props) => {
         <h3>Trip: Hawaii 2020</h3>
       </Banner>
       <TripDetails />
-      <div>ActivityList component here</div>
+      <ActivityList title="Activities for Moms" activities={momActivities} color="purple" />
+      <ActivityList title="Activities for Moms & Kids" activities={kidsActivities} color="red" />
       <div className="Hawaii2020__button-container">
         <Button text="Book Now" color="pink" size="large" />
       </div>
