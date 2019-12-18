a09b6f4bc77e49301fb772d3313917e91e72dff8
karenheyn <karenheyn@gmail.com>
2019-12-18 11:45:26 -0500

    resolved errors

--- client/src/components/Bio/Bio.js
+++ client/src/components/Bio/Bio.js
@@ -12,22 +12,24 @@ const Bio = props => {
           {props.isCurrentUser ? <Icon icon={"edit"} /> : null}
         </h2>
         <h3 className="Bio__subheader">Bio</h3>
-        <p>{props.bio}</p>
+        <p className="Bio__paragraph">{props.bio}</p>
         <h3 className="Bio__interests">Her Interests</h3>
-        <div classname="hashtag__container">
+        <div className="hashtag__container">
           {props.hashtags.map(hashtag => (
-            <span classname="Bio__span">{hashtag}</span>
+            <span className="Bio__span" key={hashtag + 1}>
+              {hashtag}
+            </span>
           ))}
         </div>
         <div className="icons__div">
           <Link to={props.instagram}>
-            <Icon onclick={props.handleclick} icon={["fab", "instagram"]} />
+            <Icon onClick={props.onClick} icon={["fab", "instagram"]} />
           </Link>
           <Link to={props.pinterest}>
-            <Icon onclick={props.handleclick} icon={["fab", "pinterest"]} />
+            <Icon onClick={props.onClick} icon={["fab", "pinterest"]} />
           </Link>
           <Link to={props.facebook}>
-            <Icon onclick={props.handleclick} icon={["fab", "facebook"]} />
+            <Icon onClick={props.onClick} icon={["fab", "facebook"]} />
           </Link>
         </div>
       </div>
--- client/src/components/Bio/Bio.stories.js
+++ client/src/components/Bio/Bio.stories.js
@@ -7,7 +7,7 @@ let people = [
     name: "Suzie",
     bio:
       "Nec et iriure utamur, per ubique mnesarchum an. At mei exerci voluptaria suscipiantur, ornatus consulatu interesset mei id. Ea possim percipit recteque his. Mea id ludus alterum apeirian, alienum dissentiet ut estLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    hashtags: ["#shred", "#the", "#pow", "#pow"],
+    hashtags: ["#parenting", "#teenages", "#health&wellness", "#Hawaii2020"],
     facebook: "www.facebook.com",
     instagram: "www.instagram.com",
     pinterest: "www.pinterest.com",
@@ -15,6 +15,7 @@ let people = [
   },
   { name: "Martha", bio: "sup yall" }
 ];
+
 storiesOf("Bio", module).add("Default", () => (
   <Bio
     instagram={people[0].intagram}
@@ -25,5 +26,6 @@ storiesOf("Bio", module).add("Default", () => (
     bio={people[0].bio}
     hashtags={people[0].hashtags}
     isCurrentUser={people[0].isUser}
+    onClick={console.log("Hello")}
   />
 ));
