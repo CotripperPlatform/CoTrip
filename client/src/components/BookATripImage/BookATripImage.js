5e01ff14451f64079938f3dc1fcfbf817da5da1a
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-12-18 11:07:19 -0500

    working on background images

--- client/src/components/BookATripImage/BookATripImage.js
+++ client/src/components/BookATripImage/BookATripImage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./BookATripImage.css";
 import { Link } from "react-router-dom";
+import image1 from "../../assets/images/hawaii.png";
 
 const BookATripImage = (props) => {
   let {cards} = props;
@@ -9,9 +10,11 @@ const BookATripImage = (props) => {
     <div className="BookATripImage">
       <div className="BookATripImage__image-group">
         {cards.map(card => {
+          console.log(card.image);
           return (
               <div className="BookATripImage__image-container">
-                <img src={require(`${card.image}`)} className="BookATripImage__image"></img>
+                {/* <img src={require(`${card.image}`)} className="BookATripImage__image"></img> */}
+                <div className="BookATripImage__image" style={{ backgroundImage: 'url(' + require(`${card.image}`) + ')' }}></div>
                 <div><Link to={card.url} className="BookATripImage__link">{card.description}</Link></div>
               </div>
           )
--- client/src/components/BookATripImage/BookATripImage.stories.js
+++ client/src/components/BookATripImage/BookATripImage.stories.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import BookATripImage from './BookATripImage';
+import image1 from "../../assets/images/hawaii.png";
+import image2 from "../../assets/images/greece.png";
+import image3 from "../../assets/images/southernCaribbean.png";
+import image4 from "../../assets/images/easternCaribbean.png";
 
 storiesOf('BookATripImage', module)
+//   .add("Default", () => <BookATripImage
+//     cards={[
+//       {"image": "../../assets/images/hawaii.png", "url": "#", "description": "Hawaii May 2020"},
+//       {"image": "../../assets/images/greece.png", "url": "#", "description": "Greece & Croatia Cruise June 2020"},
+//       {"image": "../../assets/images/southernCaribbean.png", "url": "#", "description": "Southern Caribbean Cruise July 2020"},
+//       {"image": "../../assets/images/hawaii.png", "url": "#", "description": "Hawaii August 2020"},
+//       {"image": "../../assets/images/easternCaribbean.png", "url": "#", "description": "Eastern Caribbean Cruise New Year's 2021"}
+//     ]}
+// />)
+
   .add("Default", () => <BookATripImage
     cards={[
-      {"image": "../../assets/images/hawaii.png", "url": "#", "description": "Hawaii May 2020"},
-      {"image": "../../assets/images/greece.png", "url": "#", "description": "Greece & Croatia Cruise June 2020"},
-      {"image": "../../assets/images/southernCaribbean.png", "url": "#", "description": "Southern Caribbean Cruise July 2020"},
-      {"image": "../../assets/images/hawaii.png", "url": "#", "description": "Hawaii August 2020"},
-      {"image": "../../assets/images/easternCaribbean.png", "url": "#", "description": "Eastern Caribbean Cruise New Year's 2021"}
+      {"image": image1, "url": "#", "description": "Hawaii May 2020"},
+      {"image": image2, "url": "#", "description": "Greece & Croatia Cruise June 2020"},
+      {"image": image3, "url": "#", "description": "Southern Caribbean Cruise July 2020"},
+      {"image": image1, "url": "#", "description": "Hawaii August 2020"},
+      {"image": image4, "url": "#", "description": "Eastern Caribbean Cruise New Year's 2021"}
     ]}
 />)
