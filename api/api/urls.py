b827dc072982b5dab537db09f90e1574c78a93f6
Jenna Dean <jenna.dean9@gmail.com>
2019-12-18 15:05:38 -0500

    added accounts to project urls file

--- api/api/urls.py
+++ api/api/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cotrip_app.urls')),
+    path('', include('accounts.urls'))
 
 ]
 
