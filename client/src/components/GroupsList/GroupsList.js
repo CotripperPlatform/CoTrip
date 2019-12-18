ae9b2eff21057aadb33182c18bea099cfa5b76d5
Tissa Khosla <tissa.music@gmail.com>
2019-12-18 14:54:56 -0500

    styled responsiveness further with link desplaying always

--- client/src/components/GroupsList/GroupsList.js
+++ client/src/components/GroupsList/GroupsList.js
@@ -14,30 +14,32 @@ const GroupsList = (props) => {
       <h1>{props.heading}</h1>
       <div className="Groups__list">
         <div className="Groups__card--1">
-        <GroupCard
-          name="DIY with your kids"
-          members="98"
-          location="Boston, MA"
-          picture={cardImage}
-        />
+          <GroupCard
+            name="DIY with your kids"
+            members="98"
+            location="Boston, MA"
+            picture={cardImage}
+          />
         </div>
         <div className="Groups__card--2">
-        <GroupCard
-          name="Raising Teenagers"
-          members="29"
-          location="Mixed Location"
-          picture={cardImage3}
-        />
+          <GroupCard
+            name="Raising Teenagers"
+            members="29"
+            location="Mixed Location"
+            picture={cardImage3}
+          />
         </div>
         <div className="Groups__card--3">
-        <GroupCard
-          name="Virtual Bookclub"
-          members="106"
-          location="Online"
-          picture={cardImage2}
-        />
+          <GroupCard
+            name="Virtual Bookclub"
+            members="106"
+            location="Online"
+            picture={cardImage2}
+          />
         </div>
-        <Link className="Groups__list__link"to={props.to}>{props.moreGroups}</Link>
+        <div className="Groups__card--4">
+          <Link className="Groups__list__link" to={props.to}>{props.moreGroups}</Link>
+        </div>
       </div>
     </div>
   )
