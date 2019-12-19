b077e3047c72cfd159b98009517ef99e35135eba
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-18 21:57:49 -0500

    removing homepage file contents from branch

--- client/src/pages/HomePage/HomePage.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import HomePage from './HomePage';
-
-storiesOf('HomePage', module)
-  .add("Default", () => (
-    <HomePage />
-  )
-)
