5b9f7b8eb69316e325ceb71ec3ad81a5a4356a49
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-18 21:40:19 -0500

    added container to hold all content for group card

--- client/src/components/UpcomingTripsCard/UpcomingTripsCard.js
+++ client/src/components/UpcomingTripsCard/UpcomingTripsCard.js
@@ -8,19 +8,21 @@ const UpcomingTripsCard = props => {
   return (
     <Card color="pink" className="UpcomingTripsCard" size="large">
       <header className="UpcomingTripsCard__header">Upcoming Trips</header>
-      <div className="UpcomingTripsCard__info">
-        <TripCard src={props.src} location={props.location} date={props.date} />
-        <div className="UpcomingTripsCard__text-info">
-          <p className="UpcomingTripsCard__general-information">General Information:</p>
-          <ul className="UpcomingTripsCard__list">
-            {props.details.map(detail => {
-              return <li key={detail}>~{detail}</li>;
-            })}
-          </ul>
+      <div className="UpcomingTripsCard__contents-container">
+        <div className="UpcomingTripsCard__info">
+          <TripCard src={props.src} location={props.location} date={props.date} />
+          <div className="UpcomingTripsCard__text-info">
+            <p className="UpcomingTripsCard__general-information">General Information:</p>
+            <ul className="UpcomingTripsCard__list">
+              {props.details.map(detail => {
+                return <li key={detail}>~{detail}</li>;
+              })}
+            </ul>
+          </div>
         </div>
-      </div>
-      <div className="UpcomingTripsCard__link">
-        <Link to={props.url}>More Details</Link>
+        <div className="UpcomingTripsCard__link">
+          <Link to={props.url}>More Details</Link>
+        </div>
       </div>
     </Card>
   );
