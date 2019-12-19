88870baed1b04d06b71be86e983ee14e7ca122f5
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-19 16:00:51 -0500

    added tripname props to hawaii2020 component

--- client/src/pages/Hawaii2020/Hawaii2020.js
+++ client/src/pages/Hawaii2020/Hawaii2020.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import "./Hawaii2020.css";
 import NavBar from "components/Navbar/Navbar";
 import Banner from "components/Banner/Banner";
@@ -10,22 +10,24 @@ import ActivityList from "components/ActivityList/ActivityList";
 import Button from "components/Button/Button";
 import Footer from "components/Footer/Footer";
 
-const Hawaii2020 = () => {
+const Hawaii2020 = props => {
   let momActivities = ["Spa", "Wine Nights", "Brunch with Other Moms"];
   let kidsActivities = [
     "Snorkeling",
     "Hiking",
     "Island Hopping",
-    "Hawaiian Luaus",
+    "Dancing",
     "Food Markets",
     "Beaches"
   ];
 
-  return(
+  let { tripName } = props;
+
+  return (
     <div className="Hawaii2020">
       <NavBar />
       <Banner background={Banner__pink}>
-        <h3>Trip: Hawaii 2020</h3>
+        <h3>Trip: {tripName} 2020</h3>
       </Banner>
       <div className="Hawaii2020__BannerImageCard-container">
         <BannerImageCard background={Banner__Image} message="Hawaii 2020" />
@@ -36,14 +38,14 @@ const Hawaii2020 = () => {
           <ActivityList title="Activities for Moms" activities={momActivities} color="purple" />
         </div>
         <div className="Hawaii2020__ActivityList-container"></div>
-          <ActivityList title="Activities for Moms & Kids" activities={kidsActivities} color="red" />
-        </div>
-        <div className="Hawaii2020__button-container">
-          <Button text="Book Now" color="pink" size="large" />
-        </div>
+        <ActivityList title="Activities for Moms & Kids" activities={kidsActivities} color="red" />
+      </div>
+      <div className="Hawaii2020__button-container">
+        <Button text="Book Now" color="pink" size="large" />
+      </div>
       <Footer />
     </div>
   );
-}
+};
 
 export default Hawaii2020;
--- client/src/pages/Hawaii2020/Hawaii2020.stories.js
+++ client/src/pages/Hawaii2020/Hawaii2020.stories.js
@@ -1,9 +1,5 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import Hawaii2020 from './Hawaii2020';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Hawaii2020 from "./Hawaii2020";
 
-storiesOf('Hawaii2020', module)
-  .add("Default", () => (
-    <Hawaii2020 />
-  )
-)
+storiesOf("Hawaii2020", module).add("Default", () => <Hawaii2020 tripName={"Hawaii"} />);
