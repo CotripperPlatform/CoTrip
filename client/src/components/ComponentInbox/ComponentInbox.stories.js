450b8f70740e6348f4076cee1b90088067a73e5e
Sara Laffin <sarala@umich.edu>
2020-03-30 11:04:43 -0400

    deleted ComponentInbox. Component actually in Message

--- client/src/components/ComponentInbox/ComponentInbox.stories.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import ComponentInbox from './ComponentInbox';
-
-storiesOf('ComponentInbox', module)
-  .add("Default", () => ( <ComponentInbox /> ))
