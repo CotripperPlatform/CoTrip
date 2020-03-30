c69a1c9380d5af6a8693db6593a681e1f6dfe0d8
Brian Brown II <bkeithbii@gmail.com>
2020-03-30 12:43:19 -0400

    Changed name + component type

---
+++ client/src/components/OnboardingCredentials/OnboardingCredentials.js
@@ -0,0 +1,100 @@
+import React from "react";
+import "./OnboardingCredentials.css";
+import InputTextField from "../InputTextField/InputTextField.js";
+import Button from "../Button/Button.js";
+import OnboardingParent from "../OnboardingParent/OnboardingParent";
+
+function OnboardingOne(props) {
+  return (
+    <OnboardingParent>
+      <div className={this.state.classList}>
+        <form className="OnboardingOne__form" action="">
+          <h1 className="OnboardingOne__header">Step One</h1>
+          <div className="OnboardingOne__container">
+            <InputTextField type="text" name="name" placeholder="Name"></InputTextField>
+          </div>
+          <div className="OnboardingOne__container">
+            <InputTextField type="text" name="email" placeholder="Email"></InputTextField>
+          </div>
+          <div className="OnboardingOne__container">
+            <InputTextField type="text" name="password" placeholder="Password"></InputTextField>
+          </div>
+          <div className="OnboardingOne__container">
+            <InputTextField
+              className="OnboardingOne__container"
+              type="text"
+              name="confirm password"
+              placeholder="Confirm Password"
+            ></InputTextField>
+          </div>
+          <div className="OnboardingOne__button-container">
+            <Button
+              className="OnboardingOne__button"
+              text="Next"
+              size="large"
+              handleClick={() => this.props.handleClick}
+            ></Button>
+          </div>
+        </form>
+      </div>
+    </OnboardingParent>
+  );
+}
+
+export default OnboardingOne;
+
+// import React, { Component } from "react";
+// import "./OnboardingOne.css";
+// import InputTextField from "../InputTextField/InputTextField.js";
+// import Button from "../Button/Button.js";
+// import OnboardingParent from "../OnboardingParent/OnboardingParent";
+
+// class OnboardingOne extends Component {
+//   constructor(props) {
+//     super(props);
+//     console.log(props);
+
+//     this.state = {
+//       classList: "OnboardingOne"
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <OnboardingParent>
+//         <div className={this.state.classList}>
+//           <form className="OnboardingOne__form" action="">
+//             <h1 className="OnboardingOne__header">Step One</h1>
+//             <div className="OnboardingOne__container">
+//               <InputTextField type="text" name="name" placeholder="Name"></InputTextField>
+//             </div>
+//             <div className="OnboardingOne__container">
+//               <InputTextField type="text" name="email" placeholder="Email"></InputTextField>
+//             </div>
+//             <div className="OnboardingOne__container">
+//               <InputTextField type="text" name="password" placeholder="Password"></InputTextField>
+//             </div>
+//             <div className="OnboardingOne__container">
+//               <InputTextField
+//                 className="OnboardingOne__container"
+//                 type="text"
+//                 name="confirm password"
+//                 placeholder="Confirm Password"
+//               ></InputTextField>
+//             </div>
+//             <div className="OnboardingOne__button-container">
+//               <Button
+//                 className="OnboardingOne__button"
+//                 text="Next"
+//                 size="large"
+//                 handleClick={() => this.props.handleClick}
+//               ></Button>
+//             </div>
+//           </form>
+//         </div>
+//       </OnboardingParent>
+//     );
+//   }
+// }
+
+// export default OnboardingOne;
