460cb77d8528e7f08e3ff6e4c5d16eb426af7d55
Colleen_O'Brien <colleen3117@gmail.com>
2020-03-30 11:42:28 -0400

    added ProfilePicture component and edited story to have correct props

--- client/src/components/CommentBody/CommentBody.js
+++ client/src/components/CommentBody/CommentBody.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./CommentBody.css";
+import ProfilePicture from "./../ProfilePicture/ProfilePicture";
 
 // Function based React Component
 const CommentBody = props => {
   // Default Class to apply to Component
   // No props right now but when there is data we can use
   // this.props.comment and this.props.date
-  const { body, date, time } = props;
+  const { body, date, time, to, image } = props;
 
   return (
     <div className="CommentBody">
+      <ProfilePicture to={to} image={image} />
       <p>{body}</p>
       <div className="CommentBody__date">
         <p>
