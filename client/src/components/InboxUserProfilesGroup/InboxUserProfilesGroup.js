0736092e8461356a56d35db6b150c59634406561
Sara Laffin <sarala@umich.edu>
2020-03-30 15:58:36 -0400

    set up InboxUserProfileGroup component file structure

---
+++ client/src/components/InboxUserProfilesGroup/InboxUserProfilesGroup.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import "./InboxUserProfilesGroup.css";
+
+// Function based React Component
+const InboxUserProfilesGroup = (props) => {
+
+  // Default Class to apply to Component
+  let classList = `InboxUserProfilesGroup`;
+
+
+  return(
+    <div className={classList}>
+      InboxUserProfilesGroup
+    </div>
+  );
+}
+
+export default InboxUserProfilesGroup;
