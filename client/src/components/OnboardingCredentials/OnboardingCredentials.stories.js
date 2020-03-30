78a17a3324abb4b9d40b0ef54fe65e6ca8d85f37
Brian Brown II <bkeithbii@gmail.com>
2020-03-30 13:11:05 -0400

    updated variables to onboarding credentials

---
+++ client/src/components/OnboardingCredentials/OnboardingCredentials.stories.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import OnboardingCredentials from "./OnboardingCredentials";
+
+storiesOf("OnboardingCredentials", module).add("Default", () => <OnboardingCredentials />);
