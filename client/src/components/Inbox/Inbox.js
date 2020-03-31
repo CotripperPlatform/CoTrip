3556c13d44754cfbef711370822b0bec5cc9f4a3
Sara Laffin <sarala@umich.edu>
2020-03-31 11:57:22 -0400

    add in CSS variables from App.css

--- client/src/components/Inbox/Inbox.js
+++ client/src/components/Inbox/Inbox.js
@@ -1,19 +1,12 @@
-
-import React from 'react';
+import React from "react";
 import "./Inbox.css";
 
 // Function based React Component
-const Inbox = (props) => {
-
+const Inbox = props => {
   // Default Class to apply to Component
   let classList = `Inbox`;
 
-
-  return(
-    <div className={classList}>
-      Inbox
-    </div>
-  );
-}
+  return <div className={classList}>Inbox</div>;
+};
 
 export default Inbox;
