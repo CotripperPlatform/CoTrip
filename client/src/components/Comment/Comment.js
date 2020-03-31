bb7f5b1b6947f25c7e927ed81a860a1da41002a4
Colleen_O'Brien <colleen3117@gmail.com>
2020-03-31 11:21:39 -0400

    fixed naming

--- client/src/components/Comment/Comment.js
+++ client/src/components/Comment/Comment.js
@@ -10,21 +10,21 @@ const Comment = props => {
   const { body, date, time, to, image, name, likes, replies, type } = props;
 
   return (
-    <body className="Comment_body">
-      <section className="Comment_section Comment_section_1">
+    <body className="Comment">
+      <section className="Comment__column Comment__profile">
         <ProfilePicture type={type} to={to} image={image} />
-        <h4 className="Comment_h4">{name}</h4>
+        <h4 className="Comment__name">{name}</h4>
       </section>
-      <section className="Comment_section Comment_section_2">
-        <p className="Comment_section_2_p">{body}</p>
-        <div className="Comment_div">
-          <article className="Comment_article">{likes} Likes</article>
-          <article className="Comment_article">{replies} Replies</article>
+      <section className="Comment__column Comment__content">
+        <p className="Comment__body">{body}</p>
+        <div className="Comment__activities">
+          <article className="Comment__activity">{likes} Likes</article>
+          <article className="Comment__activity">{replies} Replies</article>
         </div>
       </section>
-      <section className="Comment_section Comment_section_3">
-        <p className="Comment_section_3_p">{date}</p>
-        <p className="Comment_section_3_p">{time}</p>
+      <section className="Comment__column Comment__timing--container">
+        <p className="Comment__timing">{date}</p>
+        <p className="Comment__timing">{time}</p>
       </section>
     </body>
   );
--- client/src/components/Comment/Comment.stories.js
+++ client/src/components/Comment/Comment.stories.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import Comment from "./Comment";
-import Image from "../../assets/images/profile_large.png";
+import Image from "../../assets/images/profile_default.svg";
 const body =
   "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore v eritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit";
 const date = "December 9";
