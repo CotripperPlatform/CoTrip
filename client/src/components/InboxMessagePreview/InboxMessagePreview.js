6048fb176f6a03b18227de6b13b1bf54c97fb801
Sara Laffin <sarala@umich.edu>
2020-03-30 22:42:27 -0400

    transfer content from Message to InboxMessagePreview

--- client/src/components/InboxMessagePreview/InboxMessagePreview.js
+++ client/src/components/InboxMessagePreview/InboxMessagePreview.js
@@ -1,19 +1,53 @@
-
-import React from 'react';
+import React from "react";
 import "./InboxMessagePreview.css";
+import defaultPic from "assets/images/profile-picture-1.png";
+import defaultPic2 from "assets/images/profile-picture-2.png";
+
+import InboxUserProfilesGroup from "../InboxUserProfilesGroup/InboxUserProfilesGroup";
 
 // Function based React Component
-const InboxMessagePreview = (props) => {
-
+const InboxMessagePreview = props => {
   // Default Class to apply to Component
   let classList = `InboxMessagePreview`;
 
+  // create list of names above message
+  let names = props.conversation.users[0].name;
+  if (props.conversation.users.length > 1) {
+    names = `${names}, ${props.conversation.users[1].name}`;
+  }
+  if (props.conversation.users.length > 2) {
+    names = `${names}, ...`;
+  }
 
-  return(
+  return (
     <div className={classList}>
-      InboxMessagePreview
+      <InboxUserProfilesGroup users={props.conversation.users} />
+      <div className="Message__conversation">
+        <div className="Message__name">{names}</div>
+        <div className="Message__message">{props.conversation.thread[0].message}</div>
+      </div>
     </div>
   );
-}
-
+};
+InboxMessagePreview.defaultProps = {
+  conversation: {
+    thread: [
+      {
+        message:
+          "Hey, girl! Just wanted to see if you are going on this trippppppp. Hey, girl! Just wanted to see if you are going on this trip.",
+        user: "props.users[0]"
+      },
+      {
+        message: "NO!",
+        user: "props.user[1]"
+      }
+    ],
+    users: [
+      { name: "Justine", profilePic: defaultPic, to: "/" },
+      { name: "Michelle", profilePic: defaultPic2, to: "/" },
+      { name: "Becky", profilePic: defaultPic, to: "/" },
+      { name: "Bola", profilePic: defaultPic, to: "/" }
+    ]
+  }
+};
 export default InboxMessagePreview;
--- client/src/components/InboxMessagePreview/InboxMessagePreview.stories.js
+++ client/src/components/InboxMessagePreview/InboxMessagePreview.stories.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import InboxMessagePreview from './InboxMessagePreview';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import InboxMessagePreview from "./InboxMessagePreview";
 
-storiesOf('InboxMessagePreview', module)
-  .add("Default", () => (
-    <InboxMessagePreview />
-  )
-)
+import {
+  conversation1,
+  conversation2,
+  conversation3,
+  conversation4,
+  conversation5,
+  conversation6
+} from "./inboxMessagePreviewStoriesData";
+
+storiesOf("InboxMessagePreview", module)
+  .add("No Props", () => <InboxMessagePreview />)
+  .add("1 Person", () => <InboxMessagePreview conversation={conversation1} />)
+  .add("2 People", () => <InboxMessagePreview conversation={conversation2} />)
+  .add("3 People", () => <InboxMessagePreview conversation={conversation3} />)
+  .add("4 People", () => <InboxMessagePreview conversation={conversation4} />)
+  .add("8 People", () => <InboxMessagePreview conversation={conversation5} />)
+  .add("15 People", () => <InboxMessagePreview conversation={conversation6} />);
