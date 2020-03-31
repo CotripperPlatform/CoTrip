0542966ddfb6af1b7a333e19fc936993e2ab363d
Sara Laffin <sarala@umich.edu>
2020-03-31 12:12:00 -0400

    set up messagePreviews variable to fill in InboxMessagePreview components

--- client/src/components/Inbox/Inbox.js
+++ client/src/components/Inbox/Inbox.js
@@ -6,12 +6,15 @@ const Inbox = props => {
   // Default Class to apply to Component
   let classList = `Inbox`;
 
+  let messagePreviews = <div>Message!</div>;
+
   return (
     <div className={classList}>
       <div className={classList + "__header"}>
         <div className={classList + "__header-text"}>Inbox</div>
         <div className={classList + "__header-icon"}>(icon)</div>
       </div>
+      <div className={classList + "__message-previews"}>{messagePreviews}</div>
     </div>
   );
 };
