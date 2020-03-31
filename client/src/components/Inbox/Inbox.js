fcc44153445cd60688d6fbec5ad5fd8bf1172e68
Sara Laffin <sarala@umich.edu>
2020-03-31 12:08:28 -0400

    set up header with Inbox text and placeholder for icon

--- client/src/components/Inbox/Inbox.js
+++ client/src/components/Inbox/Inbox.js
@@ -6,7 +6,14 @@ const Inbox = props => {
   // Default Class to apply to Component
   let classList = `Inbox`;
 
-  return <div className={classList}>Inbox</div>;
+  return (
+    <div className={classList}>
+      <div className={classList + "__header"}>
+        <div className={classList + "__header-text"}>Inbox</div>
+        <div className={classList + "__header-icon"}>(icon)</div>
+      </div>
+    </div>
+  );
 };
 
 export default Inbox;
