a8c332339f863fc7dccfd07cb2ab46ac9022f51c
Andrew Frazier <acorbettfrazier@gmail.com>
2020-03-31 11:50:30 -0400

    extra users added to Atoms

--- client/src/components/ExtraUsers/ExtraUsers.stories.js
+++ client/src/components/ExtraUsers/ExtraUsers.stories.js
@@ -5,10 +5,10 @@ import ExtraUsers from "./ExtraUsers";
 let anArray = [1, 2, 4, 9, 3, 8, 1, 7]; //a placeholder array of given length
 
 export default {
-  title: "Layout/ExtraUsers"
+  title: "Atoms/ExtraUsers"
 };
 
-storiesOf("Layout/ExtraUsers", module)
+storiesOf("Atoms/ExtraUsers", module)
   .add("4 Extra Users ", () => <ExtraUsers to="Inbox/Recipients" users={anArray} />)
   .add("2 Extra Users", () => <ExtraUsers to="Inbox/Recipients" users={anArray.slice(0, 6)} />)
   .add("No Extra Users", () => <ExtraUsers to="Inbox/Recipients" users={anArray.slice(0, 3)} />);
