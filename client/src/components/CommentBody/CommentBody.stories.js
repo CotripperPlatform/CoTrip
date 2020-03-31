9274108b84b7b15e85dddb6eb29fb060ad088717
Andrew Frazier <acorbettfrazier@gmail.com>
2020-03-31 12:23:57 -0400

    removed unwanted lines

--- client/src/components/CommentBody/CommentBody.stories.js
+++ client/src/components/CommentBody/CommentBody.stories.js
@@ -2,10 +2,6 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import CommentBody from "./CommentBody";
 
-export default {
-  title: "Layout/Comment"
-};
-
 const body =
   "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore v eritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit";
 const date = "December 9";
