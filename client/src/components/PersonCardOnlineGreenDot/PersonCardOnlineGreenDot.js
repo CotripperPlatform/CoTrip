73b11a6db4e73f8482e9016359fba5ac5df41ba2
Hisstori <browncryan@outlook.com>
2020-03-31 11:18:36 -0400

    Green dot creation and initial commit.

---
+++ client/src/components/PersonCardOnlineGreenDot/PersonCardOnlineGreenDot.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import "./PersonCardOnlineGreenDot.css";
+
+// Function based React Component
+const PersonCardOnlineGreenDot = (props) => {
+
+  // Default Class to apply to Component
+  let classList = `PersonCardOnlineGreenDot`;
+
+
+  return(
+    <div className={classList}>
+      <circle></circle>
+    </div>
+  );
+}
+
+export default PersonCardOnlineGreenDot;
