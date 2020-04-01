4d98afbd4f9b3ade2eb3db25c6b9d7a2f4905dff
Sara Laffin <sarala@umich.edu>
2020-04-01 15:11:28 -0400

    Revert "Merge branch 'component-icon' into component-inbox"
    This reverts commit b2e4cdb03c10fb7ad593a65734695dd989196e99, reversing
    changes made to ec67a2e734d0b6fa1a3f416416f161a530378699.

--- api/cotrip_app/models.py
+++ api/cotrip_app/models.py
@@ -6,3 +6,16 @@ class Topic(models.Model):
 
     def __str__(self):
         return self.title
+
+class Location(models.Model):
+    city = models.CharField(max_length=75)
+    state = models.CharField(max_length=30, null=True, blank=True)
+    country = models.CharField(max_length=50)
+    latitude = models.DecimalField(max_digits=22,
+    decimal_places=16)
+    longitude = models.DecimalField(max_digits=22,
+    decimal_places=16)
+    image = models.ImageField(default='default.jpg', upload_to='location_pics')
+
+    def __str__(self):
+        return self.city
--- client/src/components/Icon/Icon.js
+++ client/src/components/Icon/Icon.js
@@ -1,11 +1,13 @@
-import React, { Component } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, {Component} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import "./Icon.css";
 
-const Icon = props => (
+
+const Icon = (props) => (
   <div>
-    <FontAwesomeIcon icon={props.icon} size={props.size} onClick={props.onClick} />
+    <FontAwesomeIcon icon={ props.icon} size={props.size}/>
   </div>
-);
+)
+
 
 export default Icon;
