10cb5b2594b52b66a0dbf6f50ad43001d5e91fc4
Nate_Sanchez <NSanchez@usa.com>
2020-04-01 15:32:04 -0400

    PROJECT_DIR added setting

--- api/api/settings.py
+++ api/api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from datetime import timedelta
+PROJECT_DIR=os.path.dirname(__file__)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
