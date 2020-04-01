d94eebd96b860b16163a89c200652fc79025e7fd
Sara Laffin <sarala@umich.edu>
2020-04-01 00:43:17 -0400

    moved InboxUserProfileGroup to ProfilePictureGroup component

--- client/src/components/InboxUserProfilesGroup/InboxUserProfilesGroup.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import InboxUserProfilesGroup from './InboxUserProfilesGroup';
-
-storiesOf('InboxUserProfilesGroup', module)
-  .add("Default", () => (
-    <InboxUserProfilesGroup />
-  )
-)
