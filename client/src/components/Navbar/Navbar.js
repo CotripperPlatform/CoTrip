aed4dfff41a884c0ba44e90a72a1551672f29ee2
Sara Laffin <sarala@umich.edu>
2020-04-01 15:13:52 -0400

    Revert "add clickHandleInbox function to NavBar"
    This reverts commit af7416cb7a7748c4ef905b2a0545eae146c95228.

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -15,16 +15,14 @@ class Navbar extends Component {
     this.state = {
       condensedMenuActive: false,
       // menuItems are careated in order of the following array.
-      menuItems: ["My Directory", "Community", "Forum", "Book a Trip"]
+      menuItems: ["My Directory", "Community", "Forum", "Book a Trip"],
     };
   }
 
   clickHandler = () => {
-    console.log("Clicked!");
-  };
-  clickHandleInbox = () => {
-    console.log("Clicked on inbox!");
-  };
+    console.log("Clicked!")
+  }
+
   render() {
     return (
       <div className="Navbar">
@@ -45,13 +43,15 @@ class Navbar extends Component {
             {this.state.menuItems.map((item, key) => (
               <div className="Navbar__link-item" key={key}>
                 <NavLink text={item} to={"/book-a-trip"} menuList={[]} />
-                {this.props.page === key && <div className={`Navbar__triangle`}></div>}
+                {this.props.page === key && (
+                  <div className={`Navbar__triangle`}></div>
+                )}
               </div>
             ))}
           </div>
           <div className="Navbar__right">
             <Icon icon={"search"} size="2x" onClick={this.clickHandler} />
-            <Icon icon={["far", "comment-dots"]} size="2x" onClick={this.clickHandleInbox} />
+            <Icon icon={["far", "comment-dots"]} size="2x" onClick={this.clickHandler} />
             <ProfilePicture type="extra-small" to={this.props.to} image={this.props.profileImage} />
           </div>
         </div>
