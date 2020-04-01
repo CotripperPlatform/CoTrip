fe0f55875a2dece8006351bb7aa1d62b74beeb42
Sara Laffin <sarala@umich.edu>
2020-03-31 23:56:27 -0400

    found typo in ExtraUsers story

--- client/src/components/ExtraUsers/ExtraUsers.stories.js
+++ client/src/components/ExtraUsers/ExtraUsers.stories.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import ExtraUsers from './ExtraUsers';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import ExtraUsers from "./ExtraUsers";
 
-let anArray = [1,2,4,9,3,8,1,7] //a placeholder array of given length
+let anArray = [1, 2, 4, 9, 3, 8, 1, 7]; //a placeholder array of given length
 
-storiesOf('ExtraUsers', module)
-  .add("4 Extra Users ", () => <ExtraUsers to='Inbox/Recipients' users={anArray}/>)
-  .add("2 Extra Users", () => <ExtraUsers to='Inbox/Recipients' users={anArray.slice(0,6)}/>)
-  .add("No Extra Users", () => <ExtraUsers to='Inbox/Recipients' users={anArray.slice(0,3)}/>)
+storiesOf("ExtraUsers", module)
+  .add("6 Extra Users ", () => <ExtraUsers to="Inbox/Recipients" users={anArray} />)
+  .add("4 Extra Users", () => <ExtraUsers to="Inbox/Recipients" users={anArray.slice(0, 6)} />)
+  .add("No Extra Users", () => <ExtraUsers to="Inbox/Recipients" users={anArray.slice(0, 3)} />);
