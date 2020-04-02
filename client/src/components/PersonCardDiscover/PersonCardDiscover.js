5603fff9e957f2cfee2f87df2064604940213fa8
Hisstori <browncryan@outlook.com>
2020-04-01 23:53:41 -0400

    Styling begin person card discover

---
+++ client/src/components/PersonCardDiscover/PersonCardDiscover.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import "./PersonCardDiscover.css";
+
+// Function based React Component
+const PersonCardDiscover = (props) => {
+
+  // Default Class to apply to Component
+  let classList = `PersonCardDiscover`;
+
+
+  return(
+    <div className={classList}>
+      PersonCardDiscover
+    </div>
+  );
+}
+
+export default PersonCardDiscover;
