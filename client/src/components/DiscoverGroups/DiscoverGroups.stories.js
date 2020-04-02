54e27cca0996ba9ab94fb3eb34b74f2658597137
Hisstori <browncryan@outlook.com>
2020-04-01 22:03:31 -0400

    Removed files

--- client/src/components/DiscoverGroups/DiscoverGroups.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import DiscoverGroups from './DiscoverGroups';
-
-storiesOf('DiscoverGroups', module)
-  .add("Default", () => (
-    <DiscoverGroups />
-  )
-)
