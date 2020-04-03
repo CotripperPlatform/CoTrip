728d7d8490c5df9d7839258a6b54117a0051c94a
Sara Laffin <sarala@umich.edu>
2020-04-03 16:03:48 -0400

    add hashtag boolean field option to models.py

--- api/community/models.py
+++ api/community/models.py
@@ -25,6 +25,7 @@ class Event(models.Model):
 class Topic(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
+    hashtag = models.BooleanField()
 
     def __str__(self):
         return self.title
