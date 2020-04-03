558caba78e7a9322d9cfd2246ca109c363d3bf47
Sara Laffin <sarala@umich.edu>
2020-04-02 21:03:11 -0400

    add Group, Topic, Hashtag models. untested

--- api/community/admin.py
+++ api/community/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Group, Event, Topic, Hashtag
+from .models import Group, Event, Topic, Hashtag, Media
 
 # Register your models here.
 
@@ -7,3 +7,4 @@ admin.site.register(Group)
 admin.site.register(Event)
 admin.site.register(Topic)
 admin.site.register(Hashtag)
+admin.site.register(Media)
--- api/community/models.py
+++ api/community/models.py
@@ -3,13 +3,18 @@ from django.db import models
 # Create your models here.
 
 class Group(models.model):
-
-# Location: many to one with location model
-# Title:
-# Members: many to many with profile model
-# Events: one to many with events model
+    location = models.ForeignKey('trip.Location', on_delete=models.CASCADE, related_name='groups', null=True)
+    title = models.CharField(max_length=200)
+    members = models.ManyToManyField('accounts.CustomUser')
+# Events: one to many with events model 
+#         taken care of in Event model
 # Posts: one to many with post model
-# Media: One to many with media model
+#         (should be) taken care of in the Post model
+# Media: One to many with media model 
+#         not clear on media model yet
+
+    __str__(self):
+    return self.title
 
 class Event(models.model):
     #Do I need all events, past and future, when I query a group? No. therefore, add group in here and filter events by group.
@@ -21,19 +26,33 @@ class Event(models.model):
     def __str__(self):
         return self.title
 
-class Topic (models.model):
-# Title
-# Description
+class Topic(models.model):
+    # there is a Topic model already in cotrip_app, but I've added it here too because of issue request. Should be updated in accounts.Profile when location of Topic model is finalized 
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
+
 # Followers: Many to many with user
+#       taken care of in accounts.Profile
 # Post: one to many with post
-# Media: one to many with media
+#       (should be) taken care of in the Post model
+# Media: one to many with media 
+#       not clear on media model yet
 
 class Hashtag(models.model):
-# Title
-# Description
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
+
 # Posts: one to many with post
 # Media: many to many with media
 # Followers: many to many with user
+
+
 # Media
 # Image url
 # Hashtags
