f51fedecb10c042b160394ca5025a7858007d46f
vmipfdr <paynemw@yahoo.com>
2020-04-08 15:22:04 -0400

    setting up initial forum model to be seeded correctly, attempt 3

--- api/forum/models.py
+++ api/forum/models.py
@@ -1,39 +1,40 @@
 from django.db import models
 from accounts.models import Profile
 
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     time = models.DateTimeField(auto_now=True)
     body = models.CharField(max_length=1000)
     likes = models.IntegerField(default=0)
     author = models.ForeignKey(Profile,
-        on_delete=models.CASCADE)
-    
+                               on_delete=models.CASCADE, related_name='posts', null=True, blank=True)
 
     def __str__(self):
         return self.title
 
 
 class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments', null=True, blank=True)
     time = models.DateTimeField(auto_now=True)
     body = models.CharField(max_length=500)
     likes = models.IntegerField(default=0)
     author = models.ForeignKey(Profile,
-        on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post', null=True, blank=True)
+                               on_delete=models.CASCADE, related_name='comments', null=True, blank=True)
 
     def __str__(self):
         return self.body
-
+s
 
 class Reply(models.Model):
     time = models.DateTimeField(auto_now=True)
     body = models.CharField(max_length=500)
     likes = models.IntegerField(default=0)
     author = models.ForeignKey(Profile,
-        on_delete=models.CASCADE)
-    comments = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='comments', null=True, blank=True)
+                               on_delete=models.CASCADE, related_name='replies', null=True, blank=True)
+    comment = models.ForeignKey(
+        Comment, on_delete=models.CASCADE, related_name='replies', null=True, blank=True)
 
     def __str__(self):
         return self.body
-
