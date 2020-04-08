14cf3b256e3e150e12d8a9f4bc38b7b72eb5056b
Sara Laffin <sarala@umich.edu>
2020-04-08 17:20:49 -0400

    comment out whitelist to check file uploading again

--- api/api/settings.py
+++ api/api/settings.py
@@ -64,14 +64,14 @@ MIDDLEWARE = [
 ]
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-    'https://cotripper.netlify.com'
-]
-CORS_ORIGIN_REGEX_WHITELIST = [
-    'http://localhost:3000',
-    'https://cotripper.netlify.com'
-]
+# CORS_ORIGIN_WHITELIST = [
+#     'http://localhost:3000',
+#     'https://cotripper.netlify.com'
+# ]
+# CORS_ORIGIN_REGEX_WHITELIST = [
+#     'http://localhost:3000',
+#     'https://cotripper.netlify.com'
+# ]
 
 ROOT_URLCONF = 'api.urls'
 
