e790dc5df0dcfeb61502d8c1942e4a5ee146e79c
Colleen_O'Brien <colleen3117@gmail.com>
2020-04-09 13:51:37 -0400

    took out views and urls for social media bc not needed

--- api/accounts/urls.py
+++ api/accounts/urls.py
@@ -12,13 +12,5 @@ urlpatterns = [
     path('profile', ProfileList.as_view(), name='profile_list'),
     path('profile/<int:pk>', ProfileDetail.as_view(), name='profile_detail'),
     path('profile/update', ProfileUpdate.as_view(), name='profile_update'),
-    path('profile_social_media', ProfileSocialMediaList.as_view(),
-         name='profile_social_media_list'),
-    path('profile_social_media/<int:pk>', ProfileSocialMediaDetail().as_view(),
-         name='profile_social_media_detail'),
-    path('social_media_type', SocialMediaTypeList.as_view(),
-         name='social_media_type_list'),
-    path('social_media_type/<int:pk>', SocialMediaTypeDetail().as_view(),
-         name='social_media_type_detail'),
     path('sign_s3', sign_s3)
 ]
