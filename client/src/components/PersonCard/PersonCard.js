5d6d0f4b7334de64fef390914f9ff4ed1a680681
Hisstori <browncryan@outlook.com>
2020-04-10 00:23:26 -0400

    Refactoring to pass dot as prop.

--- client/src/components/PersonCard/PersonCard.js
+++ client/src/components/PersonCard/PersonCard.js
@@ -1,29 +1,31 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./PersonCard.css";
-import PersonCardOnlineGreenDot from "../PersonCardOnlineGreenDot/PersonCardOnlineGreenDot"
 import ProfilePicture from "../ProfilePicture/ProfilePicture";
 import image from "../../assets/images/profile-picture-1.png";
+import { render } from "enzyme";
 
 // Function based React Component
-const PersonCard = props => {
-    return (
-      <div className="PersonCard">
-        <ProfilePicture type="medium" image={props.image} />
-        <div className="PersonCard__name">{props.name}</div>
-        <div className="PersonCard__location">{props.location}</div>
-        <div className="PersonCard__interests">
-          {/* <PersonCardOnlineGreenDot/> */}
-          {props.interests.map((interests, i) => (
-            <Link className="PersonCard__interests-links" to={interests.url} key={i}>
-              #{interests.interests}
-            </Link>
-          ))}
-        </div>
+const PersonCard = (props) => {
+  if (status) {
+    return props.online && (
+    <div className="PersonCard">
+      <ProfilePicture type="medium" image={props.image} />
+      <div className="PersonCard__name">{props.name}</div>
+      <div className="PersonCard__location">{props.location}</div>
+      <div className="PersonCard__interests">
+        {props.interests.map((interests, i) => (
+          <Link className="PersonCard__interests-links" to={interests.url} key={i}>
+            #{interests.interests}
+          </Link>
+        ))}
+        {/* { props.online || null} */}
       </div>
-    );
+    </div>
+  );
 };
 PersonCard.defaultProps = {
+  online: <div className="PersonCard__dot"></div>,
   image: { image },
   name: "Lindsay L.",
   location: "Washington D.C.",
@@ -31,13 +33,11 @@ PersonCard.defaultProps = {
     { url: "#", interests: "veganism" },
     { url: "#", interests: "budgeting" },
     { url: "#", interests: "travel" },
-    { url: "#", interests: "outdoors" }
+    { url: "#", interests: "outdoors" },
   ],
-  // dot: <div className="PersonCard__online"></div>,
 };
 
-// Once user logs in online should be set to true and display green dot.
-const logged_in = false;
-// ReactDOM.render(<PersonCard online={false} />,
+const status = false;
+// ReactDOM.render(<PersonCard loggedIn />,
 //   document.getElementById('root'))
 export default PersonCard;
