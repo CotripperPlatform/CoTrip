8617618456ff6f009a7665bc5b2fcad7b65cbca5
shimin <thunderous_0518@hotmail.com>
2020-05-11 15:58:15 -0400

    add a route for upcoming trips

--- api/trip/urls.py
+++ api/trip/urls.py
@@ -4,12 +4,14 @@ from .views import *
 urlpatterns = [
     path('location', LocationList.as_view(), name='location_list'),
     # path('trip', TripList.as_view(), name='trip_list'),
-    # change the trip list route trip to trips
+    
+    # 1. Change the trip list route name from 'trip' to 'trips'
+    # 2. Add the search feature for the list route, user can search list by title, start_date, end_date
+    #    When searching by condition, the api url format: trips?title=XXX&start_date=XXX&end_date=XXX
     path('trips', TripList.as_view(), name='trip_list'),
 
-    # add the route for search trips (search by title, start_date, end_date)
-    path('trips/search', TripSearch.as_view(), name='trip_search'),
+    # Route for Upcoming Trips
+    #   When searching by condition, the api url format: trips/upcoming?title=XXX&start_date=XXX&end_date=XXX
     path('trips/upcoming', TripUpcomingList.as_view(), name='trip_upcoming_list'),
-    
     path('activity', ActivityList.as_view(), name='activity_list')
 ]
--- api/trip/views.py
+++ api/trip/views.py
@@ -16,27 +16,21 @@ class LocationList(generics.ListAPIView):
 class TripList(generics.ListAPIView):
     queryset = Trip.objects.all()
     serializer_class = TripSerializer
-
+    
+    filter_backends = ( DjangoFilterBackend, OrderingFilter)
+    # filter the list by customized filter class TripFilter
+    filterset_class = TripFilter
     '''
     Sort the result list
     First, sorts the list by start_date with positive sequence, then sorts the list by end_date with positive sequence
         It means, If some trip's start_date are equal, the sort the end_date with positive sequence
     '''
-    filter_backends = ( OrderingFilter,)
-    ordering_fields = ("start_date","end_date",)
-    ordering = ('start_date',"end_date",)
-
-class TripSearch(generics.ListAPIView):
-    queryset = Trip.objects.all()
-    serializer_class = TripSerializer
-    filter_backends = ( DjangoFilterBackend, OrderingFilter)
-    # filter the list by customized filter class TripFilter
-    filterset_class = TripFilter
-    # Sort the result list
     ordering_fields = ("start_date","end_date",)
     ordering = ('start_date',"end_date",)
     
+
 class TripUpcomingList(generics.ListAPIView):
+    # get the list which its' start_date greate than today
     queryset = Trip.objects.filter(start_date__gt=date.today())
     serializer_class = TripSerializer
 
