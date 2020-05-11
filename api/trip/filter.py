ca57a56a8f6609f45db81dcf8b54b2997d1ccd1c
shimin <thunderous_0518@hotmail.com>
2020-05-11 16:55:09 -0400

    edit the comments

--- api/trip/filter.py
+++ api/trip/filter.py
@@ -3,10 +3,11 @@ import django_filters
 from .models import Trip
 
 '''
-filter class for trip by title, start_date, end_date
+filter class for trip by title and time range
     title: filter by title with fuzzy query, return the list which each trip.title contains the filter's title (ignore case)
-    start_date: return the list which each trip.start_date >= the filter's start_date
-    end_date: return the list which each trip.end_date <= the filter's end_date
+    time range: user can get a list with selecting a time range(start_date or end_date)
+        start_date: return the list which each trip.start_date >= the selected start_date
+        end_date: return the list which each trip.end_date <= the selected end_date
 '''
 class TripFilter(FilterSet):
     start_date = django_filters.DateFilter(field_name="start_date",
