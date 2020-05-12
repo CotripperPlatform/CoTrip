79f2942e82afb9f21a21a027261ee4138f8cbe79
Scott Kutler <61331933+skut21x-ga@users.noreply.github.com>
2020-05-12 00:24:13 -0400

    set the navbar arrow, set the banner background

--- client/src/pages/CommunityPage/CommunityPage.js
+++ client/src/pages/CommunityPage/CommunityPage.js
@@ -4,22 +4,21 @@ import NavBar from "../../components/Navbar/Navbar";
 import people from "assets/images/profile_default.svg";
 import Banner from "../../components/Banner/Banner";
 import InputTextField from "../../components/InputTextField/InputTextField";
-import Banner__pink from "assets/images/Banner__pink.png";
-
+import Banner__Community from "assets/images/community_banner.png";
 import Button from "../../components/Button/Button";
 import Footer from "../../components/Footer/Footer";
 
 const CommunityPage = props => {
   return (
     <div className="CommunityPage">
-      <NavBar page={3} profileImage={people} />
-      <Banner background={Banner__pink}>
-        <h3 style={{ margin: 0 }}>Book a Trip</h3>
+      <NavBar page={1} profileImage={people} />
+      <Banner background={Banner__Community}>
+        <h3 style={{ margin: 0 }}>Community: Groups</h3>
         <InputTextField
           type="text"
           variation="search"
           name="search directory"
-          placeholder="Search Trip"
+          placeholder="Search My Directory"
         />
       </Banner>
       <header className="CommunityPage__header">COMMUNITY</header>
