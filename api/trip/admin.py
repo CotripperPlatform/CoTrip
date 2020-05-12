f9e25887173feba6f5cb2241d584f283ec99d1ce
Galen Emanuel <galen.emanuel.fencing@gmail.com>
2020-05-12 14:07:54 -0400

    updated readme

--- api/trip/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Location, Trip, Activity
-
-admin.site.register(Location)
-admin.site.register(Trip)
-admin.site.register(Activity)
--- api/trip/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TripConfig(AppConfig):
-    name = 'trip'
