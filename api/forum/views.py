840a2e51c3260f5903d6aed7bfa5239f0ce0d43f
ARJUN_RAWAL <arjunrawal07@gmail.com>
2020-05-14 15:16:31 -0400

    cleaned up commented out code

--- api/forum/views.py
+++ api/forum/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Post
 from rest_framework import generics, permissions
 from .serializers import PostSerializer
-from .permissions import IsOwnerOrReadOnly
+
 
 
 class PostList(generics.ListCreateAPIView):
@@ -12,9 +12,7 @@ class PostList(generics.ListCreateAPIView):
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    # queryset = Post.objects.all()
     serializer_class = PostSerializer
-    # permissions_classes = ( IsOwnerOrReadOnly,)
     def get_queryset(self):
         user = self.request.user
         return Post.objects.filter(author=user.id)
