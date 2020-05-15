aed96aeaf3fc665d200fe158da209e43d248e5be
Scott Kutler <61331933+skut21x-ga@users.noreply.github.com>
2020-05-14 22:02:17 -0400

    added join button. need to style

--- client/src/pages/CommunityPage/CommunityPageGroup.js
+++ client/src/pages/CommunityPage/CommunityPageGroup.js
@@ -5,17 +5,16 @@ import people from "assets/images/profile_default.svg";
 import Banner from "../../components/Banner/Banner";
 import InputTextField from "../../components/InputTextField/InputTextField";
 import Banner__Community from "assets/images/community_banner.png";
-import Button from "../../components/Button/Button";
-import Footer from "../../components/Footer/Footer";
-import Pill from "../../components/Pill/Pill";
-import GroupCard from "../../components/GroupCard/GroupCard";
-import picture1 from "../../assets/images/card_small1.png";
-import picture2 from "../../assets/images/card_small2.png";
-import picture3 from "../../assets/images/card_small3.png";
-import Card from "../../components/Card/Card";
+
+import ModalContainer from "../../components/Modal/_ModalContainer";
 
 function pillClick(val) {
   console.log(val);
+}
+
+function handleEvent(evt) {
+  evt.preventDefault();
+  alert("something happens");
 }
 
 const handleClick = e => {
@@ -25,21 +24,34 @@ const handleClick = e => {
 // Page or
 const CommunityPageGroup = props => {
   return (
-    <div className="CommunityPage-Group-">
-      <NavBar page={1} profileImage={people} />
-      <Banner background={Banner__Community}>
-        <div className="community-page-header">
-          {" "}
-          <h3 style={{ margin: -5 }}>Group: Moms in DC</h3>
-        </div>
-        <InputTextField
-          type="text"
-          variation="search"
-          name="search directory"
-          placeholder="Search My Directory"
-          variation="wide"
-        />
-      </Banner>
+    <div>
+      {" "}
+      <div className="CommunityPage-Group-">
+        <NavBar page={1} profileImage={people} />
+        <Banner background={Banner__Community}>
+          <div className="community-page-header">
+            {" "}
+            <h3 style={{ margin: -5 }}>Group: Moms in DC</h3>
+          </div>
+          <InputTextField
+            type="text"
+            variation="search"
+            name="search directory"
+            placeholder="Search My Directory"
+            variation="wide"
+          />
+          <ModalContainer
+            buttonText="Join"
+            buttonTextColor="white"
+            buttonColor="pink"
+            buttonSize="small"
+            message="Are you sure you want to join?"
+            confirmText="Join"
+            cancelText="Exit"
+            onConfirm={handleEvent}
+          />
+        </Banner>{" "}
+      </div>
     </div>
   );
 };
