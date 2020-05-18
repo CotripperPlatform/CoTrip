be90e80983d5025096b92d5d9ff895279ecad1e5
Noah Clark <noahbc08@gmail.com>
2020-05-18 13:02:08 -0400

    Update settings.sql

--- api/settings.sql
+++ api/settings.sql
@@ -1,4 +1,5 @@
 DROP DATABASE cotripper;
+DROP USER cotripperuser;
 CREATE DATABASE cotripper;
 CREATE USER cotripperuser WITH PASSWORD 'cotripper';
 GRANT ALL PRIVILEGES ON DATABASE cotripper TO cotripperuser;
