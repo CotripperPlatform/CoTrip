96ceeac167e53d9efaad5b289bcd7dac85728eee
secondaryfun <c.birkenhagen@gmail.com>
2020-07-01 15:04:09 -0400

    Removed unused methods & divs.

--- client/src/pages/MemberProfilePage/MemberProfilePage.js
+++ client/src/pages/MemberProfilePage/MemberProfilePage.js
@@ -71,14 +71,6 @@ class MemberProfilePage extends Component {
 
   }
 
-
-
-  // Runs after a component has been updated
-  componentDidUpdate() { }
-
-  // Runs right before a component is removed from the DOM
-  componentWillUnmount() { }
-
   render() {
     this.state.protectedProfileData ? console.log(this.state.protectedProfileData.social_media) : console.log('no profile data')
 
@@ -184,11 +176,7 @@ class MemberProfilePage extends Component {
                       social_media={this.state.protectedProfileData.social_media}
                     />
                     : ''}
-
-
-
                 </div>
-                <div></div>
               </div>
               <div className="Connections__list-Member">
                 <Connections
