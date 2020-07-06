78a5bcf611d9aa22c8da9966bb9e0912d1c69059
secondaryfun <c.birkenhagen@gmail.com>
2020-07-06 14:54:23 -0400

    added posts to group serializer

--- api/community/serializers.py
+++ api/community/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from .models import Group, Event, Topic, Hashtag, Media
 
+
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields = '__all__'
+        fields = ['title', 'description', 'location', 'posts']
+
 
 class EventSerializer(serializers.ModelSerializer):
     group = serializers.CharField()
@@ -14,19 +16,23 @@ class EventSerializer(serializers.ModelSerializer):
         model = Event
         fields = '__all__'
 
+
 class TopicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Topic
         fields = '__all__'
+
 
 class HashtagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hashtag
         fields = '__all__'
 
+
 class MediaSerializer(serializers.ModelSerializer):
     hashtag = serializers.CharField()
     group = serializers.CharField()
+
     class Meta:
         model = Media
         fields = '__all__'
