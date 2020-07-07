0a9423fa1b69842c59fc3e60d8c7f47da2f1fd52
secondaryfun <c.birkenhagen@gmail.com>
2020-07-07 12:53:19 -0400

    comments only render if props.length > 0

--- client/src/components/Comment/Comment.js
+++ client/src/components/Comment/Comment.js
@@ -10,28 +10,30 @@ const Comment = props => {
   // No props right now but when there is data we can use
   // this.props.comment and this.props.date
   const { body, date, time, to, image, name, likes, replies, type } = props;
+  if (props.length) {
 
-  return (
-    <div className="Comment">
-      <div className="Comment__column Comment__profile">
-        <ProfilePicture type="small" to={to} image={image} />
-        <h4 className="Comment__name">{name}</h4>
-      </div>
-      <div className="Comment__column Comment__content">
-        <div className="Comment__column--left">
-          <p className="Comment__body">{body}</p>
-          <div className="Comment__activities">
-            <div className="Comment__likes">{likes} Likes</div>
-            {/* <div className="Comment__replies">{replies} Replies</div> */}
+    return (
+      <div className="Comment">
+        <div className="Comment__column Comment__profile">
+          <ProfilePicture type="small" to={to} image={image} />
+          <h4 className="Comment__name">{name}</h4>
+        </div>
+        <div className="Comment__column Comment__content">
+          <div className="Comment__column--left">
+            <p className="Comment__body">{body}</p>
+            <div className="Comment__activities">
+              <div className="Comment__likes">{likes} Likes</div>
+              {/* <div className="Comment__replies">{replies} Replies</div> */}
+            </div>
+          </div>
+          <div className="Comment__column Comment__timing--container">
+            <p className="Comment__timing">{date}</p>
+            <p className="Comment__timing">{time}</p>
           </div>
         </div>
-        <div className="Comment__column Comment__timing--container">
-          <p className="Comment__timing">{date}</p>
-          <p className="Comment__timing">{time}</p>
-        </div>
       </div>
-    </div>
-  );
-};
+    )
+  } else return ""
+}
 
 export default Comment;
