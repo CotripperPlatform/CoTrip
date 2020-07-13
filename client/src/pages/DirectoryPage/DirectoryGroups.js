1ddee269629b9ef24eca7816bae5318f9a04f508
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-07-13 10:27:43 -0400

    deleted pills and removed key error

--- client/src/pages/DirectoryPage/DirectoryGroups.js
+++ client/src/pages/DirectoryPage/DirectoryGroups.js
@@ -5,45 +5,38 @@ import people from "assets/images/profile_default.svg";
 import Banner from "../../components/Banner/Banner";
 import Banner__Directory from "assets/images/Banner__pink.png";
 import InputTextField from "../../components/InputTextField/InputTextField";
-import Banner__Community from "assets/images/community_banner.png";
 import Button from "../../components/Button/Button";
 import Footer from "../../components/Footer/Footer";
-import Pill from "../../components/Pill/Pill";
 import GroupCard from "../../components/GroupCard/GroupCard";
 import picture1 from "../../assets/images/card_small1.png";
-import picture2 from "../../assets/images/card_small2.png";
-import picture3 from "../../assets/images/card_small3.png";
 import Card from "../../components/Card/Card";
-import { BASE_URL } from '../../services/constants';
+import { BASE_URL } from "../../services/constants";
+import { render } from "react-dom";
 import axios from "axios";
-
-function pillClick(val) {
-  console.log(val);
-}
 
 // Page or
 class DirectoryGroups extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       groups: []
-    }
+    };
   }
 
   componentDidMount() {
-    axios.get(`${BASE_URL}/groups`)
+    axios
+      .get(`${BASE_URL}/groups`)
       .then(response => {
-        this.setState({ 
+        this.setState({
           groups: response.data
-        })
+        });
       })
       .catch(error => {
-        console.log('Error while fetching and parsing API request', error);
-      })
+        render("Error while fetching and parsing API request", error);
+      });
   }
 
   render() {
-    console.log(this.state)
     const groups = this.state.groups ? this.state.groups : [];
     return (
       <div className="DirectoryPage">
@@ -65,132 +58,25 @@ class DirectoryGroups extends Component {
             <Button text="My Groups" color="purple" size="long" />
           </a>
         </div>
-
-        <div className="CommunityPage_SortByButton">
-          <div className="CommunityPage_SortByText">Sort By: Location </div>
-        </div>
         <div className="CommunityPage_body">
           <div>
-            <header className="CommunityPage__header">Group Location:</header>
-          </div>
-          <div className="CommunityPage__group-location-container">
-           
-            <Pill
-              className="Pill"
-              text={"Traveling"}
-              size={"wide"}
-              color={"purple"}
-              icon={"white"}
-              onClick={pillClick}
-              selectId={0}
-            />
-            <Pill
-              className="Pill"
-              text={"Traveling"}
-              size={"wide"}
-              color={"pink"}
-              icon={"white"}
-              onClick={pillClick}
-              selectId={0}
-            />
-            <Pill
-              className="Pill"
-              text={"Traveling"}
-              size={"wide"}
-              color={"pink"}
-              icon={"white"}
-              onClick={pillClick}
-              selectId={0}
-            />
-            <Pill
-              className="Pill"
-              text={"Traveling"}
-              size={"wide"}
-              color={"pink"}
-              icon={"white"}
-              onClick={pillClick}
-              selectId={0}
-            />
-            <Pill
-              className="Pill"
-              text={"Traveling"}
-              size={"wide"}
-              color={"pink"}
-              icon={"white"}
-              onClick={pillClick}
-              selectId={0}
-            />{" "}
-            <Pill
-              className="Pill"
-              text={"Traveling"}
-              size={"wide"}
-              color={"pink"}
-              icon={"white"}
-              onClick={pillClick}
-              selectId={0}
-            />{" "}
-            <Pill
-              className="Pill"
-              text={"Traveling"}
-              size={"wide"}
-              color={"pink"}
-              icon={"white"}
-              onClick={pillClick}
-              selectId={0}
-            />
-            <Pill
-              className="Pill"
-              text={"Traveling"}
-              size={"wide"}
-              color={"pink"}
-              icon={"white"}
-              onClick={pillClick}
-              selectId={0}
-            />
-            <Pill
-              className="Pill"
-              text={"Traveling"}
-              size={"wide"}
-              color={"pink"}
-              icon={"white"}
-              onClick={pillClick}
-              selectId={0}
-            />
-            <Pill
-              className="Pill"
-              text={"Traveling"}
-              size={"wide"}
-              color={"pink"}
-              icon={"white"}
-              onClick={pillClick}
-              selectId={0}
-            />
-          </div>{" "}
-          <a className="seeMore-Button">See More</a>
-          <div>
-            <header className="CommunityPage__header">Groups in WASHINGTON, DC:</header>
+            <header className="CommunityPage__header">Your Groups</header>
           </div>
           <div className="CommunityPage__groups-in-city-container">
-          {groups.map(group =>{
-            console.log(group)
-            let memberLength = 1;
-            if (memberLength){
-              const memberLength = group.members.length;
-            }
-            return(
-              <div className="CommunityPage__groupCard-single">
-              <GroupCard
-                name={group.title}
-                members={memberLength}
-                location={group.location}
-                picture={picture1}
-                id={group.id}
-              />
-              </div>
-            )
-          }) 
-          }
-          
+            {/* Map loops through groups array, assigns the number of members to variable and generates card */}
+            {groups.map((group, index) => {
+              return (
+                <div key={index} className="CommunityPage__groupCard-single">
+                  <GroupCard
+                    name={group.title}
+                    members={group.members.length}
+                    location={group.location}
+                    picture={picture1}
+                    id={group.id}
+                  />
+                </div>
+              );
+            })}
             <div className="CommunityPage__groupCard-single">
               {" "}
               <Card outline size="medium-wide-directory">
@@ -204,6 +90,6 @@ class DirectoryGroups extends Component {
       </div>
     );
   }
-};
+}
 
 export default DirectoryGroups;
