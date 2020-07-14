0c215c27b53707007829967cb97ac169944bf6d6
secondaryfun <c.birkenhagen@gmail.com>
2020-07-14 15:37:31 -0400

    removing ForumPost changes, not part of this issue.

--- client/src/components/ForumPost/ForumPost.js
+++ client/src/components/ForumPost/ForumPost.js
@@ -27,24 +27,24 @@ export default function ForumPost(props) {
               hashtags={props.post.hashtags}
             />
           ) : (
-              <PostBody />
-            )}
+            <PostBody />
+          )}
         </div>
         <div className="ForumPost__middle">
           <div className="ForumPost__topic-pills">
             {props.topics
               ? props.topics.map(topic => (
-                <Pill
-                  key={topic}
-                  text={topic}
-                  active={false}
-                  size={"small"}
-                  color={"red"}
-                  inactiveColor={"pink"}
-                  onClick={props.pillClick}
-                  selectId={0}
-                />
-              ))
+                  <Pill
+                    key={topic}
+                    text={topic}
+                    active={false}
+                    size={"small"}
+                    color={"red"}
+                    inactiveColor={"pink"}
+                    onClick={props.pillClick}
+                    selectId={0}
+                  />
+                ))
               : null}
           </div>
           <div className="ForumPost__likes-comments">
@@ -53,7 +53,8 @@ export default function ForumPost(props) {
               {" Likes"}
             </p>
             <p>
-              {`${props.comments.length} ${props.comments.length === 1 ? " Comment" : " Comments"}`}
+              {props.comments}
+              {" Comments"}
             </p>
           </div>
         </div>
@@ -74,11 +75,11 @@ ForumPost.defaultProps = {
 };
 ForumPost.propTypes = {
   commentClick: PropTypes.func.isRequired,
-  // likeClick: PropTypes.func.isRequired,
+  likeClick: PropTypes.func.isRequired,
   pillClick: PropTypes.func.isRequired,
   to: PropTypes.string,
   likes: PropTypes.number,
-  comments: PropTypes.array,
+  comments: PropTypes.number,
   name: PropTypes.string,
   image: PropTypes.string,
   topics: PropTypes.array,
