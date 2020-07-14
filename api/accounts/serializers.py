ce094b48b86a75851c5cdb12944bbe16d063e58c
secondaryfun <c.birkenhagen@gmail.com>
2020-07-14 11:47:37 -0400

    file cleanup for PR - 1

--- api/accounts/serializers.py
+++ api/accounts/serializers.py
@@ -22,7 +22,6 @@ class ProfileSerializer(serializers.ModelSerializer):
     social_media = ProfileSocialMediaSerializer(many=True, read_only=True)
     class Meta:
         model = Profile
-        depth = 1
         fields = ['user', 'topics', 'hashtags', 'image', 'first_name', 'last_name', 'city_of_residence',
                   'age', 'dream_destination', 'bio', 'activities', 'events', 'connections', 'social_media', 'groups']
 
