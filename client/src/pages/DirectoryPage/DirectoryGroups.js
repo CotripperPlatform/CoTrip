3075b420e951859e39a8172895e45891f477776d
secondaryfun <c.birkenhagen@gmail.com>
2020-07-15 00:30:34 -0400

    DirectoryGroup Page & Community Group Page now connected to backend. Pills removed.

--- client/src/pages/DirectoryPage/DirectoryGroups.js
+++ client/src/pages/DirectoryPage/DirectoryGroups.js
@@ -2,45 +2,54 @@ import React, { Component } from "react";
 import "./DirectoryGroups.css";
 import Banner from "../../components/Banner/Banner";
 import Banner__Directory from "assets/images/Banner__pink.png";
-import InputTextField from "../../components/InputTextField/InputTextField";
 import Button from "../../components/Button/Button";
 import GroupCard from "../../components/GroupCard/GroupCard";
 import picture1 from "../../assets/images/card_small1.png";
 import Card from "../../components/Card/Card";
-import { BASE_URL } from "../../services/constants";
-import { render } from "react-dom";
-import axios from "axios";
+import { getAllGroups } from "../../services/Groups";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import TextField from "@material-ui/core/TextField";
 
-// Page or
+
 class DirectoryGroups extends Component {
   constructor(props) {
     super(props);
     this.state = {
       groups: []
     };
+
+    this.getAllGroups = getAllGroups.bind(this);
+
   }
 
   componentDidMount() {
     this.getAllGroups()
   }
 
+  handleSelect = (e) => {
+    // TODO: load selected group page
+    console.log(e.target.value)
+  }
+
   render() {
-    const groups = this.state.groups;
+    const groups = this.state.groupList ? this.state.groupList : [];
     return (
       <div className="DirectoryPage">
         <Banner background={Banner__Directory}>
           <h3 style={{ margin: 0 }}>Directory: My Groups</h3>
-          <InputTextField
-            type="text"
-            variation="wide"
-            name="search directory"
-            placeholder="Search Groups"
+          <Autocomplete
+            style={{ width: 400 }}
+            id="AutoStateField"
+            options={groups}
+            getOptionLabel={option => option.title}
+            onSelect={this.handleSelect}
+            renderInput={params => <TextField {...params} label="Search Groups" variant="filled" />}
           />
         </Banner>
         <div className="secondNav">
           <a className="secondNav" href="./people">
             <Button text="My Friends" color="outline" size="long" />
-          </a>{" "}
+          </a>
           <a className="secondNav" href="./groups">
             <Button text="My Groups" color="purple" size="long" />
           </a>
