855af8de23f2422a469788c1501cfcc95c53c9fc
secondaryfun <c.birkenhagen@gmail.com>
2020-07-14 23:13:38 -0400

    Connecting ForumPage & ForumPageDiscover

--- client/src/pages/ForumPage/ForumPage.js
+++ client/src/pages/ForumPage/ForumPage.js
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
 import "./ForumPage.css";
-import NavBar from "../../components/Navbar/Navbar";
-import people from "assets/images/profile_default.svg";
 import Banner from "../../components/Banner/Banner";
 import InputTextField from "../../components/InputTextField/InputTextField";
 import Banner__Community from "assets/images/community_banner.png";
 import Button from "../../components/Button/Button";
-import Footer from "../../components/Footer/Footer";
 import Pill from "../../components/Pill/Pill";
 import InputSelect from "../../components/InputSelect/InputSelect";
 import getHashtags from "../../services/Forum.js";
 import { Link } from "react-router-dom";
+import { getAllTopics, getAllHashtags } from "../../services/Community";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import TextField from "@material-ui/core/TextField";
 
+
+
+// TODO: Enable methods for button interactions
 const handleSelect = props => { };
 function pillClick(val) { }
 const handleClick = e => { };
@@ -19,28 +22,74 @@ class ForumPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      hashtags: []
     };
-    this.getHashtags = getHashtags.bind(this);
+    this.getAllHashtags = getAllHashtags.bind(this);
+    this.getAllTopics = getAllTopics.bind(this);
   }
 
   componentDidMount() {
-    this.getHashtags();
+    this.getAllHashtags(this.getAllTopics())
+
   }
 
+  handleSelect = (e) => {
+    // TODO: load selected hashtag or topic page
+    console.log(e.target.value)
+  }
 
   render() {
-    const hashtags = this.state.hashtags ? this.state.hashtags : [];
+    let searchList = []
+    if (this.state.hashtagList && this.state.topicList) {
+      searchList = [...this.state.hashtagList, ...this.state.topicList]
+    }
+    console.log(this.state.hashtagList)
+
+    const hashtags = !this.props.hashtags ? "" : this.props.hashtags.map(hashtag => {
+      return (
+        <div className="Forum-Pill">
+          <Link to={`./forum-page-hashtag/${hashtag.id}`} style={{ textDecoration: "none" }}>
+            <Pill
+              text={hashtag.title}
+              size={"medium"}
+              color={"pink"}
+              icon={"pink"}
+              shadow
+              border
+              onClick={pillClick}
+              selectId={hashtag.id}
+            />
+          </Link>
+        </div>
+      );
+    })
+    const topics = !this.props.topics ? "" : this.props.topics.map(topic => {
+      return (
+        <Link to={`./forum-page-topic/${topic.id}`} style={{ textDecoration: "none" }}>
+          <Pill
+            className="Forum__Pill"
+            text={topic.title}
+            size={"short"}
+            color={"red"}
+            icon={"white"}
+            onClick={pillClick}
+            selectId={topic.id}
+          />
+        </Link>
+      );
+    })
 
     return (
       <div className="ForumPage">
         <Banner background={Banner__Community}>
           <h3 style={{ margin: 0 }}>Forum: Discover Topics</h3>
-          <InputTextField
-            type="text"
-            variation="wide"
-            name="search directory"
-            placeholder="Search Topics and #hashtags"
+          <Autocomplete
+            style={{ width: 300 }}
+            id="AutoStateField"
+            options={searchList}
+            value={option => option.id}
+            getOptionLabel={option => option.title}
+            onSelect={this.handleSelect}
+            renderInput={params => <TextField {...params} label="Search Topics and #hashtags" variant="filled" />}
           />
         </Banner>
         <div className="secondNav">
@@ -56,8 +105,6 @@ class ForumPage extends Component {
             />
           </a>
         </div>
-
-
         <div className="ForumPage__body">
           <div className="ForumPage__sort">
             <InputSelect
@@ -75,131 +122,14 @@ class ForumPage extends Component {
             <header className="ForumPage__header">My Favorite Topics</header>
           </div>
           <div className="ForumPage__pill-container">
-            <Pill
-              className="Forum__Pill"
-              text={"Traveling"}
-              size={"medium"}
-              size={"short"}
-              color={"red"}
-              icon={"white"}
-              onClick={pillClick}
-              selectId={0}
-            />
-            <Pill
-              className="Forum__Pill"
-              text={"Traveling"}
-              size={"medium"}
-              size={"short"}
-              color={"red"}
-              icon={"white"}
-              onClick={pillClick}
-              selectId={0}
-            />
-            <Pill
-              className="Forum__Pill"
-              text={"Traveling"}
-              size={"medium"}
-              size={"short"}
-              color={"pink"}
-              icon={"white"}
-              onClick={pillClick}
-              selectId={0}
-            />
-            <Pill
-              className="Forum__Pill"
-              text={"Traveling"}
-              size={"medium"}
-              size={"short"}
-              color={"pink"}
-              icon={"white"}
-              onClick={pillClick}
-              selectId={0}
-            />
-            <Pill
-              className="Forum__Pill"
-              text={"Traveling"}
-              size={"medium"}
-              size={"short"}
-              color={"pink"}
-              icon={"white"}
-              onClick={pillClick}
-              selectId={0}
-            />{" "}
-            <Pill
-              className="Forum__Pill"
-              text={"Traveling"}
-              size={"medium"}
-              size={"short"}
-              color={"pink"}
-              icon={"white"}
-              onClick={pillClick}
-              selectId={0}
-            />{" "}
-            <Pill
-              className="Forum__Pill"
-              text={"Traveling"}
-              size={"medium"}
-              size={"short"}
-              color={"pink"}
-              icon={"white"}
-              onClick={pillClick}
-              selectId={0}
-            />
-            <Pill
-              className="Forum-Pill"
-              text={"Traveling"}
-              size={"medium"}
-              size={"short"}
-              color={"pink"}
-              icon={"white"}
-              onClick={pillClick}
-              selectId={0}
-            />
-            <Pill
-              className="Forum__Pill"
-              text={"Traveling"}
-              size={"medium"}
-              size={"short"}
-              color={"pink"}
-              icon={"white"}
-              onClick={pillClick}
-              selectId={0}
-            />
-            <Pill
-              className="Forum__Pill"
-              text={"Traveling"}
-              size={"medium"}
-              size={"short"}
-              color={"pink"}
-              icon={"white"}
-              onClick={pillClick}
-              selectId={0}
-            />
+            {topics}
           </div>
           <div>
             <header className="ForumPage__header">#hashtags I'm Following</header>
           </div>
           <div className="ForumPage__hollow-pill-container">
-            {hashtags.map(hashtag => {
-              return (
-                <div className="Forum-Pill">
-                  <Link to={"./forum-page-hashtag/"} style={{ textDecoration: "none" }}>
-                    <Pill
-                      text={hashtag.title}
-                      size={"medium"}
-                      color={"pink"}
-                      icon={"pink"}
-                      shadow
-                      border
-                      onClick={pillClick}
-                      selectId={hashtag.id}
-                    />
-                  </Link>
-                </div>
-              );
-            })}
+            {hashtags}
           </div>
-          <div className="ForumPage__button-container"></div>
         </div>
       </div>
     );
--- client/src/pages/ForumPage/ForumPageDiscover.js
+++ client/src/pages/ForumPage/ForumPageDiscover.js
@@ -1,255 +1,137 @@
-import React from "react";
+import React, { Component } from "react";
 import "./ForumPage.css";
-import NavBar from "../../components/Navbar/Navbar";
-import people from "assets/images/profile_default.svg";
 import Banner from "../../components/Banner/Banner";
 import InputTextField from "../../components/InputTextField/InputTextField";
 import Banner__Community from "assets/images/community_banner.png";
 import Button from "../../components/Button/Button";
-import Footer from "../../components/Footer/Footer";
 import Pill from "../../components/Pill/Pill";
 import InputSelect from "../../components/InputSelect/InputSelect";
+import { Link } from "react-router-dom";
+import { getAllTopics, getAllHashtags } from "../../services/Community";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import TextField from "@material-ui/core/TextField";
 
-const handleSelect = props => {
-  console.log(props);
-};
-function pillClick(val) {
-  console.log(val);
-}
-const handleClick = e => {
-  console.log("Clicked");
-};
 
-const ForumPageDiscover = props => {
-  return (
-    <div className="ForumPage">
-      <Banner background={Banner__Community}>
-        <h3 style={{ margin: 0 }}>Search: Packing</h3>
-        <InputTextField
-          type="text"
-          variation="wide"
-          name="search directory"
-          placeholder="Packing"
-        />
-      </Banner>
-      <div className="secondNav">
-        <a className="secondNav" href="./forum-page">
-          <Button
-            text="My Favorite Topics"
-            color="outlinepink"
-            size="long"
-            handleClick={handleClick}
-          />
-        </a>
-        <a className="secondNav">
-          <Button text="Discover Topics" color="pink" size="long" handleClick={handleClick} />
-        </a>
-      </div>
+// TODO: Enable methods for button interactions
+const handleSelect = props => { };
+function pillClick(val) { }
+const handleClick = e => { };
 
-      <div className="forum-page-body">
-        <div className="forum-page-sort">
-          <InputSelect
-            onSelect={handleSelect}
-            optionPrefix={"Sort By:  "}
-            options={[
-              { value: "Popularity", title: "Popularity" },
-              { value: "location", title: "Location" },
-              { value: "date", title: "Date" },
-              { value: "type", title: "Type" }
-            ]}
-          />
+class ForumPageDiscover extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+    this.getAllHashtags = getAllHashtags.bind(this);
+    this.getAllTopics = getAllTopics.bind(this);
+  }
+
+  componentDidMount() {
+    this.getAllHashtags(this.getAllTopics())
+
+  }
+
+  handleSelect = (e) => {
+    // TODO: load selected hashtag or topic page
+    console.log(e.target.value)
+  }
+
+  render() {
+    let searchList = []
+    if (this.state.hashtagList && this.state.topicList) {
+      searchList = [...this.state.hashtagList, ...this.state.topicList]
+    }
+
+    const hashtags = !this.state.hashtagList ? "" : this.state.hashtagList.map(hashtag => {
+      return (
+        <div className="Forum-Pill">
+          <Link to={`./forum-page-hashtag/${hashtag.id}`} style={{ textDecoration: "none" }}>
+            <Pill
+              text={hashtag.title}
+              size={"medium"}
+              color={"pink"}
+              icon={"pink"}
+              shadow
+              border
+              onClick={pillClick}
+              selectId={hashtag.id}
+            />
+          </Link>
         </div>
-        <div>
-          <header className="ForumPage__header">Popular Topics</header>
-        </div>
-        <div className="forum-page-pill-container">
+      );
+    })
+    const topics = !this.state.topicList ? "" : this.state.topicList.map(topic => {
+      return (
+        <Link to={`./forum-page-topic/${topic.id}`} style={{ textDecoration: "none" }}>
           <Pill
-            className="Forum-Pill"
-            text={"Traveling"}
-            size={"medium"}
+            className="Forum__Pill"
+            text={topic.title}
             size={"short"}
             color={"red"}
             icon={"white"}
             onClick={pillClick}
-            selectId={0}
+            selectId={topic.id}
           />
-        </div>
-        <div>
-          <header className="ForumPage__header">Topics</header>
-        </div>
-        <div className="forum-page-pill-container">
-          <Pill
-            className="Forum-Pill"
-            text={"Traveling"}
-            size={"medium"}
-            size={"short"}
-            color={"pink"}
-            icon={"white"}
-            onClick={pillClick}
-            selectId={0}
-          />{" "}
-          <Pill
-            className="Forum-Pill"
-            text={"Traveling"}
-            size={"medium"}
-            size={"short"}
-            color={"pink"}
-            icon={"white"}
-            onClick={pillClick}
-            selectId={0}
-          />{" "}
-          <Pill
-            className="Forum-Pill"
-            text={"Traveling"}
-            size={"medium"}
-            size={"short"}
-            color={"pink"}
-            icon={"white"}
-            onClick={pillClick}
-            selectId={0}
-          />
-          <Pill
-            className="Forum-Pill"
-            text={"Traveling"}
-            size={"medium"}
-            size={"short"}
-            color={"pink"}
-            icon={"white"}
-            onClick={pillClick}
-            selectId={0}
-          />
-          <Pill
-            className="Forum-Pill"
-            text={"Traveling"}
-            size={"medium"}
-            size={"short"}
-            color={"pink"}
-            icon={"white"}
-            onClick={pillClick}
-            selectId={0}
-          />
-        </div>
-        <div>
-          <header className="ForumPage__header">
-            #hashtags: <a href="/forum-page-hashtag">example tag</a>
-          </header>
-        </div>
-        <div className="forum-page-hollow-pill-container">
-          <Pill
-            text={"#hashtags"}
-            size={"medium"}
-            size={"short"}
-            color={"pink"}
-            icon={"pink"}
-            shadow
-            border
-            onClick={pillClick}
-            selectId={0}
-          />
-          <Pill
-            text={"#hashtags"}
-            size={"medium"}
-            size={"short"}
-            color={"pink"}
-            icon={"pink"}
-            shadow
-            border
-            onClick={pillClick}
-            selectId={0}
-          />
-          <Pill
-            text={"#hashtags"}
-            size={"medium"}
-            size={"short"}
-            color={"pink"}
-            icon={"pink"}
-            shadow
-            border
-            onClick={pillClick}
-            selectId={0}
-          />
-          <Pill
-            text={"#hashtags"}
-            size={"medium"}
-            size={"short"}
-            color={"pink"}
-            icon={"pink"}
-            shadow
-            border
-            onClick={pillClick}
-            selectId={0}
-          />
-          <Pill
-            text={"#hashtags"}
-            size={"medium"}
-            size={"short"}
-            color={"pink"}
-            icon={"pink"}
-            shadow
-            border
-            onClick={pillClick}
-            selectId={0}
-          />{" "}
-          <Pill
-            text={"#hashtags"}
-            size={"medium"}
-            size={"short"}
-            color={"pink"}
-            icon={"pink"}
-            shadow
-            border
-            onClick={pillClick}
-            selectId={0}
-          />
-          <Pill
-            text={"#hashtags"}
-            size={"medium"}
-            size={"short"}
-            color={"pink"}
-            icon={"pink"}
-            shadow
-            border
-            onClick={pillClick}
-            selectId={0}
-          />
-          <Pill
-            text={"#hashtags"}
-            size={"medium"}
-            size={"short"}
-            color={"pink"}
-            icon={"pink"}
-            shadow
-            border
-            onClick={pillClick}
-            selectId={0}
-          />
-          <Pill
-            text={"#hashtags"}
-            size={"medium"}
-            size={"short"}
-            color={"pink"}
-            icon={"pink"}
-            shadow
-            border
-            onClick={pillClick}
-            selectId={0}
-          />
-          <Pill
-            text={"#hashtags"}
-            size={"medium"}
-            size={"short"}
-            color={"pink"}
-            icon={"pink"}
-            shadow
-            border
-            onClick={pillClick}
-            selectId={0}
-          />
-        </div>
-        <div className="ForumPage__button-container"></div>
-      </div>
-    </div>
-  );
-};
+        </Link>
+      );
+    })
 
+    return (
+      <div className="ForumPage">
+        <Banner background={Banner__Community}>
+          <h3 style={{ margin: 0 }}>Search: Packing</h3>
+          <Autocomplete
+            style={{ width: 300 }}
+            id="AutoStateField"
+            options={searchList}
+            value={option => option.id}
+            getOptionLabel={option => option.title}
+            onSelect={this.handleSelect}
+            renderInput={params => <TextField {...params} label="Search Topics and #hashtags" variant="filled" />}
+          />
+        </Banner>
+        <div className="secondNav">
+          <a className="secondNav" href="./forum-page">
+            <Button
+              text="My Favorite Topics"
+              color="outlinepink"
+              size="long"
+              handleClick={handleClick}
+            />
+          </a>
+          <a href="./forum-page" className="secondNav">
+            <Button text="Discover Topics" color="pink" size="long" handleClick={handleClick} />
+          </a>
+        </div>
+
+        <div className="ForumPage__body">
+          <div className="ForumPage__sort">
+            <InputSelect
+              onSelect={handleSelect}
+              optionPrefix={"Sort By:  "}
+              options={[
+                { value: "location", title: "Location" },
+                { value: "date", title: "Date" },
+                { value: "type", title: "Type" },
+                { value: "Popularity", title: "Popularity" }
+              ]}
+            />
+          </div>
+          <div>
+            <header className="ForumPage__header">My Favorite Topics</header>
+          </div>
+          <div className="ForumPage__pill-container">
+            {topics}
+          </div>
+          <div>
+            <header className="ForumPage__header">#hashtags I'm Following</header>
+          </div>
+          <div className="ForumPage__hollow-pill-container">
+            {hashtags}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 export default ForumPageDiscover;
