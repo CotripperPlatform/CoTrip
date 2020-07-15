e8303e7437d26c372f2c80a317baae58619ce0b5
secondaryfun <c.birkenhagen@gmail.com>
2020-07-14 23:56:33 -0400

    connecting directory page

--- client/src/pages/DirectoryPage/DirectoryGroups.js
+++ client/src/pages/DirectoryPage/DirectoryGroups.js
@@ -1,234 +1,81 @@
-import React from "react";
+import React, { Component } from "react";
 import "./DirectoryGroups.css";
-import NavBar from "../../components/Navbar/Navbar";
-import people from "assets/images/profile_default.svg";
 import Banner from "../../components/Banner/Banner";
 import Banner__Directory from "assets/images/Banner__pink.png";
 import InputTextField from "../../components/InputTextField/InputTextField";
-import Banner__Community from "assets/images/community_banner.png";
 import Button from "../../components/Button/Button";
-import Footer from "../../components/Footer/Footer";
-import Pill from "../../components/Pill/Pill";
 import GroupCard from "../../components/GroupCard/GroupCard";
 import picture1 from "../../assets/images/card_small1.png";
-import picture2 from "../../assets/images/card_small2.png";
-import picture3 from "../../assets/images/card_small3.png";
 import Card from "../../components/Card/Card";
-
-function pillClick(val) {
-  console.log(val);
-}
+import { BASE_URL } from "../../services/constants";
+import { render } from "react-dom";
+import axios from "axios";
 
 // Page or
-const DirectoryGroups = props => {
-  return (
-    <div className="DirectoryPage">
-      <Banner background={Banner__Directory}>
-        <h3 style={{ margin: 0 }}>Directory: My Groups</h3>
-        <InputTextField
-          type="text"
-          variation="wide"
-          name="search directory"
-          placeholder="Search Groups"
-        />
-      </Banner>
-      <div className="secondNav">
-        <a className="secondNav" href="./people">
-          <Button text="My Friends" color="outline" size="long" />
-        </a>{" "}
-        <a className="secondNav" href="./groups">
-          <Button text="My Groups" color="purple" size="long" />
-        </a>
-      </div>
+class DirectoryGroups extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      groups: []
+    };
+  }
 
-      <div className="CommunityPage_SortByButton">
-        <div className="CommunityPage_SortByText">Sort By: Location </div>
+  componentDidMount() {
+    this.getAllGroups()
+  }
+
+  render() {
+    const groups = this.state.groups;
+    return (
+      <div className="DirectoryPage">
+        <Banner background={Banner__Directory}>
+          <h3 style={{ margin: 0 }}>Directory: My Groups</h3>
+          <InputTextField
+            type="text"
+            variation="wide"
+            name="search directory"
+            placeholder="Search Groups"
+          />
+        </Banner>
+        <div className="secondNav">
+          <a className="secondNav" href="./people">
+            <Button text="My Friends" color="outline" size="long" />
+          </a>{" "}
+          <a className="secondNav" href="./groups">
+            <Button text="My Groups" color="purple" size="long" />
+          </a>
+        </div>
+        <div className="CommunityPage_body">
+          <div>
+            <header className="CommunityPage__header">Your Groups</header>
+          </div>
+          <div className="CommunityPage__groups-in-city-container">
+            {/* Map loops through groups array, assigns the number of members to variable and generates card */}
+            {groups.map((group, index) => {
+              return (
+                <div key={index} className="CommunityPage__groupCard-single">
+                  <GroupCard
+                    name={group.title}
+                    members={group.members.length}
+                    location={group.location}
+                    picture={picture1}
+                    id={group.id}
+                  />
+                </div>
+              );
+            })}
+            <div className="CommunityPage__groupCard-single">
+              {" "}
+              <Card outline size="medium-wide-directory">
+                <a href="/">Discover New Groups</a>
+              </Card>
+            </div>
+          </div>
+          <a className="seeAll-Button">See All</a>
+        </div>
       </div>
-      <div className="CommunityPage_body">
-        <div>
-          <header className="CommunityPage__header">Group Location:</header>
-        </div>
-        <div className="CommunityPage__group-location-container">
-          <Pill
-            className="Pill"
-            text={"Traveling"}
-            size={"wide"}
-            color={"purple"}
-            icon={"white"}
-            onClick={pillClick}
-            selectId={0}
-          />
-          <Pill
-            className="Pill"
-            text={"Traveling"}
-            size={"wide"}
-            color={"pink"}
-            icon={"white"}
-            onClick={pillClick}
-            selectId={0}
-          />
-          <Pill
-            className="Pill"
-            text={"Traveling"}
-            size={"wide"}
-            color={"pink"}
-            icon={"white"}
-            onClick={pillClick}
-            selectId={0}
-          />
-          <Pill
-            className="Pill"
-            text={"Traveling"}
-            size={"wide"}
-            color={"pink"}
-            icon={"white"}
-            onClick={pillClick}
-            selectId={0}
-          />
-          <Pill
-            className="Pill"
-            text={"Traveling"}
-            size={"wide"}
-            color={"pink"}
-            icon={"white"}
-            onClick={pillClick}
-            selectId={0}
-          />{" "}
-          <Pill
-            className="Pill"
-            text={"Traveling"}
-            size={"wide"}
-            color={"pink"}
-            icon={"white"}
-            onClick={pillClick}
-            selectId={0}
-          />{" "}
-          <Pill
-            className="Pill"
-            text={"Traveling"}
-            size={"wide"}
-            color={"pink"}
-            icon={"white"}
-            onClick={pillClick}
-            selectId={0}
-          />
-          <Pill
-            className="Pill"
-            text={"Traveling"}
-            size={"wide"}
-            color={"pink"}
-            icon={"white"}
-            onClick={pillClick}
-            selectId={0}
-          />
-          <Pill
-            className="Pill"
-            text={"Traveling"}
-            size={"wide"}
-            color={"pink"}
-            icon={"white"}
-            onClick={pillClick}
-            selectId={0}
-          />
-          <Pill
-            className="Pill"
-            text={"Traveling"}
-            size={"wide"}
-            color={"pink"}
-            icon={"white"}
-            onClick={pillClick}
-            selectId={0}
-          />
-        </div>{" "}
-        <a className="seeMore-Button">See More</a>
-        <div>
-          <header className="CommunityPage__header">Groups in WASHINGTON, DC:</header>
-        </div>
-        <div className="CommunityPage__groups-in-city-container">
-          <div className="CommunityPage__groupCard-single">
-            <GroupCard
-              name="DIY with your kids"
-              members="98 Members"
-              location="Mixed Locations"
-              picture={picture1}
-            />
-          </div>
-          <div className="CommunityPage__groupCard-single">
-            <GroupCard
-              name="Moms traveling to Hawaii"
-              members="5"
-              location="Chicago, IL"
-              picture={picture2}
-            />
-          </div>{" "}
-          <div className="CommunityPage__groupCard-single">
-            <GroupCard
-              name="DIY with your kids"
-              size="wide"
-              members="98 Members"
-              location="Mixed Locations"
-              picture={picture1}
-            />
-          </div>
-          <div className="CommunityPage__groupCard-single">
-            <GroupCard
-              name="Moms traveling to Hawaii"
-              members="5"
-              location="Chicago, IL"
-              picture={picture2}
-            />
-          </div>{" "}
-          <div className="CommunityPage__groupCard-single">
-            <GroupCard
-              name="DIY with your kids"
-              members="98 Members"
-              location="Mixed Locations"
-              picture={picture1}
-            />
-          </div>
-          <div className="CommunityPage__groupCard-single">
-            <GroupCard
-              name="Moms traveling to Hawaii"
-              members="5"
-              location="Chicago, IL"
-              picture={picture2}
-            />
-          </div>{" "}
-          <div className="CommunityPage__groupCard-single">
-            <GroupCard
-              name="DIY with your kids"
-              members="98 Members"
-              location="Mixed Locations"
-              picture={picture1}
-            />
-          </div>
-          <div className="CommunityPage__groupCard-single">
-            <GroupCard
-              name="Moms traveling to Hawaii"
-              members="5"
-              location="Chicago, IL"
-              picture={picture2}
-            />
-          </div>
-          <div className="CommunityPage__groupCard-single">
-            <GroupCard
-              name="Group Name"
-              members="# of "
-              location="Location, State"
-              picture={picture3}
-            />
-          </div>{" "}
-          <div className="CommunityPage__groupCard-single">
-            {" "}
-            <Card outline size="medium-wide-directory">
-              <a href="/">Discover New Groups</a>
-            </Card>{" "}
-          </div>
-        </div>
-        <a className="seeAll-Button">See All</a>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default DirectoryGroups;
