ff042d4fcdd20def2070980095a3861d8e0224b5
Bcastillo44 <bryancastillo44@outlook.com>
2020-07-15 10:15:21 -0400

    Fixed ForumPage.js with Topics and hashtags

--- client/src/pages/ForumPage/ForumPage.js
+++ client/src/pages/ForumPage/ForumPage.js
@@ -6,77 +6,76 @@ import Banner__Community from "assets/images/community_banner.png";
 import Button from "../../components/Button/Button";
 import Pill from "../../components/Pill/Pill";
 import InputSelect from "../../components/InputSelect/InputSelect";
-import getHashtags from "../../services/Forum.js";
 import { Link } from "react-router-dom";
 import { getAllTopics, getAllHashtags } from "../../services/Community";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import TextField from "@material-ui/core/TextField";
 
-
-
 // TODO: Enable methods for button interactions
-const handleSelect = props => { };
-function pillClick(val) { }
-const handleClick = e => { };
+const handleSelect = props => {};
+function pillClick(val) {}
+const handleClick = e => {};
+
 class ForumPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
     this.getAllHashtags = getAllHashtags.bind(this);
     this.getAllTopics = getAllTopics.bind(this);
   }
 
   componentDidMount() {
-    this.getAllHashtags(this.getAllTopics())
-
+    this.getAllHashtags(this.getAllTopics());
   }
 
-  handleSelect = (e) => {
+  handleSelect = e => {
     // TODO: load selected hashtag or topic page
-    console.log(e.target.value)
-  }
+    console.log(e.target.value);
+  };
 
   render() {
-    let searchList = []
+    let searchList = [];
     if (this.state.hashtagList && this.state.topicList) {
-      searchList = [...this.state.hashtagList, ...this.state.topicList]
+      searchList = [...this.state.hashtagList, ...this.state.topicList];
     }
-    console.log(this.state.hashtagList)
 
-    const hashtags = !this.props.hashtags ? "" : this.props.hashtags.map(hashtag => {
-      return (
-        <div className="Forum-Pill">
-          <Link to={`./forum-page-hashtag/${hashtag.id}`} style={{ textDecoration: "none" }}>
-            <Pill
-              text={hashtag.title}
-              size={"medium"}
-              color={"pink"}
-              icon={"pink"}
-              shadow
-              border
-              onClick={pillClick}
-              selectId={hashtag.id}
-            />
-          </Link>
-        </div>
-      );
-    })
-    const topics = !this.props.topics ? "" : this.props.topics.map(topic => {
-      return (
-        <Link to={`./forum-page-topic/${topic.id}`} style={{ textDecoration: "none" }}>
-          <Pill
-            className="Forum__Pill"
-            text={topic.title}
-            size={"short"}
-            color={"red"}
-            icon={"white"}
-            onClick={pillClick}
-            selectId={topic.id}
-          />
-        </Link>
-      );
-    })
+    const hashtags = !this.state.hashtagList
+      ? ""
+      : this.state.hashtagList.map(hashtag => {
+          return (
+            <div className="Forum-Pill">
+              <Link to={`./forum-page-hashtag/${hashtag.id}`} style={{ textDecoration: "none" }}>
+                <Pill
+                  text={hashtag.title}
+                  size={"medium"}
+                  color={"pink"}
+                  icon={"pink"}
+                  shadow
+                  border
+                  onClick={pillClick}
+                  selectId={hashtag.id}
+                />
+              </Link>
+            </div>
+          );
+        });
+    const topics = !this.state.topicList
+      ? ""
+      : this.state.topicList.map(topic => {
+          return (
+            <Link to={`./forum-page-topic/${topic.id}`} style={{ textDecoration: "none" }}>
+              <Pill
+                className="Forum__Pill"
+                text={topic.title}
+                size={"medium"}
+                color={"red"}
+                icon={"white"}
+                onClick={pillClick}
+                selectId={topic.id}
+              />
+            </Link>
+          );
+        });
 
     return (
       <div className="ForumPage">
@@ -89,7 +88,9 @@ class ForumPage extends Component {
             value={option => option.id}
             getOptionLabel={option => option.title}
             onSelect={this.handleSelect}
-            renderInput={params => <TextField {...params} label="Search Topics and #hashtags" variant="filled" />}
+            renderInput={params => (
+              <TextField {...params} label="Search Topics and #hashtags" variant="filled" />
+            )}
           />
         </Banner>
         <div className="secondNav">
@@ -121,15 +122,11 @@ class ForumPage extends Component {
           <div>
             <header className="ForumPage__header">My Favorite Topics</header>
           </div>
-          <div className="ForumPage__pill-container">
-            {topics}
-          </div>
+          <div className="ForumPage__pill-container">{topics}</div>
           <div>
             <header className="ForumPage__header">#hashtags I'm Following</header>
           </div>
-          <div className="ForumPage__hollow-pill-container">
-            {hashtags}
-          </div>
+          <div className="ForumPage__hollow-pill-container">{hashtags}</div>
         </div>
       </div>
     );
